59eae3e03d2b89a068e4a9ef59410000eae0a7f9
Troy Goode <troygoode@gmail.com>
2012-08-22 22:50:04 -0400

    basic styling

--- client/grid.js
+++ client/grid.js
@@ -1,8 +1,38 @@
 Posts = new Meteor.Collection('posts');
-
 Meteor.subscribe('posts');
+
+// Posts
+
+Template.posts.show = function(){
+  return !Session.get('selected_post');
+};
 
 Template.posts.posts = function(){
   var posts = Posts.find({}, {sort: {headline: 1}});
   return posts;
+};
+
+// Post
+
+Template.post.events = {
+  'click .discuss-link': function(){
+    Session.set('selected_post', this);
+  }
+};
+
+Template.post.ago = function(){
+  var submitted = new Date(this.submitted);
+  return submitted.toString();
+};
+
+// Selected Post
+
+Template.selected_post.show = function(){
+  return !!Session.get('selected_post');
+};
+
+Template.selected_post.post = function(){
+  var post = Session.get('selected_post');
+  console.log(post);
+  return post;
 };
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -4,11 +4,15 @@ function prepopulateDatabase(){
         headline: 'The first post ever, a link to Hacker News'
       , url: 'http://news.ycombinator.com/'
       , submitter: 'Sacha'
+      , submitted: new Date(2012, 7, 22).getTime()
+      , votes: 0
     }
     , {
         headline: 'Another post to fill the page up a little'
       , url: 'http://sachagreif.com/'
       , submitter: 'Sacha'
+      , submitted: new Date(2012, 7, 22).getTime()
+      , votes: 0
     }
   ].forEach(function(post){
     Posts.insert(post);
