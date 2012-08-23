507540af1dae3fd4b9320649de821fd3790836f8
Troy Goode <troygoode@gmail.com>
2012-08-23 00:18:07 -0400

    can reply to comments

--- client/templates/selected_comment.js
+++ client/templates/selected_comment.js
@@ -1,3 +1,21 @@
+Template.selected_comment.events = {
+  'click input[type=submit]': function(){
+    var post = Session.get('selected_post');
+    var parentComment = Session.get('selected_comment');
+    var $comment = $('#comment');
+    var comment = {
+        post: post._id
+      , parent: parentComment._id
+      , body: $comment.val()
+      , submitter: Meteor.user().username
+      , submitted: new Date().getTime()
+    };
+    Comments.insert(comment);
+    Session.set('selected_comment', null);
+    Session.set('state', 'view_post');
+  }
+};
+
 Template.selected_comment.show = function(){
   return Session.equals('state', 'reply');
 };
