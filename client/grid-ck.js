f353a6943ab1e408ced1d6c30a01b224b0bcacb8
Sacha Greif <sacha357@gmail.com>
2012-08-31 18:56:37 +0900

    updated templates

---
+++ client/grid-ck.js
@@ -0,0 +1,1 @@
+Posts=new Meteor.Collection("posts");Meteor.subscribe("posts");Comments=new Meteor.Collection("comments");Meteor.subscribe("comments");MyVotes=new Meteor.Collection("myvotes");Meteor.subscribe("myvotes");Session.set("state","list");
