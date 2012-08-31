2c2a6bf7efbc075f8fb39415db09e2e20149c576
Troy Goode <troygoode@gmail.com>
2012-08-31 19:13:40 -0400

    ignore -ck files

--- client/grid-ck.js
+++
@@ -1,1 +0,0 @@
-Posts=new Meteor.Collection("posts");Meteor.subscribe("posts");Comments=new Meteor.Collection("comments");Meteor.subscribe("comments");MyVotes=new Meteor.Collection("myvotes");Meteor.subscribe("myvotes");Session.set("state","list");
