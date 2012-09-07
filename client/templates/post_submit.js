61a2b47cccf7e19841c4fd180899fe10b5961b99
Sacha Greif <sacha357@gmail.com>
2012-09-07 18:17:19 +0900

    added suggest title link

--- client/templates/post_submit.js
+++ client/templates/post_submit.js
@@ -1,6 +1,6 @@
 Template.post_submit.events = {
-  'click input[type=submit]': function(event){
-    event.preventDefault();
+  'click input[type=submit]': function(e){
+    e.preventDefault();
     if(!Meteor.user()) throw 'You must be logged in.';
 
     var title= $('#title').val();
@@ -23,5 +23,23 @@ Template.post_submit.events = {
     Session.set('selected_post', post);
     // Session.set('state', 'view_post');
     Router.navigate('posts/'+postId, {trigger: true});
+  }
+
+  ,'click .get-title-link': function(e){
+    e.preventDefault();
+    var url=$("#url").val();
+    $(".get-title-link").addClass("loading");
+    if(url){
+      $.get(url, function(response){
+          if ((suggestedTitle=((/<title>(.*?)<\/title>/m).exec(response.responseText))) != null){
+              $("#title").val(suggestedTitle[1]);
+          }else{
+              alert("Sorry, couldn't find a title...");
+          } 
+          $(".get-title-link").removeClass("loading");
+       });  
+    }else{
+      alert("Please fill in an URL first!");
+    }
   }
 };
