5c1e1f91adcdc5be1b6d8a4ff99c51f25e172261
Sacha Greif <sacha357@gmail.com>
2012-09-17 10:28:42 +0900

    working on comment queuing

--- client/templates/comment_item.js
+++ client/templates/comment_item.js
@@ -7,6 +7,90 @@
     return commentIsNew;
   };
 
+  findQueueContainer=function($comment){
+    // go up and down the DOM until we find either A) a queue container or B) an unqueued comment
+    $up=$comment.prevAll(".queue-container, .comment-displayed").first();
+    $down=$comment.nextAll(".queue-container, .comment-displayed").first();
+    $queuedAncestors=$comment.parents(".comment-queued");
+
+    if($queuedAncestors.exists()){
+      console.log("----------- case 1", $queuedAncestors);
+      // 1. 
+      // our comment has one or more queued ancestor, so we look for the root-most
+      // ancestor's queue container
+      console.log($queuedAncestors.last());
+      console.log("data on", $queuedAncestors.last());
+      console.log("data2", $queuedAncestors.last().data());
+      $container=$queuedAncestors.last().data("queue");
+    }else if($up.hasClass("queue-container")){
+      console.log("----------- case 2", $up);
+      // 2. 
+      // we've found a container on the same level above our comment, so we use it to store our queue
+      $container=$up;
+    }else if($down.hasClass("queue-container")){
+      console.log("----------- case 3", $down);
+      // 3. 
+      // we've found a container on the same level below our comment, so we use it to store our queue
+      $container=$down;
+    }else if($up.hasClass('comment-displayed') || !$up.exists()){
+      console.log("----------- case 4", $up);
+      // 4. 
+      // we've found containers neither above or below, but 
+      // A) we've hit a displayed comment or
+      // B) we've haven't found any comments (i.e. we're at the beginning of the list)
+      // so we put our queue container just before the comment
+      $container=$('<div class="queue-container"><ul></ul></div>').insertBefore($comment);
+      $container.click(function(){
+        $(this).find("a").each(function(){
+          var target=$(this).attr("href");
+          console.log("target: ", target);
+          $(target).removeClass("comment-queued").addClass("comment-displayed");
+        });
+        $(this).hide("slow").remove();
+      });
+    }
+    console.log("comment", $comment);
+    console.log("up", $up);
+    console.log("down", $down);
+    console.log("queuedAncestors", $queuedAncestors);
+    console.log("container", $container);
+    // $.data( $comment, "queue", $container );
+    return $container;
+  };
+
+
+Template.comment_item.rendered=function(){
+  t("comment_item");
+  if(this.data){
+    var comment=this.data;
+    var $comment=$("#"+comment._id);
+
+    if(false && Meteor.user() && Meteor.user()._id==comment.user_id){
+      // if user is logged in, and the comment belongs to the user, then never queue it
+    }else{
+      if(commentIsNew(comment) && !$comment.hasClass("comment-queued")){
+        // if comment is new and has not already been previously queued
+        // note: testing on the class works because Meteor apparently preserves newly assigned CSS classes
+        // across template renderings
+        // TODO: save scroll position
+        if(Meteor.users.findOne(comment.user_id)){
+          // get comment author name
+          var author=Meteor.users.findOne(comment.user_id).username;
+        }
+        var $container=findQueueContainer($comment);
+        var comment_link='<li class="icon-user"><a href="#'+comment._id+'" class="has-tooltip"><span class="tooltip"><span>'+author+'</span></span></a></li>';
+        $(comment_link).appendTo($container.find("ul")).hide().fadeIn("slow");
+        $comment.removeClass("comment-displayed").addClass("comment-queued");
+        $comment.data("queue", $container);
+        console.log("data on:", $comment);
+        console.log("data1", $.data($comment));
+        // TODO: take the user back to their previous scroll position
+      }
+    }
+  }
+}
+
+
   Template.comment_item.events = {
     'click .goto-comment': function(event){
       event.preventDefault();
@@ -69,61 +153,6 @@
   }
 })();
 
-Template.comment_item.rendered=function(){
-  // t("comment_item");
-  if(this.data){
-    var comment=this.data;
-    if(Meteor.users.findOne(comment.user_id)){
-      var author=Meteor.users.findOne(comment.user_id).username;
-    }
-    console.log(comment);
-    var $comment=$("#"+comment._id);
-    var $commentParentList=$comment.closest(".comment-list");
-
-    if(false && Meteor.user() && Meteor.user()._id==comment.user_id){
-      // if user is logged in, and the comment belongs to the user, then never queue it
-    }else{
-      if(commentIsNew(comment)){
-        // go up and down the DOM until we find either A) a queue container or B) an unqueued comment
-        $up=$comment.prevAll(".queue-container, .comment-displayed").first();
-        $down=$comment.nextAll(".queue-container, .comment-displayed").first();
-        if($up.hasClass("queue-container")){
-          console.log("case 1", $up);
-          // we've found a container above our comment, so we use it to store our queue
-          $container=$up;
-        }else if($down.hasClass("queue-container")){
-          console.log("case 2", $down);
-          // we've found a container below our comment, so we use it to store our queue
-          $container=$down;
-        }else if($up.hasClass('comment-displayed') || !$up.exists()){
-          console.log("case 3", $up);
-          // we've found containers neither above or below, but 
-          // A) we've hit a displayed comment or
-          // B) we've haven't found any comments (i.e. we're at the beginning of the list)
-          // so we put our queue container just before the comment
-          $container=$('<div class="queue-container"><ul></ul></div>').insertBefore($comment);
-          $container.click(function(){
-            console.log($container);
-            $(this).find("a").each(function(){
-              var target=$(this).attr("href");
-              console.log("target: ", target);
-              $(target).removeClass("comment-queued").addClass("comment-displayed");
-            });
-            $(this).hide("slow").remove();
-          });
-        }else{
-          console.log("case 4");
-        }
-        console.log("up", $up);
-        console.log("down", $down);
-        console.log("container", $container);
-        var comment_link='<li class="icon-user"><a href="#'+comment._id+'" class="has-tooltip"><span class="tooltip"><span>'+author+'</span></span></a></li>';
-        $(comment_link).appendTo($container.find("ul")).hide().fadeIn("slow");
-        $comment.removeClass("comment-displayed").addClass("comment-queued");
-      }
-    }
-  }
-}
 
 Template.comment_item.helpers({
   // isQueued: function() {
