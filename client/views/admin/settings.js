c6ec235c1ebf9fbc54757fe26105b93a970bdcdb
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2012-11-13 15:54:52 +0100

    Generate default form and allow user to overwrite options.

--- client/views/admin/settings.js
+++ client/views/admin/settings.js
@@ -1,23 +1,62 @@
 // note: this is some horrible code, I know
 
-Template.settings.generate_settings_form = function () {
-	Meteor.defer(function() {
-		$('#json-form').jsonForm({
-		  schema: {
-		    name: {
-		      type: 'string',
-		      title: 'Name',
-		      required: true
-		    },
-		    age: {
-		      type: 'number',
-		      title: 'Age'
-		    }
-		  },
-		  onSubmit: function (errors, values) {
-		    
-		  }
+var StringUtils = {
+	humanize: function(string) {
+		return this.capitalize(this.convertCamelToSpaces(string));
+	},
+	
+	capitalize: function(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	},
+	
+	convertCamelToSpaces: function(string) {
+		return string.replace(/([A-Z])/g, function(match) {
+			return ' ' + match;
 		});
+	}
+}
+
+var ModelForm = function (model, formOptions) {
+	this.model = model;
+	this.formOptions = formOptions;
+	
+	this.generate = function () {
+		var formSchema = {};
+		
+		for (var field in model) {
+			if (field != '_id') {
+				formSchema[field] = {			
+					type: (formOptions[field] && formOptions[field]['type']) || model[field].constructor.name.toLowerCase(),
+					title: (formOptions[field] && formOptions[field]['title']) || StringUtils.humanize(field),
+					id: field
+				}				
+			}
+		}
+		
+		$('#json-form').jsonForm({
+		  schema: formSchema
+		});
+	}	
+}
+
+Template.settings.generate_settings_form = function (setting) {
+	Meteor.defer(function() {
+		var options = {
+			'requireViewInvite': {
+				title: 'Require Invite to view?'
+			},
+			'requirePostInvite': {
+				title: 'Require Invite to post?'
+			},
+			'requirePostsApproval': {
+				title: 'Posts must be approved by admin?'
+			},
+			'title': {
+				title: 'Site Title'
+			}
+		};
+		
+		new ModelForm(setting, options).generate();
 	})
 }
 
