64530543b795ed27e88a8727a7fc40583c02e777
Sacha Greif <sacha357@gmail.com>
2012-11-18 08:46:50 +0900

    refactoring settings form code and removing redudant libraries

--- client/views/admin/settings.js
+++ client/views/admin/settings.js
@@ -1,15 +1,27 @@
 var settingsForm;
 
-Template.settings.generate_settings_form = function (setting) {
-	Meteor.defer(function() {
-		form().generateFor(setting);
-	})	
-}
-
 function form() {	
 	settingsForm = new ModelForm(Settings, settingsFormOptions());
 	return settingsForm;
 }
+
+Template.settings.helpers({
+	generateSettingsForm: function (setting) {
+		Meteor.defer(function() {
+			form().generateFor(setting);
+		})	
+	},
+	noSettings: function(){
+		if (Settings.findOne()) {
+			return false;
+		}
+		return true;
+	},
+	setting: function(){
+		var setting = Settings.find().fetch()[0];
+		return new Setting(setting) || new Setting();
+	}
+});
 
 Template.settings.events = {
 	'click input[type=submit]': function(e){
@@ -26,26 +38,3 @@ Template.settings.events = {
 		});
 	}
 };
-
-Template.settings.no_settings = function(){
-	if (Settings.findOne()) {
-		return false;
-	}
-	return true;
-}
-
-Template.settings.setting = function(){
-	var setting = Settings.find().fetch()[0];
-	return new Setting(setting) || new Setting();
-};
-
-Template.settings.is_theme = function(theme){
-	if(theme==this.setting.theme){
-		return true;
-	}
-	return false;
-};
-
-Template.settings.is_ascndr = function() { return this.theme=="ascndr" ? true : false; }
-Template.settings.is_telescope = function() { return this.theme=="telescope" ? true : false; }
-Template.settings.is_default = function() { return this.theme=="default" ? true : false; }
