19143689d07f1a4e2c051f07ab1237aea54d2558
Sacha Greif <sacha357@gmail.com>
2012-12-11 11:39:06 +0900

    deactivating goSquared for now

--- client/lib/analytics.js
+++ client/lib/analytics.js
@@ -5,16 +5,16 @@ var analyticsInit = function() {
   }
 
   // GoSquared
-  if (false && (goSquaredId = getSetting("goSquaredId"))) {
-    var GoSquared = {};
-    GoSquared.acct = goSquaredId;
-    window._gstc_lt = +new Date;
-    var d = document, g = d.createElement("script");
-    g.type = "text/javascript";
-    g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
-    var s = d.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(g, s);
-  }
+  // if (goSquaredId = getSetting("goSquaredId")) {
+  //   var GoSquared = {};
+  //   GoSquared.acct = goSquaredId;
+  //   window._gstc_lt = +new Date;
+  //   var d = document, g = d.createElement("script");
+  //   g.type = "text/javascript";
+  //   g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+  //   var s = d.getElementsByTagName("script")[0];
+  //   s.parentNode.insertBefore(g, s);
+  // }
 
   // Clicky
   if((clickyId=getSetting("clickyId"))){
