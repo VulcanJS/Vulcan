719165e30618ddc12186ed787c80d5ca694c39e0
Sacha Greif <sacha357@gmail.com>
2013-01-16 07:25:39 +0900

    added template name to content-wrapper class

--- client/body.js
+++ client/body.js
@@ -1,10 +1,16 @@
+Template.body.helpers({
+  pageName : function(){
+    return Meteor.Router.page();
+  }
+});
+
 Template.body.created = function(){
-	Session.set('currentScroll', null);
+  Session.set('currentScroll', null);
 }
 
 Template.body.rendered = function(){
     if(currentScroll=Session.get('currentScroll')){
       $('body').scrollTop(currentScroll);
       Session.set('currentScroll', null);
-    }  	
+    }   
 }
