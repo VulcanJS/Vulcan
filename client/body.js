d88b431784a003c8dbb5a08978893efa18d34ce7
Sacha Greif <sacha357@gmail.com>
2013-02-14 10:17:26 +0900

    make colors customizable

--- client/body.js
+++ client/body.js
@@ -1,7 +1,19 @@
 Template.body.helpers({
   pageName : function(){
     return Meteor.Router.page();
-  }
+  },
+  backgroundColor: function(){
+  	return getSetting('backgroundColor', '#e0f1f7');
+  },
+  secondaryColor: function(){
+  	return getSetting('secondaryColor', '#7ac0e4');
+  },
+  buttonColor: function(){
+  	return getSetting('buttonColor', '#f36c3d');
+  },
+  headerColor: function(){
+  	return getSetting('headerColor', '#4e555d');
+  },      
 });
 
 Template.body.created = function(){
