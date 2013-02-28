48899a36c0c1dc117b95ef76062681d780cec8da
Sacha Greif <sacha357@gmail.com>
2013-02-28 09:40:42 +0900

    added RSS feed for /new

--- server/rss.js
+++ server/rss.js
@@ -1,19 +1,17 @@
 // serve up RSS at the right url
 Meteor.serve('feed.xml', function() {
   var feed = new RSS({
-    title: GroundControlConfig.blogName,
-    description: GroundControlConfig.description
+    title: getSetting('title'),
+    description: getSetting('tagline')
   });
   
-  var converter = new Showdown.converter();
-  Posts.find().forEach(function(post) {
+  Posts.find({status: STATUS_APPROVED}, {sort: {submitted: -1}}).forEach(function(post) {
     feed.item({
-     title: post.title,
-     description: converter.makeHtml(post.body),
+     title: post.headline,
+     description: post.body+'</br></br> <a href="'+getPostUrl(post._id)+'">Comments</a>',
      author: post.author,
-     date: post.publishedAt,
-     // XXX: work this out properly
-     url: '/posts/' + post.slug
+     date: post.submitted,
+     url: (post.url ? post.url : getPostUrl(post._id))
     });
   });
   
