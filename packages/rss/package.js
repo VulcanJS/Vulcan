a9a26be0583bcb024a630d8f8c948cd405b54ca1
Sacha Greif <sacha357@gmail.com>
2013-09-02 10:40:15 +0900

    updated RSS package

--- packages/rss/package.js
+++ packages/rss/package.js
@@ -1,8 +1,10 @@
-Package.describe("RSS feed generator");
-
+Package.describe({
+  summary: "RSS feed generator"
+});
 Npm.depends({rss: '0.0.4'});
 
 Package.on_use(function (api) {
   api.add_files('rss.js', 'server');
-  
+  if(api.export)
+    api.export('RSS');
 });
