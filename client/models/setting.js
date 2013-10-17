68a83868d8f01eab8b27bac3e4ae1dcdab54827a
Sacha Greif <sacha357@gmail.com>
2013-10-17 10:30:57 +0800

    merging

--- client/models/setting.js
+++ client/models/setting.js
@@ -44,13 +44,9 @@ Setting = FormModel.extend({
     this.overwriteTitle('mixpanelId', '<a href="http://mixpanel.com/">Mixpanel</a> ID');
     this.overwriteTitle('clickyId', '<a href="http://getclicky.com/">Clicky</a> ID');
     this.overwriteTitle('goSquaredId', '<a href="http://gosquared.com/">GoSquared</a> ID');
-<<<<<<< HEAD
     // this.overwriteTitle('bitlyToken', '<a href="https://bitly.com/a/oauth_apps">Bitly</a> Token');
-=======
-    this.overwriteTitle('bitlyToken', '<a href="https://bitly.com/a/oauth_apps">Bitly</a> Token');
     this.overwriteTitle('mailChimpAPIKey', '<a href="http://mailchimp.com">MailChimp</a> API Key');
     this.overwriteTitle('mailChimpListId', '<a href="http://mailchimp.com">MailChimp</a> List ID');
->>>>>>> master
     this.overwriteTitle('logoUrl', 'Logo URL');
     this.overwriteType('footerCode', 'textarea');
     this.overwriteType('extraCode', 'textarea');
