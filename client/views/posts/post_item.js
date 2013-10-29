4988d914f5a23b94cba83f6b2d14e9b539e5c520
Sacha Greif <sacha357@gmail.com>
2013-10-29 16:04:24 +0900

    working on animations

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -81,49 +81,52 @@ Template.post_item.helpers({
 });
 
 var recalculatePosition = function ($object) {
-  console.log('recalculatePosition for '+$object)
-  var positionsArray = $object.data('positionsArray');
-
-  if(typeof positionsArray !== 'undefined'){
-    
-    var positionsLength = positionsArray.length,
-        top = $object.position().top
+  var pArray = $object.data('positionsArray'),
+      top = $object.position().top;
+      
+  if(typeof pArray !== 'undefined'){ 
 
     // if current position is different from the last position in the array, add current position
-    if(top != positionsArray[positionsLength-1]){
-      positionsArray.push(top);
-      $object.data('positionsArray', positionsArray);
+    if(top != pArray[pArray.length-1]){
+      pArray.push(top);
+      $object.data('positionsArray', pArray);
     }
 
-    console.log(positionsArray)
-
     // delta is the difference between the last two positions in the array
-    var delta = positionsArray[positionsLength-2] - positionsArray[positionsLength-1]
+    var delta = pArray[pArray.length-2] - pArray[pArray.length-1];
 
     // if new position is different from previous position
     if(delta != 0){
 
-      // console.log('previousPosition: '+previousPosition)
-      // console.log('newPosition: '+newPosition)
-      // console.log('delta: '+delta)
-
       // send object back to previous position
-      $object.css("top", delta + "px").addClass('animate');
+      $object.removeClass('animate').css("top", delta + "px").addClass('animate');
 
       // then wait a little and animate it to new one
       setTimeout(function() { 
         $object.css("top", "0px")
-      }, 1);
+      }, 1500);
     
     }
   }
 }
 
 Template.post_item.rendered = function(){
-  // when one post re-renders, force all of them to recalculate their position
-  $('.post').each(function(index, item){
-    recalculatePosition($(item));
-  });
+
+  var instance = this,
+      $instance = $(instance.firstNode.nextSibling),
+      top = $instance.position().top;
+
+  instance.renderCount = (typeof instance.renderCount === 'undefined') ? 1 : instance.renderCount+1;
+
+  if(instance.renderCount>1){
+    // when one post re-renders, force all of them to recalculate their position
+    $('.post').each(function(index, item){
+      recalculatePosition($(item));
+    });
+  }else{
+    $instance.data('positionsArray', [top]);
+  }
+
 };
 
 Template.post_item.events = {
