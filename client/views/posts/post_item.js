90b2c8c6e4913b9c8728e574e1bbd629a3d83483
Sacha Greif <sacha357@gmail.com>
2013-10-29 16:04:37 +0900

    disabling animations

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -80,54 +80,54 @@ Template.post_item.helpers({
   }
 });
 
-var recalculatePosition = function ($object) {
-  var pArray = $object.data('positionsArray'),
-      top = $object.position().top;
+// var recalculatePosition = function ($object) {
+//   var pArray = $object.data('positionsArray'),
+//       top = $object.position().top;
       
-  if(typeof pArray !== 'undefined'){ 
+//   if(typeof pArray !== 'undefined'){ 
 
-    // if current position is different from the last position in the array, add current position
-    if(top != pArray[pArray.length-1]){
-      pArray.push(top);
-      $object.data('positionsArray', pArray);
-    }
+//     // if current position is different from the last position in the array, add current position
+//     if(top != pArray[pArray.length-1]){
+//       pArray.push(top);
+//       $object.data('positionsArray', pArray);
+//     }
 
-    // delta is the difference between the last two positions in the array
-    var delta = pArray[pArray.length-2] - pArray[pArray.length-1];
+//     // delta is the difference between the last two positions in the array
+//     var delta = pArray[pArray.length-2] - pArray[pArray.length-1];
 
-    // if new position is different from previous position
-    if(delta != 0){
+//     // if new position is different from previous position
+//     if(delta != 0){
 
-      // send object back to previous position
-      $object.removeClass('animate').css("top", delta + "px").addClass('animate');
+//       // send object back to previous position
+//       $object.removeClass('animate').css("top", delta + "px").addClass('animate');
 
-      // then wait a little and animate it to new one
-      setTimeout(function() { 
-        $object.css("top", "0px")
-      }, 1500);
+//       // then wait a little and animate it to new one
+//       setTimeout(function() { 
+//         $object.css("top", "0px")
+//       }, 1500);
     
-    }
-  }
-}
+//     }
+//   }
+// }
 
-Template.post_item.rendered = function(){
+// Template.post_item.rendered = function(){
 
-  var instance = this,
-      $instance = $(instance.firstNode.nextSibling),
-      top = $instance.position().top;
+//   var instance = this,
+//       $instance = $(instance.firstNode.nextSibling),
+//       top = $instance.position().top;
 
-  instance.renderCount = (typeof instance.renderCount === 'undefined') ? 1 : instance.renderCount+1;
+//   instance.renderCount = (typeof instance.renderCount === 'undefined') ? 1 : instance.renderCount+1;
 
-  if(instance.renderCount>1){
-    // when one post re-renders, force all of them to recalculate their position
-    $('.post').each(function(index, item){
-      recalculatePosition($(item));
-    });
-  }else{
-    $instance.data('positionsArray', [top]);
-  }
+//   if(instance.renderCount>1){
+//     // when one post re-renders, force all of them to recalculate their position
+//     $('.post').each(function(index, item){
+//       recalculatePosition($(item));
+//     });
+//   }else{
+//     $instance.data('positionsArray', [top]);
+//   }
 
-};
+// };
 
 Template.post_item.events = {
   'click .upvote-link': function(e, instance){
