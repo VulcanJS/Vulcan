1b6072e114721b1ac07e3a2fca6e88a5729ce23c
Sacha Greif <sacha357@gmail.com>
2013-11-12 08:23:15 +0900

    fixing updateScore syntax

--- server/scoring.js
+++ server/scoring.js
@@ -9,10 +9,10 @@ Meteor.startup(function () {
       var updatedComments = 0;
       // console.log('tick ('+scoreInterval+')');
       Posts.find({'inactive': {$ne : true}}).forEach(function (post) {
-        updatedPosts += updateScore(Posts, post._id);
+        updatedPosts += updateScore(Posts, post);
       });
       Comments.find({'inactive': {$ne : true}}).forEach(function (comment) {
-        updatedComments += updateScore(Comments, comment._id);
+        updatedComments += updateScore(Comments, comment);
       });
       // console.log("Updated "+updatedPosts+"/"+Posts.find().count()+" Posts")
       // console.log("Updated "+updatedComments+"/"+Comments.find().count()+" Comments")
@@ -23,10 +23,10 @@ Meteor.startup(function () {
       var updatedPosts = 0;
       var updatedComments = 0;
       Posts.find({'inactive': true}).forEach(function (post) {
-        updatedPosts += updateScore(Posts, post._id);
+        updatedPosts += updateScore(Posts, post);
       });
       Comments.find({'inactive': true}).forEach(function (comment) {
-        updatedComments += updateScore(Comments, comment._id);
+        updatedComments += updateScore(Comments, comment);
       });
     }, 3600 * 1000);
 
