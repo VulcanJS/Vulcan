12c18e011973a5e0fd84cc2cfba28545120eb804
Ali Elouafiq <elouafiq@ali.ma>
2014-05-04 17:21:52 +0100

    adding Facebook Login
    and a fancy favicon

--- client/views/users/signin.js
+++ client/views/users/signin.js
@@ -20,5 +20,10 @@ Template.signin.events({
     Meteor.loginWithTwitter(function(){
       Router.go('/');
     });
-  }
+  },
+   'click .facebook-button': function(){
+        Meteor.loginWithFacebook(function(){
+            Router.go('/');
+        });
+    }
 });
