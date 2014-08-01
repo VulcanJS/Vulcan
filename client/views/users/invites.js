d2d4a0dd6b4591a76aee5671d751018a5b2a337d
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-08-01 13:16:40 +0200

    propagate invitation call results through throwError

--- client/views/users/invites.js
+++ client/views/users/invites.js
@@ -14,3 +14,31 @@ Template[getTemplate('invites')].helpers({
     return InviteSchema;
   }
 });
+
+var scrollUp = function(){
+  Deps.afterFlush(function() {
+    var element = $('.grid > .error');
+    $('html, body').animate({scrollTop: element.offset().top});
+  });
+};
+
+AutoForm.hooks({
+  inviteForm: {
+    onSuccess: function(operation, result, template) {
+      if(result && result.newUser){
+        throwError('Almost there. Please give your friend a sign up link to get in.');
+      } else {
+        throwError('Thank you!');
+      }
+
+      scrollUp();
+    },
+
+    onError: function(operation, error, template) {
+      if(error && error.reason){
+        throwError(error.reason);
+        scrollUp();
+      }
+    }
+  }
+});
