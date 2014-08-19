b121a2da2dea0cdf871685c31473ab54fcb3c838
Sacha Greif <sacha357@gmail.com>
2014-08-19 09:35:25 +0900

    fix categories bug

--- packages/telescope-tags/lib/client/views/categories.js
+++ packages/telescope-tags/lib/client/views/categories.js
@@ -2,6 +2,9 @@ Meteor.startup(function () {
   Template[getTemplate('categories')].helpers({
     categories: function(){
       return Categories.find({}, {sort: {order: 1, name: 1}});
+    },
+    categoryItem: function () {
+      return getTemplate('categoryItem');
     }
   });
 
