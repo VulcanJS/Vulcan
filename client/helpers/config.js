2e02af42c1a8b0256bab50e6e832f3273dce50e0
Sacha Greif <sacha357@gmail.com>
2014-09-16 12:19:09 +0900

    changing filename to force refresh

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -12,7 +12,7 @@ EpicEditorOptions={
 	theme: {
 		base:'/themes/base/epiceditor.css',
 		preview:'/themes/preview/github.css',
-		editor:'/themes/editor/epic-light.css'
+		editor:'/themes/editor/epic-light2.css'
 	}
 };
 
