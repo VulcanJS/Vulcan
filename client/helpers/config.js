f9390a301d23562367a6c37821443a0cb7f9a59d
Sacha Greif <sacha357@gmail.com>
2014-09-16 11:51:13 +0900

    various small css fixes

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -6,7 +6,9 @@ EpicEditorOptions={
 	container:  'editor',
 	basePath:   '/editor',
 	clientSideStorage: false,
-	autogrow: true,
+	autogrow: {
+		minHeight: 100
+	},
 	theme: {
 		base:'/themes/base/epiceditor.css',
 		preview:'/themes/preview/github.css',
