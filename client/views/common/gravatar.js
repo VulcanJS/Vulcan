6592f5761abf7d6d59c9bddb04ca21986a8779c5
shaialon <shai@totango.com>
2014-09-26 23:06:12 +0300

    Change to user / userId

--- client/views/common/gravatar.js
+++ client/views/common/gravatar.js
@@ -1,6 +1,14 @@
 Template[getTemplate('gravatar')].helpers({
   gravatarUrl: function () {
-    return getAvatarUrl(this.email);
+    if (this.user){
+        return getAvatarUrl(this.user);
+    }
+    else if (this.userId) {
+        var user=Meteor.users.findOne(this.userId);
+        if (user){
+            return getAvatarUrl(user);
+        }
+    }
   },
   cssClass: function(){
     if (this.class){
