0514e7166214d42a2b0ac3daf25724c6c3abda18
Ted Goldstein <tedgoldstein@gmail.com>
2014-09-27 09:08:05 -0700

    MedBook theme improvements.

--- packages/telescope-theme-medbook/package.js
+++ packages/telescope-theme-medbook/package.js
@@ -6,7 +6,9 @@ Package.on_use(function (api) {
 
   api.add_files([
     'lib/client/stylesheets/screen.css',
+    'lib/client/stylesheets/cbioquery.css',
     'lib/client/templates/new_posts_list.html',
+    'lib/client/templates/cbioquery.html',
     'lib/client/medbook.js',
     ], ['client']);
   
