a9d5d7a3b6affa05a366368ee70e881596c1b504
Kestanous <CMTopus@me.com>
2014-09-28 18:27:40 -0400

    Notifications: allow for multi user notifications

--- packages/notifications/lib/server.js
+++ packages/notifications/lib/server.js
@@ -4,42 +4,52 @@ Meteor.publish('notifications', function() {
 });
 
 //You can insert manually but this should save you some work.
-Notifications.createNotification = function(userId, params, callback) {
-  //TODO (possibility): allow for array of userIds or single userId, if array do multi insert
+Notifications.createNotification = function(userIds, params, callback) {
+  
 
+  check(userIds, Match.OneOf([String], String)); //TODO: better Collection ID check
+  check(params, Object);
   if (!Notifications.eventTypes[params.event])
-    throw new Error('Notification event type does not exists');
+    throw new Error('Notification: event type does not exists');
 
-  //TODO: sanity check input
+  // always assume multiple users.
+  if (_.isString(userIds)) 
+    userIds = [userIds]
 
-  //When creating a new notification
-  // 
-  // timestamp - you should timestamp every doc
-  // userId - there must be a user to notify
-  // event - this is the eventType, consider renaming
-  // properties - in database metadata, consider renaming
-  // read - default false, consider auto-delete?
-  // url - allow of iron:router magic. set read to true if visited (see routeSeenByUser)
+  users = Meteor.users.find({_id: {$in: userIds}}, {fields: {notifications: 1}})
 
-  var notification = {
-    timestamp: new Date().getTime(),
-    userId: userId,
-    event: params.event,
-    properties: params.properties,
-    read: false,
-    url: params.url
-  };
+  users.forEach(function (user) {
+    userId = user._id
 
-  //I figured I would make it work like an insert
-  //Given that this is really server only, do we really want to try/catch?
-  //TODO: remove try/catch unless someone says otherwise
-  var error, newNotificationId;
-  try { 
-    newNotificationId = Notifications.collection.insert(notification);
-  } catch (e) {
-    error = e;
-  } finally {
-    //TODO: check if callback is function
-    callback(error, newNotificationId);
-  }
+    //When creating a new notification
+    // 
+    // timestamp - you should timestamp every doc
+    // userId - there must be a user to notify
+    // event - this is the eventType, consider renaming
+    // properties - in database metadata, consider renaming
+    // read - default false, consider auto-delete?
+    // url - allow of iron:router magic. set read to true if visited (see routeSeenByUser)
+
+    var notification = {
+      timestamp: new Date().getTime(),
+      userId: userId,
+      event: params.event,
+      properties: params.properties,
+      read: false,
+      url: params.url
+    };
+
+    //I figured I would make it work like an insert
+    //Given that this is really server only, do we really want to try/catch?
+    //TODO: remove try/catch unless someone says otherwise
+    var error, newNotificationId;
+    try { 
+      newNotificationId = Notifications.collection.insert(notification);
+    } catch (e) {
+      error = e;
+    } finally {
+      //TODO: check if callback is function
+      callback(error, newNotificationId);
+    }
+  });
 };
