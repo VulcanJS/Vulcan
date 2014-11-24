30ca412921c28e5817cc1eb554d0591bac38039b
Anthony Mayer <adwmayer@gmail.com>
2014-11-23 21:11:15 -0800

    Compiling scss as part of build rather than with compass.
    Remove dependency on compass, add fourseven:scss (includes autoprefixer),
    add reset package, delete compiled css, and some random scss cleanup.

--- packages/telescope-theme-hubble/package.js
+++ packages/telescope-theme-hubble/package.js
@@ -2,14 +2,43 @@ Package.describe({summary: "Telescope Hubble theme"});
 
 Package.onUse(function (api) {
 
-  api.use(['telescope-lib', 'telescope-base'], ['client', 'server']);
+  api.use(
+    [
+      'telescope-lib',
+      'telescope-base',
+      'fourseven:scss'
+    ],
+    ['client', 'server']
+  );
 
-  api.add_files([
+  api.addFiles([
     'lib/hubble.js',
     ], ['client', 'server']);
 
-  api.add_files([
-    'lib/client/css/screen.css',
-    ], ['client']);
-  
+  api.addFiles(
+    [
+      // modules
+      'lib/client/scss/modules/_accounts.scss',
+      'lib/client/scss/modules/_comments.scss',
+      'lib/client/scss/modules/_dialogs.scss',
+      'lib/client/scss/modules/_errors.scss',
+      'lib/client/scss/modules/_nav.scss',
+      'lib/client/scss/modules/_posts.scss',
+      'lib/client/scss/modules/_share.scss',
+      'lib/client/scss/modules/_user-profile.scss',
+
+      // partials
+      'lib/client/scss/partials/_colors.scss',
+      'lib/client/scss/partials/_grid.scss',
+      'lib/client/scss/partials/_icons.scss',
+      'lib/client/scss/partials/_mixins.scss',
+      'lib/client/scss/partials/_tooltips.scss',
+      'lib/client/scss/partials/_typography.scss',
+
+      // screen
+      'lib/client/scss/screen.scss'
+    ],
+    'client'
+  );
+
 });
