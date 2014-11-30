aa25a75dc83e928b9991232b8b4eff07faf97054
Sacha Greif <sacha357@gmail.com>
2014-11-30 11:30:48 +0900

    adding sass and i18n to embedly module

--- packages/telescope-module-embedly/package.js
+++ packages/telescope-module-embedly/package.js
@@ -13,21 +13,32 @@ Package.onUse( function(api) {
     'telescope-lib', 
     'telescope-base',
     'aldeed:autoform',
+    'tap:i18n',
+    'fourseven:scss',
     'templating',
     'http'
   ]);
 
-  api.add_files(['lib/embedly.js'], ['client', 'server']);
+  api.add_files([
+    'package-tap.i18n',
+    'lib/embedly.js'
+  ], ['client', 'server']);
 
-  api.add_files(['lib/server/get_embedly_data.js'], ['server']);
+  api.add_files([
+    'lib/server/get_embedly_data.js'
+  ], ['server']);
 
   api.add_files([
     'lib/client/autoform-postthumbnail.html', 
     'lib/client/autoform-postthumbnail.js', 
     'lib/client/post_thumbnail.html', 
     'lib/client/post_thumbnail.js', 
-    'lib/client/post_thumbnail.css',
+    'lib/client/post_thumbnail.scss',
     'lib/client/post_video.html',
     'lib/client/post_video.js'
   ], ['client']);
+
+  api.add_files([
+    "i18n/en.i18n.json"
+  ], ["client", "server"]);
 });
