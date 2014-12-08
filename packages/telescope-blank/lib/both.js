ea1407bd52cc558ca90ee25e72ded5c47c9598c4
Sacha Greif <sacha357@gmail.com>
2014-12-08 10:28:56 +0900

    working on blank package; updated packages

--- packages/telescope-blank/lib/both.js
+++ packages/telescope-blank/lib/both.js
@@ -1,1 +1,14 @@
-both.js
+var customProperty = {
+  propertyName: 'customProperty',
+  propertySchema: {
+    type: String, // property type
+    label: 'customLabel', // key string used for internationalization
+    optional: true, // make this property optional
+    autoform: {
+      editable: true, // make this property editable by users
+      group: 'xyz', // assign a custom form group
+      type: "bootstrap-datetimepicker" // assign a custom input type
+    }
+  }
+}
+addToPostSchema.push(customProperty);
