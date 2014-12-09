e71be66c579d79136fdb383cddb399d0e7eedbab
Anthony Mayer <adwmayer@gmail.com>
2014-12-09 09:29:17 -0800

    Should fix various url problems by taking siteUrl into account when getting route urls.
    Trying to make as much use of path as possible on the client side.

--- client/views/nav/user_menu.js
+++ client/views/nav/user_menu.js
@@ -6,7 +6,7 @@ Template[getTemplate('userMenu')].helpers({
     return getDisplayName(Meteor.user());
   },
   profileUrl: function () {
-    return getProfileUrl(Meteor.user());
+    return Router.routes['user_profile'].path({_idOrSlug: Meteor.user().slug});
   },
   userEditUrl: function () {
     return Router.routes['user_edit'].path(Meteor.user());
