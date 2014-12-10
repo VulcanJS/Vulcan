ca8af2b7921c30845ac357b523ef862e23d34363
Sacha Greif <sacha357@gmail.com>
2014-12-10 09:35:42 +0900

    Fix version comparison function (#606)

--- packages/telescope-update-prompt/lib/client/update.js
+++ packages/telescope-update-prompt/lib/client/update.js
@@ -1,12 +1,22 @@
 compareVersions = function (v1, v2) { // return true if v2 is newer than v1
   var v1Array = v1.split('.');
   var v2Array = v2.split('.');
+  var isGreater = false;
   // go through each segment of v2 and stop if we find one that's higher
   // than the equivalent segment of v1; else return false
-  return v2Array.some( function (value, index) {
-    return parseInt(value) > parseInt(v1Array[index]);
+  v2Array.some( function (value, index) {
+    if (parseInt(value) > parseInt(v1Array[index])) {
+      // v2 segment > v1 segment
+      isGreater = true;
+      return true; // stop comparison
+    } else if (parseInt(value) < parseInt(v1Array[index])) {
+      // v2 segment < v1 segment
+      isGreater = false;
+      return true; // stop comparison
+    }
+    return false; // continue comparison as long as both values are equal
   });
-  return false;
+  return isGreater;
 }
 
 Meteor.startup(function () {
