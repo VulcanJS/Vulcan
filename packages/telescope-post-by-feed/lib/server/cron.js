3d8e6f923770dc0321758b74b8d297e6a3b925c1
Sacha Greif <sacha357@gmail.com>
2014-12-19 11:37:23 +0900

    updating autoform and simpleschema

--- packages/telescope-post-by-feed/lib/server/cron.js
+++ packages/telescope-post-by-feed/lib/server/cron.js
@@ -1,7 +1,7 @@
 SyncedCron.add({
   name: 'Post by RSS feed',
   schedule: function(parser) {
-    return parser.text('every 10 minutes');
+    return parser.text('every 30 minutes');
   }, 
   job: function() {
     fetchFeeds();
