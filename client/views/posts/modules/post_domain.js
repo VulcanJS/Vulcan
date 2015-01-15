6c5f2e0991a56bd97548432efe5aa9364d0f218c
yonglisc <yonglisc@gmail.com>
2015-01-14 16:10:47 -0800

    before add poll

--- client/views/posts/modules/post_domain.js
+++ client/views/posts/modules/post_domain.js
@@ -2,6 +2,15 @@ Template[getTemplate('postDomain')].helpers({
   domain: function(){
     var a = document.createElement('a');
     a.href = this.url;
+    if (a.hostname.indexOf('www.') !== -1) {
+    	return a.hostname.slice(4);
+    } 
     return a.hostname;
+  },
+  postLink: function(){
+    return !!this.url ? getOutgoingUrl(this.url) : "/posts/"+this._id;
+  },
+  postTarget: function() {
+    return !!this.url ? '_blank' : '';
   }
 });
--- client/views/posts/modules/post_title.js
+++ client/views/posts/modules/post_title.js
@@ -1,8 +1,8 @@
 Template[getTemplate('postTitle')].helpers({
   postLink: function(){
-    return !!this.url ? getOutgoingUrl(this.url) : "/posts/"+this._id;
+    return "/posts/"+this._id;
   },
   postTarget: function() {
-    return !!this.url ? '_blank' : '';
+    return '';
   }
 });
