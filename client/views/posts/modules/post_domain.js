23dfc552d7c86e2bb2b1805c04f280829365e96e
yonglisc <yonglisc@gmail.com>
2015-01-19 15:06:33 -0800

    fixed empty read on

--- client/views/posts/modules/post_domain.js
+++ client/views/posts/modules/post_domain.js
@@ -5,6 +5,9 @@ Template[getTemplate('postDomain')].helpers({
     if (a.hostname.indexOf('www.') !== -1) {
     	return a.hostname.slice(4);
     } 
+    if (a.hostname.indexOf('localhost') !== -1 || a.hostname.indexOf('127.0.0.1') !== -1 ) {
+      return false;
+    }
     return a.hostname;
   },
   postLink: function(){
