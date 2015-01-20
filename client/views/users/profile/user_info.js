aa838da7536f407731b2e4691e513931b40abb10
yonglisc <yonglisc@gmail.com>
2015-01-20 08:25:01 -0800

    added user opinion & contribution statistics

--- client/views/users/profile/user_info.js
+++ client/views/users/profile/user_info.js
@@ -18,6 +18,42 @@ Template[getTemplate('userInfo')].helpers({
   },
   getGitHubName: function () {
     return getGitHubName(this);
+  },
+  sameWonder: function () {
+    if (_.isUndefined(this.votes.pollvotedPosts)) {
+      return 0;
+    }
+    var other = this.votes.pollvotedPosts,
+        self = Meteor.user().votes.pollvotedPosts;
+    var common = _.pluck(self.concat(other), 'itemId');
+    return Math.round( (_.union(common).length / common.length) * 100 );
+  },
+  sharedOpinion: function () {
+    if (_.isUndefined(this.votes.pollvotedPosts)) {
+      return 0;
+    }
+    var other = this.votes.pollvotedPosts,
+        self = Meteor.user().votes.pollvotedPosts,
+        common = self.concat(other),
+        itemIds = _.pluck(self.concat(other), 'itemId'),
+        sameIds = _.union(itemIds),
+        cleanCommon = [];
+
+    for (var i=0, l=common.length; i<l; i++) {
+      if ( _.contains(sameIds,common[i].itemId) && !_.isUndefined(common[i].voteOrder) ) {
+        cleanCommon.push(common[i].itemId+"-"+common[i].voteOrder);
+      }
+    } 
+
+    if (_.isEmpty(cleanCommon)) {
+      return 0;
+    }
+
+    return Math.round( (_.union(cleanCommon).length / (sameIds.length / 2) ) * 100 );
+
+  },
+  notSelf: function () {
+    return Meteor.user()._id !== this._id;
   }
 });
 
