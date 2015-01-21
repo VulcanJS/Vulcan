93f3985e7ba713df25ef7787ca831d291f4f1020
yonglisc <yonglisc@gmail.com>
2015-01-21 01:09:08 -0800

    update host url

--- client/views/posts/modules/post_comments_link.js
+++ client/views/posts/modules/post_comments_link.js
@@ -1,6 +1,6 @@
 Template[getTemplate('postCommentsLink')].events({
 	'click .comments-link': function () {
-  		var url= 'http://localhost:3000/posts/'+this._id;
+  		var url= 'http://wondercount.com/posts/'+this._id;
   		var iframe = '<iframe id="post-modal" src="'+url+'" width="100%" height="100%" scrolling="auto" frameborder="0" seamless></iframe>'
 	    $('.post-modal-content').html(iframe);
 	    $('.ui.post.modal')
--- client/views/posts/modules/post_title.js
+++ client/views/posts/modules/post_title.js
@@ -9,7 +9,7 @@ Template[getTemplate('postTitle')].helpers({
 
 Template[getTemplate('postTitle')].events({
   	'click .post-title': function(url){
-  		var url= 'http://localhost:3000/posts/'+this._id;
+  		var url= 'http://wondercount.com/posts/'+this._id;
   		var iframe = '<iframe id="post-modal" src="'+url+'" width="100%" height="100%" scrolling="auto" frameborder="0" seamless></iframe>'
 	    $('.post-modal-content').html(iframe);
 	    $('.ui.post.modal')
