a0ebc73cf7f5c0176ca935343d1892aa726ed933
Sacha Greif <sacha357@gmail.com>
2015-01-22 11:58:14 +0900

    fix bug preventing posting comments

--- collections/comments.js
+++ collections/comments.js
@@ -1,72 +1,83 @@
 CommentSchemaObject = {
   _id: {
-      type: String,
-      optional: true
+    type: String,
+    optional: true
   },
   parentCommentId: {
-      type: String,
-      optional: true
+    type: String,
+    optional: true,
+    autoform: {
+      editable: true,
+      omit: true
+    }
   },
   createdAt: {
-      type: Date,
-      optional: true
+    type: Date,
+    optional: true
   },
   postedAt: { // for now, comments are always created and posted at the same time
-      type: Date,
-      optional: true
+    type: Date,
+    optional: true
   },
   body: {
-      type: String
+    type: String,
+    autoform: {
+      editable: true
+    }
   },
   htmlBody: {
-      type: String,
-      optional: true
+    type: String,
+    optional: true
   },
   baseScore: {
-      type: Number,
-      decimal: true,
-      optional: true
+    type: Number,
+    decimal: true,
+    optional: true
   },
   score: {
-      type: Number,
-      decimal: true,
-      optional: true
+    type: Number,
+    decimal: true,
+    optional: true
   },
   upvotes: {
-      type: Number,
-      optional: true
+    type: Number,
+    optional: true
   },
   upvoters: {
-      type: [String], // XXX
-      optional: true
+    type: [String], // XXX
+    optional: true
   },
   downvotes: {
       type: Number,
       optional: true
   },
   downvoters: {
-      type: [String], // XXX
-      optional: true
+    type: [String], // XXX
+    optional: true
   },
   author: {
-      type: String,
-      optional: true
+    type: String,
+    optional: true
   },
   inactive: {
-      type: Boolean,
-      optional: true
+    type: Boolean,
+    optional: true
   },
   postId: {
-      type: String, // XXX
-      optional: true
+    type: String, // XXX
+    optional: true,
+    autoform: {
+      editable: true,
+      omit: true
+    }
   },
   userId: {
-      type: String, // XXX
-      optional: true
+    type: String, // XXX
+    optional: true
   },
   isDeleted: {
-      type: Boolean,
-      optional: true
+    type: Boolean,
+    optional: true
   }
 };
 
