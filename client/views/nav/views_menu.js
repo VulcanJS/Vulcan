4b0e1de6bb1fab24c2c3116b3a1a23eb32c7edfa
yonglisc <yonglisc@gmail.com>
2015-01-29 21:34:44 -0800

    Add friends alike button

--- client/views/nav/views_menu.js
+++ client/views/nav/views_menu.js
@@ -6,12 +6,18 @@ Template[getTemplate('viewsMenu')].helpers({
     return viewsMenu;
   },
   friendsUrl: function () {
+  	if (!Meteor.user()) {
+  		return '';
+  	}
     return Router.path('user_friends', {slug: Meteor.user().slug});
   }
 });
 
 Template[getTemplate('viewsMenu')].events({
   'click .view-friend-link': function () {
+  	if (!Meteor.user()) {
+  		return Meteor.loginWithFacebook({requestPermissions: ['email', 'public_profile', 'user_friends']});
+  	}
   	var user = Meteor.user();
     Meteor.call('updateFriendsWonders', user, function (error, result) {
     	if (error) {
