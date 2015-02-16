467fc494f22c73a7cc463e729a2be5bff5b65e3e
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-02-16 17:57:05 -0500

    switch to "this" instead of "instance.data"

--- client/views/users/list/users_list_actions.js
+++ client/views/users/list/users_list_actions.js
@@ -8,29 +8,29 @@ Template[getTemplate('users_list_actions')].helpers({
 });
 
 Template[getTemplate('users_list_actions')].events({
-  'click .invite-link': function(e, instance){
+  'click .invite-link': function(e){
     e.preventDefault();
-    Meteor.call('inviteUser', { userId : instance.data._id });
+    Meteor.call('inviteUser', { userId : this._id });
   },
-  'click .uninvite-link': function(e, instance){
+  'click .uninvite-link': function(e){
     e.preventDefault();
-    Meteor.users.update(instance.data._id,{
+    Meteor.users.update(this._id,{
       $set:{
         isInvited: false
       }
     });
   },
-  'click .admin-link': function(e, instance){
+  'click .admin-link': function(e){
     e.preventDefault();
-    updateAdmin(instance.data._id, true);
+    updateAdmin(this._id, true);
   },
-  'click .unadmin-link': function(e, instance){
+  'click .unadmin-link': function(e){
     e.preventDefault();
-    updateAdmin(instance.data._id, false);
+    updateAdmin(this._id, false);
   },
-  'click .delete-link': function(e, instance){
+  'click .delete-link': function(e){
     e.preventDefault();
-    if(confirm(i18n.t("are_you_sure_you_want_to_delete")+getDisplayName(instance.data)+"?"))
-      Meteor.users.remove(instance.data._id);
+    if(confirm(i18n.t("are_you_sure_you_want_to_delete")+getDisplayName(this)+"?"))
+      Meteor.users.remove(this._id);
   }
 });
