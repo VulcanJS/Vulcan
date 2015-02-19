29a9fff750bafff3bc2b44891f2420c9d5fd13f3
Massimiliano Marini <massimiliano.marini@gmail.com>
2015-02-19 19:43:44 +0100

    telescope-users package

--- client/views/users/user_profile.js
+++
@@ -1,8 +0,0 @@
-Template[getTemplate('user_profile')].helpers({
-  userProfileDisplay: function () {
-    return userProfileDisplay;
-  },
-  getTemplate: function () {
-    return getTemplate(this.template);
-  }
-});
--- server/publications/users_dashboard.js
+++
@@ -1,9 +0,0 @@
-// Publish all users
-
-Meteor.publish('allUsers', function(filterBy, sortBy, limit) {
-  if(isAdminById(this.userId)){
-    var parameters = getUsersParameters(filterBy, sortBy, limit);
-    return Meteor.users.find(parameters.find, parameters.options);
-  }
-  return [];
-});
