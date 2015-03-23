f59ad553b69688459b1b064a0c9a23beac13fb53
Sacha Greif <sacha357@gmail.com>
2015-03-23 11:35:49 +0900

    close dropdown when clicking "mark all as read"

--- packages/telescope-notifications/lib/client/templates/notifications_mark_as_read.js
+++ packages/telescope-notifications/lib/client/templates/notifications_mark_as_read.js
@@ -1,5 +1,7 @@
 Template[getTemplate('notificationsMarkAsRead')].events({
-  'click .mark-as-read': function(){
+  'click .mark-as-read': function(e, t){
+    e.preventDefault();
+    t.$('li').parents('.dropdown').removeClass('dropdown-open');
     Meteor.call('heraldMarkAllAsRead', 
       function(error, result){
         error && console.log(error);
