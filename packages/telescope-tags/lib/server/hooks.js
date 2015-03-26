db49d9746c0779e0b0a757107a584043da869861
Sacha Greif <sacha357@gmail.com>
2015-03-26 18:27:14 +0900

    add http:// to image links in newsletter template

--- packages/telescope-tags/lib/server/hooks.js
+++ packages/telescope-tags/lib/server/hooks.js
@@ -1,8 +1,12 @@
-var checkCategories = function(post) {
+// make sure all categories in the post.categories array exist in the db
+var checkCategories = function (post) {
+
+  // if there are not categories, stop here
   if (!post.categories || post.categories.length === 0) {
     return;
   }
 
+  // check how many of the categories given also exist in the db
   var categoryCount = Categories.find({_id: {$in: post.categories}}).count();
 
   if (post.categories.length !== categoryCount) {
