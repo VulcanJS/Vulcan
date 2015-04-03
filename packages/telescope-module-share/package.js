c1d0d6f6f776008a1f10424aaaa91c3ffac11dac
Sacha Greif <sacha357@gmail.com>
2015-04-03 18:52:23 +0900

    clean up

--- packages/telescope-module-share/package.js
+++ packages/telescope-module-share/package.js
@@ -1,18 +1,32 @@
-Package.describe({summary: "Telescope share module package"});
+Package.describe({
+  version: '0.1.0',
+  name: "telescope-module-share",
+  summary: "Telescope share module package"
+});
 
 Package.onUse(function (api) {
-
-  api.use(['telescope-lib', 'telescope-base', 'fourseven:scss'], ['client', 'server']);
 
   api.use([
     'jquery',
     'underscore',
-    'templating'
+    'templating',
+    'fourseven:scss'
   ], 'client');
 
-  api.add_files(['lib/share.js'], ['client', 'server']);
+  api.use([
+    'telescope-lib', 
+    'telescope-base', 
+  ]);
 
-  api.add_files(['lib/client/post_share.html', 'lib/client/post_share.js', 'lib/client/post_share.scss'], ['client']);
+  api.add_files([
+    'lib/share.js'
+  ], ['client', 'server']);
+
+  api.add_files([
+    'lib/client/post_share.html', 
+    'lib/client/post_share.js', 
+    'lib/client/post_share.scss'
+  ], ['client']);
   
   // api.export();
 });
