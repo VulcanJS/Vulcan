b7d38982ff0e4b0b42e843741f51de52ef6a7b48
Sacha Greif <sacha357@gmail.com>
2015-04-08 19:36:41 +0900

    don't display pages menu if there are no pages

--- packages/telescope-pages/lib/client/templates/pages_menu.js
+++ packages/telescope-pages/lib/client/templates/pages_menu.js
@@ -1,4 +1,7 @@
 Template[getTemplate("pagesMenu")].helpers({
+  hasPages: function () {
+    return Pages.collection.find().count()
+  },
   pages: function () {
     return Pages.collection.find();
   }
