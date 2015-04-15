ce8b12661985fc4d2b4c90a3310585483226c151
Neil Lobo <neil@sharethis.com>
2015-04-15 11:34:09 -0700

    added categories in code

--- server/start.js
+++ server/start.js
@@ -3,7 +3,53 @@ Meteor.startup(function () {
     name: "firstRun",
     unique: true, // will only get logged a single time
     important: true
-  })
+  });
+
+  // Categories = new Mongo.Collection('categories');
+  Categories.remove({});
+
+  Categories.insert({'name' : 'Arts & Entertainment',
+  'description' : 'Movies, Music, Celbs',
+  'order' : 1,
+  'slug' : 'arts-entertainment',
+  'image' : null
+  });
+
+  Categories.insert({'name' : 'Health & Fitness',
+  'description' : 'Get Lean. Get Pumped',
+  'order' : 2,
+  'slug' : 'health-fitness',
+  'image' : null
+  });
+
+  Categories.insert({'name' : 'Games',
+  'description' : 'Game away',
+  'order' : 1,
+  'slug' : 'games',
+  'image' : null
+  });
+
+  Categories.insert({'name' : 'News',
+  'description' : 'World. Political. Serious Stuff',
+  'order' : 1,
+  'slug' : 'news',
+  'image' : null
+  });
+
+  Categories.insert({'name' : 'Sports',
+  'description' : 'Bro Time Sports',
+  'order' : 1,
+  'slug' : 'sports',
+  'image' : null
+  });
+
+  Categories.insert({'name' : 'Travel',
+  'description' : 'Go Places',
+  'order' : 8,
+  'slug' : 'travel',
+  'image' : null
+  });
+
 });
 
 if (Settings.get('mailUrl'))
