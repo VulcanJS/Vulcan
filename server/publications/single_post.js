97f603888513818fa31e2e47183bec568a25eab3
Delgermurun <delgermurun.p@nmtec.co>
2015-04-20 22:21:40 +0900

    check post existence before access on postUsers publication
    Close #915

--- server/publications/single_post.js
+++ server/publications/single_post.js
@@ -12,32 +12,32 @@ Meteor.publish('singlePost', function(id) {
 Meteor.publish('postUsers', function(postId) {
   if (can.viewById(this.userId)){
     // publish post author and post commenters
-    var post = Posts.findOne(postId),
-        users = [post.userId]; // publish post author's ID
+    var post = Posts.findOne(postId);
+    if (!post) {
+      return [];
+    }
 
-    if (post) {
+    var users = [post.userId]; // publish post author's ID
 
-      // get IDs from all commenters on the post
-      var comments = Comments.find({postId: post._id}).fetch();
-      if (comments.length) {
-        users = users.concat(_.pluck(comments, "userId"));
-      }
+    // get IDs from all commenters on the post
+    var comments = Comments.find({postId: post._id}).fetch();
+    if (comments.length) {
+      users = users.concat(_.pluck(comments, "userId"));
+    }
 
-      // publish upvoters
-      if (post.upvoters && post.upvoters.length) {
-        users = users.concat(post.upvoters);
-      }
+    // publish upvoters
+    if (post.upvoters && post.upvoters.length) {
+      users = users.concat(post.upvoters);
+    }
 
-      // publish downvoters
-      if (post.downvoters && post.downvoters.length) {
-        users = users.concat(post.downvoters);
-      }
-
+    // publish downvoters
+    if (post.downvoters && post.downvoters.length) {
+      users = users.concat(post.downvoters);
     }
 
     // remove any duplicate IDs
     users = _.unique(users);
-    
+
     return Meteor.users.find({_id: {$in: users}}, {fields: privacyOptions});
   }
   return [];
