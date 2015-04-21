f8a817aa5c7561d1804f480ef566e14ed69141ef
Sacha Greif <SachaG@users.noreply.github.com>
2015-04-21 18:25:35 +0900

    Only migrate users that do have a profile

--- server/migrations.js
+++ server/migrations.js
@@ -456,7 +456,7 @@ var migrationsList = {
   },
   updateUserNames: function () {
     var i = 0;
-    var allUsers = Meteor.users.find({username: {$exists: true}, 'profile.isDummy': {$ne: true}});
+    var allUsers = Meteor.users.find({username: {$exists: true}, profile: {$exists: true}, 'profile.isDummy': {$ne: true}});
 
     console.log('> Found '+allUsers.count()+' users.\n');
 
