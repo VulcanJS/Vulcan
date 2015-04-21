50da57e059ea89a98bf7bfd11d0f9eadd47026f7
Sacha Greif <sacha357@gmail.com>
2015-04-22 07:50:26 +0900

    creating comments package

---
+++ packages/telescope-comments/package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  name: "telescope:comments",
+  summary: "Telescope comments package",
+  version: "0.1.0",
+  git: "https://github.com/TelescopeJS/Telescope.git"
+});
+
+Package.onUse(function (api) {
+
+  api.versionsFrom(['METEOR@1.0']);
+
+  api.use([
+    'jquery',
+    'underscore',
+    'mongo',
+    'aldeed:simple-schema@1.3.2',
+    'telescope:lib@0.3.0',
+    'telescope:users@0.1.0'
+  ]);
+
+  api.add_files([
+    'lib/comments.js',
+  ], ['client', 'server']);
+
+  api.add_files([
+    'lib/server/publications.js',
+  ], ['server']);
+
+  api.export('Posts');
+
+});
