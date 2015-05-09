2f9f6b9ec30100910bbc4802dd7ec1635135674e
Capensis <info@capensis.biz>
2015-05-09 16:01:40 +0100

    fixed scroll top and saved message
    i18n string !!

--- packages/telescope-settings/lib/client/templates/settings_form.js
+++ packages/telescope-settings/lib/client/templates/settings_form.js
@@ -1,10 +1,17 @@
 AutoForm.addHooks(['updateSettingsForm', 'insertSettingsForm'], {
     onSuccess: function(operation, result) {
       this.template.$('button[type=submit]').removeClass('loading');
+      Messages.flash(i18n.t('Settings Saved'), 'success');//i18n string !!
+      Messages.clearSeen();
+      $('body').scrollTop(0);
+      $('.success-message.module').fadeOut( 5000,"swing" );
     },
 
     onError: function(operation, result, template) {
       this.template.$('button[type=submit]').removeClass('loading');
+      Messages.flash(i18n.t('You have an error please correct the fields'), 'error');//i18n string !!
+      Messages.clearSeen();
+      $('.error.error-message.module').fadeOut( 5000,"swing" );
     }
 });
 
