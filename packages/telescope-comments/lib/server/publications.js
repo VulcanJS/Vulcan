3b2c9574965ff46175190eb4b91ee516f79c7de8
Sacha Greif <sacha357@gmail.com>
2015-05-21 15:51:19 +0900

    make sure comment exists

--- packages/telescope-comments/lib/server/publications.js
+++ packages/telescope-comments/lib/server/publications.js
@@ -49,16 +49,18 @@ Meteor.publish('commentUsers', function(commentId) {
   if(Users.can.viewById(this.userId)){
 
     var comment = Comments.findOne(commentId);
+
     if (!!comment) {
       userIds.push(comment.userId);
-    }
 
-    var post = Posts.findOne(comment.postId);
-    if (!!post) {
-      userIds.push(post.userId);
-    }
+      var post = Posts.findOne(comment.postId);
+      if (!!post) {
+        userIds.push(post.userId);
+      }
 
-    return Meteor.users.find({_id: {$in: userIds}}, {fields: Users.pubsub.publicProperties});
+      return Meteor.users.find({_id: {$in: userIds}}, {fields: Users.pubsub.publicProperties});
+    
+    }
 
   }
 
