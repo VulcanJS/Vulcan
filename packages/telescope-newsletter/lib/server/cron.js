a34c3b70b226d731c0b731e56479b6ebfb5880af
John Pinkerton <jpinkerton88@gmail.com>
2015-06-23 22:40:43 -0400

    Add option for each day of week for newsletter. Resolves #1034

--- packages/telescope-newsletter/lib/server/cron.js
+++ packages/telescope-newsletter/lib/server/cron.js
@@ -12,30 +12,16 @@ var getSchedule = function (parser) {
   var frequency = Settings.get('newsletterFrequency', defaultFrequency);
   var recur = parser.recur();
   var schedule;
-  switch (frequency) {
-    case 1: // every day
-      // sched = {schedules: [{dw: [1,2,3,4,5,6,0]}]};
-      schedule = recur.on(1,2,3,4,5,6,0).dayOfWeek();
-      break;
 
-    case 2: // Mondays, Wednesdays, Fridays
-      // sched = {schedules: [{dw: [2,4,6]}]};
-      schedule = recur.on(2,4,6).dayOfWeek();
-      break;
 
-    case 3: // Mondays, Thursdays
-      // sched = {schedules: [{dw: [2,5]}]};
-      schedule = recur.on(2,5).dayOfWeek();
-      break;
-
-    case 7: // Once a week (Mondays)
-      // sched = {schedules: [{dw: [2]}]};
-      schedule = recur.on(2).dayOfWeek();
-      break;
-
-    default: // Once a week (Mondays)
-      schedule = recur.on(2).dayOfWeek();
+  // Default is once a week (Mondays)
+  if (!!frequency) {
+    schedule = recur.on(frequency).dayOfWeek();
   }
+  else {
+    schedule = recur.on(2).dayOfWeek();
+  }
+
   return schedule.on(Settings.get('newsletterTime', defaultTime)).time();
 };
 
