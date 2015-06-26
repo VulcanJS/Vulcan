ff7649fbb1c3d85407d788afa65dd7b3d2862afa
John Pinkerton <jpinkerton88@gmail.com>
2015-06-26 15:33:56 -0400

    add migration for newsletter frequency

--- packages/telescope-migrations/lib/server/migrations.js
+++ packages/telescope-migrations/lib/server/migrations.js
@@ -694,6 +694,29 @@ var migrationsList = {
       console.log("---------------------");
     });
     return i;
+  },
+  updateNewsletterFrequency: function () {
+    var i = 0;
+    Settings.find().forEach(function (setting) {
+      i++;
+      var days;
+      switch (setting.newsletterFrequency) {
+        case 1:
+          days = [1,2,3,4,5,6,7];
+          break;
+        case 2:
+          days = [2,4,6];
+          break;
+        case 3:
+          days = [2,5];
+          break;
+        default:
+          days = [2];
+          break;
+      }
+      Settings.update(setting._id, { $set: { newsletterFrequency: days } });
+    });
+    return i;
   }
 };
 
