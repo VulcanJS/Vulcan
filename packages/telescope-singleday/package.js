1058d49956da2a3fe61736af9b62ffe3ef2015d1
Sacha Greif <sacha357@gmail.com>
2015-07-07 16:40:30 +0900

    load translations for daily and single day package

--- packages/telescope-singleday/package.js
+++ packages/telescope-singleday/package.js
@@ -49,15 +49,23 @@ Package.onUse(function (api) {
   // i18n languages (must come last)
 
   api.addFiles([
-    'i18n/bg.i18n.json',
-    'i18n/de.i18n.json',
-    'i18n/nl.i18n.json',
-    'i18n/en.i18n.json',
-    'i18n/es.i18n.json',
-    'i18n/fr.i18n.json',
-    'i18n/it.i18n.json',
-    'i18n/tr.i18n.json',
-    'i18n/zh-CN.i18n.json'
-  ], ['client', 'server']);
+    "i18n/ar.i18n.json",
+    "i18n/bg.i18n.json",
+    "i18n/de.i18n.json",
+    "i18n/el.i18n.json",
+    "i18n/en.i18n.json",
+    "i18n/es.i18n.json",
+    "i18n/fr.i18n.json",
+    "i18n/it.i18n.json",
+    "i18n/nl.i18n.json",
+    "i18n/pl.i18n.json",
+    "i18n/pt-BR.i18n.json",
+    "i18n/ro.i18n.json",
+    "i18n/ru.i18n.json",
+    "i18n/sv.i18n.json",
+    "i18n/tr.i18n.json",
+    "i18n/vi.i18n.json",
+    "i18n/zh-CN.i18n.json"
+  ], ["client", "server"]);
 
 });
