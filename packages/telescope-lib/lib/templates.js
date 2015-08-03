d57ded5933b20e272c50ad26c3e1577fbdcd09b4
Sacha Greif <sacha357@gmail.com>
2015-08-03 17:40:29 +0900

    fix typo

--- packages/telescope-lib/lib/templates.js
+++ packages/telescope-lib/lib/templates.js
@@ -1,4 +1,4 @@
-Telescope.config.customPrefixes = ["custom"];
+Telescope.config.customPrefixes = ["custom_"];
 
 Telescope.config.addCustomPrefix = function (prefix) {
   Telescope.config.customPrefixes.push(prefix);
