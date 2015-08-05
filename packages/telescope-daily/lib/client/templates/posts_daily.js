586125243b7700b079c91921ffcd66c8f815d5f3
Sacha Greif <sacha357@gmail.com>
2015-08-05 14:05:53 +0900

    use template reactive variables for day count to fix autoscroll bug

--- packages/telescope-daily/lib/client/templates/posts_daily.js
+++ packages/telescope-daily/lib/client/templates/posts_daily.js
@@ -1,33 +1,45 @@
-Meteor.startup(function () {
+Template.posts_daily.onCreated(function () {
+  var instance = this;
+  instance.daysCount = new ReactiveVar(instance.data.daysCount);
+});
 
-  Template.posts_daily.helpers({
-    days: function () {
-      var daysArray = [];
-      for (var i = 0; i < this.daysCount; i++) {
-        daysArray.push({
-          date: moment().subtract(i, 'days').startOf('day').toDate(),
-          index: i
-        });
-      }
-      return daysArray;
-    },
-    context: function () {
-      var days = Template.parentData(1);
-      var context = {
-        terms: {
-          view: "singleday",
-          date: this.date,
-          after: moment(this.date).startOf('day').toDate(),
-          before: moment(this.date).endOf('day').toDate(),
-          enableCache: days.daysCount <= 15 ? true : false // only cache first 15 days
-        }
-      };
-      return context;
-    },
-    loadMoreDaysUrl: function () {
-      var count = parseInt(this.daysCount) + daysPerPage;
-      return '/daily/' + count;
+Template.posts_daily.helpers({
+  days: function () {
+    var instance = Template.instance();
+    var daysCount = instance.daysCount.get();
+
+    var daysArray = [];
+    for (var i = 0; i < daysCount; i++) {
+      daysArray.push({
+        date: moment().subtract(i, 'days').startOf('day').toDate(),
+        index: i
+      });
     }
-  });
+    return daysArray;
+  },
+  context: function () {
+    var instance = Template.instance();
+    var daysCount = instance.daysCount.get();
 
+    // var days = Template.parentData(1);
+    var context = {
+      terms: {
+        view: "singleday",
+        date: this.date,
+        after: moment(this.date).startOf('day').toDate(),
+        before: moment(this.date).endOf('day').toDate(),
+        enableCache: daysCount <= 15 ? true : false // only cache first 15 days
+      }
+    };
+    return context;
+  },
+  loadMoreHandler: function () {
+    var instance = Template.instance();
+    var daysCount = instance.daysCount.get();
+
+    return function () {
+      var newCount = daysCount + daysPerPage;
+      instance.daysCount.set(newCount);
+    };
+  }
 });
