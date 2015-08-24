aea9c327f9b23cadaa2f1325c69c6e4aae9416ad
Sacha Greif <sacha357@gmail.com>
2015-08-24 12:26:20 +0900

    Get link source name and URL from Embedly if available

--- packages/telescope-embedly/lib/embedly.js
+++ packages/telescope-embedly/lib/embedly.js
@@ -1,68 +1,78 @@
-var thumbnailProperty = {
-  fieldName: 'thumbnailUrl',
-  fieldSchema: {
-    type: String,
-    optional: true,
-    editableBy: ["member", "admin"],
-    autoform: {
-      type: 'bootstrap-postthumbnail'
+Posts.addField([
+  {
+    fieldName: 'thumbnailUrl',
+    fieldSchema: {
+      type: String,
+      optional: true,
+      editableBy: ["member", "admin"],
+      autoform: {
+        type: 'bootstrap-postthumbnail'
+      }
+    }
+  },
+  {
+    fieldName: 'media',
+    fieldSchema: {
+      type: Object,
+      optional: true,
+      blackbox: true
+    }
+  },
+  {
+    fieldName: 'sourceName',
+    fieldSchema: {
+      type: String,
+      optional: true,
+    }
+  },
+  {
+    fieldName: 'sourceUrl',
+    fieldSchema: {
+      type: String,
+      optional: true,
     }
   }
-};
-Posts.addField(thumbnailProperty);
-
-var mediaProperty = {
-  fieldName: 'media',
-  fieldSchema: {
-    type: Object,
-    optional: true,
-    blackbox: true
-  }
-};
-Posts.addField(mediaProperty);
+]);
 
 Telescope.modules.add("postThumbnail", {
   template: 'post_thumbnail',
   order: 15
 });
 
-var embedlyKeyProperty = {
-  fieldName: 'embedlyKey',
-  fieldSchema: {
-    type: String,
-    optional: true,
-    private: true,
-    autoform: {
-      group: 'embedly',
-      class: 'private-field'
+Settings.addField([
+  {
+    fieldName: 'embedlyKey',
+    fieldSchema: {
+      type: String,
+      optional: true,
+      private: true,
+      autoform: {
+        group: 'embedly',
+        class: 'private-field'
+      }
+    }
+  },
+  {
+    fieldName: 'thumbnailWidth',
+    fieldSchema: {
+      type: Number,
+      optional: true,
+      autoform: {
+        group: 'embedly'
+      }
+    }
+  },
+  {
+    fieldName: 'thumbnailHeight',
+    fieldSchema: {
+      type: Number,
+      optional: true,
+      autoform: {
+        group: 'embedly'
+      }
     }
   }
-};
-Settings.addField(embedlyKeyProperty);
-
-var thumbnailWidthProperty = {
-  fieldName: 'thumbnailWidth',
-  fieldSchema: {
-    type: Number,
-    optional: true,
-    autoform: {
-      group: 'embedly'
-    }
-  }
-};
-Settings.addField(thumbnailWidthProperty);
-
-var thumbnailHeightProperty = {
-  fieldName: 'thumbnailHeight',
-  fieldSchema: {
-    type: Number,
-    optional: true,
-    autoform: {
-      group: 'embedly'
-    }
-  }
-};
-Settings.addField(thumbnailHeightProperty);
+]);
 
 function addThumbnailClass (postClass, post) {
   var thumbnailClass = !!post.thumbnailUrl ? "has-thumbnail" : "no-thumbnail";
--- packages/telescope-embedly/lib/server/get_embedly_data.js
+++ packages/telescope-embedly/lib/server/get_embedly_data.js
@@ -28,7 +28,15 @@ getEmbedlyData = function (url) {
     if (!!result.data.images && !!result.data.images.length) // there may not always be an image
       result.data.thumbnailUrl = result.data.images[0].url.replace("http:", ""); // add thumbnailUrl as its own property and remove "http"
 
-    return _.pick(result.data, 'title', 'media', 'description', 'thumbnailUrl');
+    if (result.data.authors && result.data.authors.length > 0) {
+      result.data.sourceName = result.data.authors[0].name;
+      result.data.sourceUrl = result.data.authors[0].url;
+    }
+
+    var embedlyData = _.pick(result.data, 'title', 'media', 'description', 'thumbnailUrl', 'sourceName', 'sourceUrl');
+    console.log(embedlyData);
+
+    return embedlyData;
 
   } catch (error) {
     console.log(error)
@@ -39,8 +47,8 @@ getEmbedlyData = function (url) {
   }
 }
 
-// For security reason, we use a separate server-side API call to set the media object,
-// and the thumbnail object if it hasn't already been set
+// For security reason, we make the media property non-modifiable by the client and
+// we use a separate server-side API call to set it (and the thumbnail object if it hasn't already been set)
 
 // Async variant that directly modifies the post object with update()
 function addMediaAfterSubmit (post) {
@@ -50,13 +58,16 @@ function addMediaAfterSubmit (post) {
     if (!!data) {
       // only add a thumbnailUrl if there isn't one already
       if (!post.thumbnailUrl && !!data.thumbnailUrl) {
-        post.thumbnailUrl = data.thumbnailUrl;
         set.thumbnailUrl = data.thumbnailUrl;
       }
       // add media if necessary
       if (!!data.media.html) {
-        post.media = data.media;
         set.media = data.media;
+      }
+      // add source name & url if they exist
+      if (!!data.sourceName && !!data.sourceUrl) {
+        set.sourceName = data.sourceName;
+        set.sourceUrl = data.sourceUrl;
       }
     }
     // make sure set object is not empty (Embedly call could have failed)
@@ -64,16 +75,23 @@ function addMediaAfterSubmit (post) {
       Posts.update(post._id, {$set: set});
     }
   }
-  return post;
-};
+}
 Telescope.callbacks.add("postSubmitAsync", addMediaAfterSubmit);
 
 function updateMediaOnEdit (modifier, post) {
   var newUrl = modifier.$set.url;
   if(newUrl && newUrl !== post.url){
     var data = getEmbedlyData(newUrl);
-    if(!!data && !!data.media.html) {
-      modifier.$set.media = data.media;
+    if(!!data) {
+      if (!!data.media.html) {
+        modifier.$set.media = data.media;
+      }
+
+      // add source name & url if they exist
+      if (!!data.sourceName && !!data.sourceUrl) {
+        modifier.$set.sourceName = data.sourceName;
+        modifier.$set.sourceUrl = data.sourceUrl;
+      }
     }
   }
   return modifier;
