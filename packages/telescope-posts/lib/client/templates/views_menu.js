c4c6b09701ca970eb2f31da435898d185ebe93ae
Sacha Greif <sacha357@gmail.com>
2015-10-06 16:35:42 +0900

    small layout tweaks

--- packages/telescope-posts/lib/client/templates/views_menu.js
+++ packages/telescope-posts/lib/client/templates/views_menu.js
@@ -7,6 +7,11 @@ var getMenuItems = function () {
     return (item.adminOnly && !Users.is.admin(Meteor.user())) || (!!Settings.get('postViews') && !_.contains(Settings.get('postViews'), item.label));
   });
 
+  viewableItems = _.map(viewableItems, function (item) {
+    item.itemClass = "view-"+item.label;
+    return item;
+  });
+  console.log(viewableItems)
   return viewableItems; 
 };
 
