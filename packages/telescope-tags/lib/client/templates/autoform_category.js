8903efc672f01f29f3310410051ae82a296d8fc5
Sacha Greif <sacha357@gmail.com>
2015-10-20 11:56:02 +0900

    fix embody profiling issue and make categories prefillable

--- packages/telescope-tags/lib/client/templates/autoform_category.js
+++ packages/telescope-tags/lib/client/templates/autoform_category.js
@@ -11,9 +11,14 @@ AutoForm.addInputType("bootstrap-category", {
 
 Template.afCategory_bootstrap3.helpers({
   menuItems: function () {
-    var selectedCategories = this.value;
+
+    var selectedCategoriesIds = this.value;
+    var prefilledCategoriesIds = _.pluck(Session.get("prefilledCategories"), "_id");
+
+    selectedCategoriesIds = _.compact(prefilledCategoriesIds.concat(selectedCategoriesIds));
+
     var menuItems = _.map(Categories.find({}, {sort: {order: 1, name: 1}}).fetch(), function (category) {
-      var isSelected = _.contains(selectedCategories, category._id);
+      var isSelected = _.contains(selectedCategoriesIds, category._id);
       return {
         _id: category._id,
         parentId: category.parentId,
@@ -33,6 +38,10 @@ Template.afCategory_bootstrap3.helpers({
     atts = AutoForm.Utility.addClass(atts, "form-control");
     return atts;
   }
+});
+
+Template.afCategory_bootstrap3.onDestroyed(function () {
+  Session.set("prefilledCategories", null);
 });
 
 Template.afCategory_bootstrap3.events({
