1a344dbb6470c26cf07afb5fee9205f1e2bc27dc
Sacha Greif <sacha357@gmail.com>
2015-11-23 17:10:34 +0900

    fix Prerender https issue (#1202) (thanks @capensisma!)

--- packages/telescope-prerender/lib/server/prerender.js
+++ packages/telescope-prerender/lib/server/prerender.js
@@ -1,9 +1,12 @@
-if (Settings.get("prerenderIOToken")) {
-  var siteUrl = Settings.get("siteUrl") || Meteor.absoluteUrl();
-  var prerender = Npm.require('prerender-node')
-    .set('protocol', 'http')
-    .set('host', siteUrl.replace("http://", "").replace("/", ""))
-    .set('prerenderToken', Settings.get("prerenderIOToken"));
+if (Settings.get('prerenderIOToken')) {
+  
+  var siteUrl = Settings.get('siteUrl') || Meteor.absoluteUrl();
+  var protocol = siteUrl.indexOf('https') !== -1 ? 'https' : 'http'
+
+  var prerender = Npm.require('')
+    .set('protocol', protocol)
+    .set('host', siteUrl.replace('http://', '').replace('/', ''))
+    .set('prerenderToken', Settings.get('prerenderIOToken'));
 
   Meteor.startup(function() {
     WebApp.rawConnectHandlers.use(prerender);
