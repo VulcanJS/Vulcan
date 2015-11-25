2788f6366b528b95d9438f9288c0d23cbf4cc26e
Sacha Greif <sacha357@gmail.com>
2015-11-25 11:17:37 +0900

    fix typo

--- packages/telescope-prerender/lib/server/prerender.js
+++ packages/telescope-prerender/lib/server/prerender.js
@@ -3,7 +3,7 @@ if (Settings.get('prerenderIOToken')) {
   var siteUrl = Settings.get('siteUrl') || Meteor.absoluteUrl();
   var protocol = siteUrl.indexOf('https') !== -1 ? 'https' : 'http'
 
-  var prerender = Npm.require('')
+  var prerender = Npm.require('prerender-node')
     .set('protocol', protocol)
     .set('host', siteUrl.replace('http://', '').replace('/', ''))
     .set('prerenderToken', Settings.get('prerenderIOToken'));
