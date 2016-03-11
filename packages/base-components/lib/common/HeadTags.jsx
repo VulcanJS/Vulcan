a9f49f147ac764fd3bc65efc7828d28fb1b1b8e5
xavizalote <xavier.cazalot@gmail.com>
2016-03-11 18:07:57 +0100

    avoid confusion! -> no need of another layer (settings obj)

--- packages/base-components/lib/common/HeadTags.jsx
+++ packages/base-components/lib/common/HeadTags.jsx
@@ -1,36 +1,38 @@
 import Helmet from 'react-helmet';
 
-const settings = {
-	logoUrl: Telescope.settings.get("logoUrl", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/SN1994D.jpg/800px-SN1994D.jpg"), // supernova picture
-	siteTitle: Telescope.settings.get("title", "Telescope"),
-	tagline: Telescope.settings.get("tagline", "Telescope, the Nova way"),
-	favicon: Telescope.settings.get("favicon", "http://www.telescopeapp.org/images/favicon.ico")
-};
+const HeadTags = () => {
 
-const HeadTags = () => (
-	<Helmet
-		title={settings.siteTitle}
-		base={{href: Meteor.absoluteUrl()}}
-		meta={[
-			{charset: "utf-8"},
-			{name: "description", content: settings.tagline},
-			// facebook
-			{property: "og:type", content: "article"},
-			{property: "og:url", content: Telescope.utils.getSiteUrl()},
-			{property: "og:image", content: settings.logoUrl},
-			{property: "og:title", content: settings.siteTitle},
-			{property: "og:description", content: settings.tagline},
-			//twitter
-			{name: "twitter:card", content: "summary"},
-			{name: "twitter:image:src", content: settings.logoUrl},
-			{name: "twitter:title", content: settings.siteTitle},
-			{name: "twitter:description", content: settings.tagline}
-		]}
-		link={[
-			{rel: "canonical", href: Telescope.utils.getSiteUrl()},
-			{rel: "shortcut icon", href: settings.favicon}
-		]}
-	/>
-);
+	const	logoUrl = Telescope.get("logoUrl", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/SN1994D.jpg/800px-SN1994D.jpg"); // supernova picture
+	const siteTitle = Telescope.get("title", "Telescope");
+	const	tagline = Telescope.get("tagline");
+	const favicon = Telescope.get("favicon", "/img/favicon.ico");
+
+	return (
+		<Helmet
+			title={siteTitle}
+			base={{href: Meteor.absoluteUrl()}}
+			meta={[
+				{charset: "utf-8"},
+				{name: "description", content: tagline},
+				// facebook
+				{property: "og:type", content: "article"},
+				{property: "og:url", content: Telescope.utils.getSiteUrl()},
+				{property: "og:image", content: logoUrl},
+				{property: "og:title", content: siteTitle},
+				{property: "og:description", content: tagline},
+				//twitter
+				{name: "twitter:card", content: "summary"},
+				{name: "twitter:image:src", content: logoUrl},
+				{name: "twitter:title", content: siteTitle},
+				{name: "twitter:description", content: tagline}
+			]}
+			link={[
+				{rel: "canonical", href: Telescope.utils.getSiteUrl()},
+				{rel: "shortcut icon", href: favicon}
+			]}
+		/>
+	);
+	
+};
 
 module.exports = HeadTags;
