84fba4af129fa0b77e603e211fa590593bfb227f
Sacha Greif <sacha357@gmail.com>
2016-03-27 22:25:32 +0900

    handle case where there are no categories

--- packages/base-components/lib/categories/list/CategoriesList.jsx
+++ packages/base-components/lib/categories/list/CategoriesList.jsx
@@ -7,24 +7,28 @@ const CategoriesList = ({categories}, context) => {
   const currentRoute = context.currentRoute;
   const currentCategory = currentRoute.queryParams.cat;
   
-  return (
-    <div className="categories">
-      <DropdownButton bsStyle="default" className="btn-secondary" title="Categories" id="categories-dropdown">
-        <MenuItem href={Router.path("posts.list")} eventKey={0} className="dropdown-item post-category">All Categories</MenuItem>
-        {categories.map((category, index) => 
-          <MenuItem 
-            href={Router.extendPathWithQueryParams("posts.list", {}, {cat: category.slug})} 
-            eventKey={index+1} 
-            key={category._id} 
-            className={currentCategory === category.slug ? "post-category-active dropdown-item post-category" : "dropdown-item post-category"} 
-          >
-            {currentCategory === category.slug ? <Icon name="voted"/> :  null}
-            {category.name}
-          </MenuItem>
-        )}
-      </DropdownButton>
-    </div>
-  )
+  if (categories && categories.length > 0) {
+    return (
+      <div className="categories">
+        <DropdownButton bsStyle="default" className="btn-secondary" title="Categories" id="categories-dropdown">
+          <MenuItem href={Router.path("posts.list")} eventKey={0} className="dropdown-item post-category">All Categories</MenuItem>
+          {categories.map((category, index) => 
+            <MenuItem 
+              href={Router.extendPathWithQueryParams("posts.list", {}, {cat: category.slug})} 
+              eventKey={index+1} 
+              key={category._id} 
+              className={currentCategory === category.slug ? "post-category-active dropdown-item post-category" : "dropdown-item post-category"} 
+            >
+              {currentCategory === category.slug ? <Icon name="voted"/> :  null}
+              {category.name}
+            </MenuItem>
+          )}
+        </DropdownButton>
+      </div>
+    )
+  } else {
+    return null
+  }
   
 };
 
