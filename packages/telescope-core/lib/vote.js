a1cacf5b879127c3bdaff7291ed15c9a64f075fd
Shingo <shingo0620@gmail.com>
2016-04-13 10:32:19 +0800

    Fix exception while invoking method 'upvotePost'
    Fix typo in Telescope.operateOnItem

--- packages/telescope-core/lib/vote.js
+++ packages/telescope-core/lib/vote.js
@@ -37,7 +37,7 @@ Telescope.operateOnItem = function (collection, itemId, user, operation) {
     case "upvote":
 
       if (hasDownvotedItem) {
-        Telescope.operateOnItem(collection, itemd, user, "cancelDownvote");
+        Telescope.operateOnItem(collection, itemId, user, "cancelDownvote");
       }
       update = {
         $addToSet: {upvoters: user._id},
