2e835d389e7fbe029f97b90833bd1c1cdeceb703
xavizalote <xavier.cazalot@gmail.com>
2016-04-15 12:00:53 +0200

    add a log out button to close modal ;)

--- packages/nova-base-components/lib/users/UserProfileCheck.jsx
+++ packages/nova-base-components/lib/users/UserProfileCheck.jsx
@@ -1,12 +1,12 @@
 import React, { PropTypes, Component } from 'react';
-import { Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
 import NovaForm from "meteor/nova:forms";
 
 const UserProfileCheck = ({ currentUser }) => {
   return currentUser && !Users.hasCompletedProfile(currentUser) ?
     (<Modal bsSize='large' show={ true }>
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title>Complete your Profile</Modal.Title>
       </Modal.Header>
       <Modal.Body>
@@ -15,11 +15,12 @@ const UserProfileCheck = ({ currentUser }) => {
           collection={ Meteor.users }
           document={ currentUser }
           methodName="users.edit"
-          labelFunction={(fieldName)=>Telescope.utils.getFieldLabel(fieldName, Meteor.users)}
+          labelFunction={ (fieldName)=>Telescope.utils.getFieldLabel(fieldName, Meteor.users) }
           successCallback={ (user)=> Telescope.callbacks.runAsync("profileCompletedAsync", user) }
           requiredFieldsOnly={ true }
         />
       </Modal.Body>
+      <Button bsStyle="primary" className="complete-profile-logout" onClick={ () => Meteor.logout() }>Log Out</Button>
     </Modal>)
     : (null);
 };
