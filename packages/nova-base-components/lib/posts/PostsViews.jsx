f2416eaf6345b0ad59c6aefd98fa698c0833223d
Sacha Greif <sacha357@gmail.com>
2016-04-18 12:16:07 +0900

    working on mobile version

--- packages/nova-base-components/lib/posts/PostsViews.jsx
+++ packages/nova-base-components/lib/posts/PostsViews.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import Router from '../router.js';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Button, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
 
 const PostsViews = (props, context) => {
 
@@ -17,10 +17,21 @@ const PostsViews = (props, context) => {
   
   return (
     <div className="post-views">
+      {/*
       <ButtonGroup>
         {views.map(view => <Button className={currentRoute.route.name === "posts.list" && currentView === view ? "post-view-active" : "post-view-inactive"} bsStyle="default" key={view} href={Router.extendPathWithQueryParams("posts.list", {}, {view: view})}>{Telescope.utils.capitalise(view)}</Button>)}
       </ButtonGroup>
       <Button bsStyle="default" href={Router.path("posts.daily")} className={currentRoute.route.name === "posts.daily" ? "post-view-active" : "post-view-inactive"} >Daily</Button>
+      */}
+      <DropdownButton 
+        bsStyle="default" 
+        className="views btn-secondary" 
+        title="View" 
+        id="views-dropdown"
+      >
+        {views.map(view => <MenuItem key={view} href={Router.extendPathWithQueryParams("posts.list", {}, {view: view})} className={currentRoute.route.name === "posts.list" && currentView === view ? "dropdown-item post-view-active" : "dropdown-item post-view-inactive"}>{Telescope.utils.capitalise(view)}</MenuItem>)}
+        <MenuItem href={Router.path("posts.daily")} className={currentRoute.route.name === "posts.daily" ? "dropdown-item post-view-active" : "dropdown-item post-view-inactive"} >Daily</MenuItem>
+      </DropdownButton>
     </div>
   )
 }
