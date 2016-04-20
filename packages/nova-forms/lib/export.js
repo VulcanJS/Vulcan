31959ae2877db32af29725822bb5245b858dd844
Sacha Greif <sacha357@gmail.com>
2016-04-20 09:23:11 +0900

    update formsy and get rid of warnings (yay!)

--- packages/nova-forms/lib/export.js
+++ packages/nova-forms/lib/export.js
@@ -1,9 +1,8 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
-  "react": "^0.14.6",
-  "formsy-react": "^0.17.0",
-  "formsy-react-components": "^0.6.6",
-  "react-bootstrap": "^0.28.3"
+  "formsy-react": "^0.18.0",
+  "formsy-react-components": "^0.7.1",
+  "react-bootstrap": "^0.29.0"
   // 'rebass': '^0.2.4',
 });
 
