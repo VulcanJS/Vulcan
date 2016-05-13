96729e31f397c93ba9ff4f2dc50ac7f6afb6fb2a
Sacha Greif <sacha357@gmail.com>
2016-05-13 16:25:04 +0900

    show user's posts on their profile page

--- packages/nova-base-components/lib/users/UsersProfile.jsx
+++ packages/nova-base-components/lib/users/UsersProfile.jsx
@@ -1,10 +1,15 @@
 import React, { PropTypes, Component } from 'react';
+import SmartContainers from "meteor/utilities:react-list-container";
+const ListContainer = SmartContainers.ListContainer;
 
 const UsersProfile = ({user, currentUser}) => {
 
-  ({HeadTags} = Telescope.components);
+  ({HeadTags, PostsList} = Telescope.components);
 
   const twitterName = Users.getTwitterName(user);
+
+  const terms = {view:"userPosts", userId: user._id};
+  const {selector, options} = Posts.parameters.get(terms);
 
   return (
     <div className="page users-profile">
@@ -15,6 +20,18 @@ const UsersProfile = ({user, currentUser}) => {
         {twitterName ? <li><a href={"http://twitter.com/" + twitterName}>@{twitterName}</a></li> : null }
         {user.telescope.website ? <li><a href={user.telescope.website}>{user.telescope.website}</a></li> : null }
       </ul>
+      <h3>Posts</h3>
+      <ListContainer
+        collection={Posts}
+        publication="posts.list"
+        terms={terms}
+        selector={selector}
+        options={options}
+        joins={Posts.getJoins()}
+        cacheSubscription={false}
+        component={PostsList}
+        componentProps={{showHeader: false}}
+      />
     </div>
   )
 }
