68311f5e89060d25673b507b14e3a1d6bff43ef0
Sacha Greif <sacha357@gmail.com>
2016-05-22 14:36:40 +0900

    tweak category loading code

--- packages/nova-categories/lib/server/load_categories.js
+++ packages/nova-categories/lib/server/load_categories.js
@@ -1,22 +1,22 @@
 // Load categories from settings, if there are any
 
-// Loading at startup allows inserting categories with extended schema
-Meteor.startup(() => {
-  if (Meteor.settings && Meteor.settings.categories) {
-    Meteor.settings.categories.forEach(category => {
-  
-      // look for existing category with same slug
-      let existingCategory = Categories.findOne({slug: category.slug});
-  
-      if (existingCategory) {
-        // if category exists, update it with settings data except slug
-        delete category.slug;
-        Categories.update(existingCategory._id, {$set: category});
-      } else {
-        // if not, create it
-        Categories.insert(category);
-        console.log(`// Creating category “${category.name}”`);
-      }
-    });
-  }
-});
+if (Meteor.settings && Meteor.settings.categories) {
+  Meteor.settings.categories.forEach(category => {
+
+    // get slug (or slugified name)
+    const slug = category.slug || Telescope.utils.slugify(category.name);
+
+    // look for existing category with same slug
+    let existingCategory = Categories.findOne({slug: slug});
+
+    if (existingCategory) {
+      // if category exists, update it with settings data except slug
+      delete category.slug;
+      Categories.update(existingCategory._id, {$set: category});
+    } else {
+      // if not, create it
+      Categories.insert(category);
+      console.log(`// Creating category “${category.name}”`);
+    }
+  });
+}
