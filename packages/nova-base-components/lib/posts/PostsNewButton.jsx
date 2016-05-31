6051ddb68e64bfe84eb82c969e258ac49391da26
Sacha Greif <sacha357@gmail.com>
2016-05-31 10:33:50 +0900

    adapt new post dialog size based on logged in status

--- packages/nova-base-components/lib/posts/PostsNewButton.jsx
+++ packages/nova-base-components/lib/posts/PostsNewButton.jsx
@@ -4,8 +4,10 @@ import { ModalTrigger } from "meteor/nova:core";
 
 const PostsNewButton = (props, context) => {
 
+  const size = context.currentUser ? "small" : "large";
+
   return (
-    <ModalTrigger title="New Post" component={<Button className="posts-new-button" bsStyle="primary">New Post</Button>}>
+    <ModalTrigger size={size} title="New Post" component={<Button className="posts-new-button" bsStyle="primary">New Post</Button>}>
       <Telescope.components.PostsNewForm/>
     </ModalTrigger>
   )
