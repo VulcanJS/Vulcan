12bee733cfa448ef8923712d821287cadf64d10f
xavizalote <xavier.cazalot@gmail.com>
2016-06-14 23:20:39 +0200

    fix mailchimp method name in callback

--- packages/nova-newsletter/lib/callbacks.js
+++ packages/nova-newsletter/lib/callbacks.js
@@ -1,6 +1,6 @@
 function subscribeUserOnProfileCompletion (user) {
   if (!!Telescope.settings.get('autoSubscribe') && !!Users.getEmail(user)) {
-    addToMailChimpList(user, false, function (error, result) {
+    MailChimpList.add(user, false, function (error, result) {
       console.log(error);
       console.log(result);
     });
