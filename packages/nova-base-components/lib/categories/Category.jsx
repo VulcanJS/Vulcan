44d40fd33cdd1b4a90aaf7d9583184287907fd2d
Sacha Greif <sacha357@gmail.com>
2016-06-14 17:06:23 +0900

    remove outdated import

--- packages/nova-base-components/lib/categories/Category.jsx
+++ packages/nova-base-components/lib/categories/Category.jsx
@@ -1,5 +1,4 @@
 import React, { PropTypes, Component } from 'react';
-import Actions from "../actions.js"
 import { Button, DropdownButton, MenuItem } from 'react-bootstrap';
 import classNames from "classnames";
 import { Messages, ModalTrigger } from 'meteor/nova:core';
