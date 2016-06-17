c262b68cc71632e07b4357ea16cd16e8b756ce11
Sacha Greif <sacha357@gmail.com>
2016-06-17 14:44:53 +0900

    make PostsHome easier to extend

--- packages/nova-base-components/lib/posts/PostsHome.jsx
+++ packages/nova-base-components/lib/posts/PostsHome.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import { ListContainer, DocumentContainer } from "meteor/utilities:react-list-container";
 
-const PostsHome = (props, context) => {
+class PostsHome extends Component {
 
-  const defaultView = {view: 'top'};
-  const params = {...defaultView, ...props.location.query, listId: "posts.list.main"};
-  const {selector, options} = Posts.parameters.get(params);
+  getDefaultView() {
+    return {view: 'top'}
+  }
+  
+  render() {
 
-  return (
-    <ListContainer 
-      collection={Posts} 
-      publication="posts.list"
-      selector={selector}
-      options={options}
-      terms={params} 
-      joins={Posts.getJoins()}
-      component={Telescope.components.PostsList}
-      cacheSubscription={false}
-      listId={params.listId}
-      limit={Telescope.settings.get("postsPerPage", 10)}
-    />
-  )
+    const params = {...this.getDefaultView(), ...this.props.location.query, listId: "posts.list.main"};
+    const {selector, options} = Posts.parameters.get(params);
 
+    return (
+      <ListContainer 
+        collection={Posts} 
+        publication="posts.list"
+        selector={selector}
+        options={options}
+        terms={params} 
+        joins={Posts.getJoins()}
+        component={Telescope.components.PostsList}
+        cacheSubscription={false}
+        listId={params.listId}
+        limit={Telescope.settings.get("postsPerPage", 10)}
+      />
+    )
+  }
 };
-
-PostsHome.displayName = "PostsHome";
 
 module.exports = PostsHome;
