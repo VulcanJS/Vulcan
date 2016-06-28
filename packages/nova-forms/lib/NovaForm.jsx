fdd020793d7a95e99bbb764a209956dbe4fb1c12
sweetppro <rgray@sweetpproductions.com>
2016-06-28 09:56:20 +0200

    add post._id as a childContextType for access by custom forms

--- packages/nova-forms/lib/NovaForm.jsx
+++ packages/nova-forms/lib/NovaForm.jsx
@@ -246,7 +246,8 @@ class NovaForm extends Component{
       throwError: this.throwError,
       autofilledValues: this.state.autofilledValues,
       addToAutofilledValues: this.addToAutofilledValues,
-      updateCurrentValue: this.updateCurrentValue
+      updateCurrentValue: this.updateCurrentValue,
+      _id: this.getDocument()._id
     };
   }
 
@@ -389,7 +390,8 @@ NovaForm.childContextTypes = {
   autofilledValues: React.PropTypes.object,
   addToAutofilledValues: React.PropTypes.func,
   updateCurrentValue: React.PropTypes.func,
-  throwError: React.PropTypes.func
+  throwError: React.PropTypes.func,
+  _id: React.PropTypes.string
 }
 
 module.exports = NovaForm;
