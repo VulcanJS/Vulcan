a944fc7fcf1bf3056484ef02031af36006d413db
Sacha Greif <sacha357@gmail.com>
2016-07-12 17:34:59 +0900

    get rid of duplicate stylesheets

--- packages/nova-base-styles/package.js
+++ packages/nova-base-styles/package.js
@@ -19,23 +19,6 @@ Package.onUse(function (api) {
 
   api.addFiles([
     'lib/stylesheets/bootstrap.css',
-
-    // 'lib/stylesheets/solid.1.4.4.css',
-    'lib/stylesheets/_breakpoints.scss',
-    'lib/stylesheets/_colors.scss',
-    'lib/stylesheets/_variables.scss',
-    'lib/stylesheets/_global.scss',
-    'lib/stylesheets/_accounts.scss',
-    'lib/stylesheets/_categories.scss',
-    'lib/stylesheets/_cheatsheet.scss',
-    'lib/stylesheets/_comments.scss',
-    'lib/stylesheets/_header.scss',
-    'lib/stylesheets/_forms.scss',
-    'lib/stylesheets/_other.scss',
-    'lib/stylesheets/_posts.scss',
-    'lib/stylesheets/_newsletter.scss',
-    'lib/stylesheets/_spinner.scss',
-    'lib/stylesheets/_users.scss',
     'lib/stylesheets/main.scss'
   ], ['client']);
 
