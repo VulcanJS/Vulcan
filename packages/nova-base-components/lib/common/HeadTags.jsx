1ef77c8c04df79ef607b1367b9b3360b44f89df0
xavizalote <xavier.cazalot@gmail.com>
2016-07-18 18:01:41 +0200

    meta: avoid repetition (no more concat,  should fix Facebook scrape) + favicon setting name

--- packages/nova-base-components/lib/common/HeadTags.jsx
+++ packages/nova-base-components/lib/common/HeadTags.jsx
@@ -21,7 +21,7 @@ class HeadTags extends Component {
 			image = Telescope.utils.getSiteUrl() + image;
 		}
 
-		Telescope.headtags.meta = Telescope.headtags.meta.concat([
+		Telescope.headtags.meta = [
 			{ charset: "utf-8" },
 			{ name: "description", content: description },
 			// responsive
@@ -37,12 +37,12 @@ class HeadTags extends Component {
 			{ name: "twitter:image:src", content: image },
 			{ name: "twitter:title", content: title },
 			{ name: "twitter:description", content: description }
-		]);
+		];
 
-		Telescope.headtags.link = Telescope.headtags.link.concat([
+		Telescope.headtags.link = [
 			{ rel: "canonical", href: Telescope.utils.getSiteUrl() },
-			{ rel: "shortcut icon", href: Telescope.settings.get("favicon", "/img/favicon.ico") }
-		]);
+			{ rel: "shortcut icon", href: Telescope.settings.get("faviconUrl", "/img/favicon.ico") }
+		];
 
 		return (
 			<div>
