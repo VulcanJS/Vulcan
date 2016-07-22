b9f839797a4680e74a795777d1b0b206690228a9
Danny Vaughton <danny.vaughton@gmail.com>
2016-07-22 10:14:36 +0700

    Added category sorting, by order

--- packages/nova-categories/lib/server/publications.js
+++ packages/nova-categories/lib/server/publications.js
@@ -3,12 +3,12 @@ import Users from 'meteor/nova:users';
 import Categories from "../collection.js";
 
 Meteor.publish('categories', function() {
-  
+
   const currentUser = this.userId && Users.findOne(this.userId);
 
   if(Users.canDo(currentUser, "posts.view.approved.all")){
-    
-    var categories = Categories.find();
+
+    var categories = Categories.find({$query:{}, $orderby:{ "order": 1 }});
     var publication = this;
 
     categories.forEach(function (category) {
