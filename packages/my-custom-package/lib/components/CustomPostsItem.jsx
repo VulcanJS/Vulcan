54c1c1764323a2a2eb2565686f33724f76b6cfe3
schabluk <lukasz.schabek@gmail.com>
2016-07-27 10:42:25 +0200

    add observe button

--- packages/my-custom-package/lib/components/CustomPostsItem.jsx
+++ packages/my-custom-package/lib/components/CustomPostsItem.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import { FormattedMessage, FormattedRelative } from 'react-intl';
-import { Button } from 'react-bootstrap';
+import { Button, Tooltip } from 'react-bootstrap';
 import moment from 'moment';
 import { ModalTrigger } from "meteor/nova:core";
 import { Link } from 'react-router';
@@ -9,11 +9,64 @@ import Categories from "meteor/nova:categories";
 
 class CustomPostsItem extends Telescope.components.PostsItem {
 
+  constructor(props, context) {
+    super(props, context);
+
+    this.renderSubscribeButton = this.renderSubscribeButton.bind(this);
+    this.onSubscribe = this.onSubscribe.bind(this);
+    this.isSubscribed = this.isSubscribed.bind(this);
+  }
+
+  isSubscribed(post, user) {
+    if (!post || !user)
+      return false;
+    return post.subscribers && post.subscribers.indexOf(user._id) != -1;
+  }
+
+  renderSubscribeButton() {
+    const post = this.props.post;
+    const user = this.context.currentUser;
+
+    let btnStyle = "default";
+
+    let isSubscribed = this.isSubscribed(post, user);
+    if( isSubscribed ) {
+      btnStyle = "info";
+    }
+
+    return (
+      <button type="button" title="Observe"
+        className={`btn btn-${btnStyle} btn-sm pull-right`}
+        style={{padding: '.5rem', lineHeight: 1, borderRadius: '50%', marginLeft: '.5rem'}}
+        onClick={this.onSubscribe} >
+        <i className="fa fa-eye"></i>
+      </button>
+    )
+  }
+
+  onSubscribe() {
+    const post = this.props.post;
+    const user = this.context.currentUser;
+
+    let callAction = 'subscribePost';
+
+    let isSubscribed = this.isSubscribed(post, user);
+    if( isSubscribed ) {
+      callAction = "unsubscribePost";
+    }
+
+    this.context.actions.call(callAction, post._id, (error, result) => {
+      if (result)
+        this.context.events.track(callAction, {'_id': post._id});
+    })
+  }
+
   render() {
 
     const post = this.props.post;
+    const user = this.context.currentUser;
 
-    let postClass = "posts-item"; 
+    let postClass = "posts-item";
     if (post.sticky) postClass += " posts-sticky";
 
     // ⭐ custom code starts here ⭐
@@ -24,22 +77,22 @@ class CustomPostsItem extends Telescope.components.PostsItem {
 
     return (
       <div className={postClass}>
-        
+
         <div className="posts-item-vote">
           <Telescope.components.Vote post={post} currentUser={this.context.currentUser}/>
         </div>
-        
+
         {post.thumbnailUrl ? <Telescope.components.PostsThumbnail post={post}/> : null}
 
         <div className="posts-item-content">
-          
+
           <h3 className="posts-item-title">
             <Link to={Posts.getLink(post)} className="posts-item-title-link" target={Posts.getLinkTarget(post)}>
               {post.title}
             </Link>
             {this.renderCategories()}
           </h3>
-          
+
           <div className="posts-item-meta">
             {post.user? <div className="posts-item-user"><Telescope.components.UsersAvatar user={post.user} size="small"/><Telescope.components.UsersName user={post.user}/></div> : null}
             <div className="posts-item-date"><FormattedRelative value={post.postedAt}/></div>
@@ -55,19 +108,22 @@ class CustomPostsItem extends Telescope.components.PostsItem {
         </div>
 
         {this.renderCommenters()}
-        
-      
+
+        {(user && post.author !== user.username) ? this.renderSubscribeButton() : null}
+
       </div>
     )
   }
 };
-  
+
 CustomPostsItem.propTypes = {
   post: React.PropTypes.object.isRequired
 }
 
 CustomPostsItem.contextTypes = {
-  currentUser: React.PropTypes.object
+  currentUser: React.PropTypes.object,
+  actions: React.PropTypes.object,
+  events: React.PropTypes.object
 };
 
 export default CustomPostsItem;
