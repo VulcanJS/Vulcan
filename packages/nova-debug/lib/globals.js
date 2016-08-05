e2a0fe26ba728bdf812eabf8f7c7b5969f7a2734
Pedro Leandro Coutin Portuondo <pcoutin@arailabs.com>
2016-08-05 15:38:01 -0700

    Corrected imports for debug convenience globals

--- packages/nova-debug/lib/globals.js
+++ packages/nova-debug/lib/globals.js
@@ -1,6 +1,6 @@
 import PostsImport from "meteor/nova:posts";
-import CommentsImport from "meteor/nova:posts";
-import UsersImport from "meteor/nova:posts";
+import CommentsImport from "meteor/nova:comments";
+import UsersImport from "meteor/nova:users";
 import CategoriesImport from "meteor/nova:categories";
 
 Posts = PostsImport;
