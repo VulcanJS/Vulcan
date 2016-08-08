e7e0d3333902ae7cfc2c470f2b85708fee7b36d1
Sacha Greif <sacha357@gmail.com>
2016-08-08 10:54:33 +0900

    clean up nova:core

--- packages/nova-core/lib/server.js
+++ packages/nova-core/lib/server.js
@@ -1,9 +1,11 @@
+import "./modules.js";
+
+import "./server/start.js";
+
 import Messages from "./messages.js";
 import ModalTrigger from "./components/ModalTrigger.jsx";
 import ContextPasser from "./components/ContextPasser.jsx";
 import FlashContainer from "./containers/FlashContainer.jsx";
 import AppComposer from "./containers/AppComposer.jsx";
-import CurrentUserContainer from "./containers/CurrentUserContainer.jsx";
-// import NovaCounts from "./counts.js";
 
-export { Messages, ModalTrigger, ContextPasser, AppComposer, FlashContainer, CurrentUserContainer};
+export { Messages, ModalTrigger, ContextPasser, AppComposer, FlashContainer};
