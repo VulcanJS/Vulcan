eed031c4d686f80b41d4b32cf1bad4e18a5de6e3
MeteorPlus <meteorplus@users.noreply.github.com>
2016-09-30 03:37:55 +0100

    Added notifications update/add
    TODO: add the hash# so the page slides to the comment newly added

--- packages/nova-notifications/lib/notifications.js
+++ packages/nova-notifications/lib/notifications.js
@@ -1,44 +1,17 @@
-import Telescope from 'meteor/nova:lib';
-import Users from 'meteor/nova:users';
-import NovaEmail from 'meteor/nova:email';
+import Comments from './collection.js';
+import Posts from "meteor/nova:posts";
 
-Telescope.notifications = {};
-
-Telescope.notifications.create = (userIds, notificationName, data) => {
-
-  // if userIds is not an array, wrap it in one
-  if (!Array.isArray(userIds)) userIds = [userIds];
-
-  userIds.forEach(userId => {
-
-    const user = Users.findOne(userId);
-    const email = NovaEmail.emails[notificationName];
-    const properties = email.getProperties(data);
-    const subject = email.subject(properties);
-    const html = NovaEmail.getTemplate(email.template)(properties);
-
-    const userEmail = Users.getEmail(user);
-    if (!!userEmail) {
-      NovaEmail.buildAndSendHTML(Users.getEmail(user), subject, html);
-    } else {
-      console.log(`// Couldn't send notification: admin user ${user._id} doesn't have an email`);
-    }
-  });
-
+Comments.getNotificationProperties = function (data) {
+  const comment = data.comment;
+  var commentAuthor = Meteor.users.findOne(comment.userId);
+  var post = Posts.findOne(comment.postId);
+  var properties = {
+    profileUrl: commentAuthor && commentAuthor.getProfileUrl(true),
+    postUrl: Posts.getPageUrl(post, true),
+    authorName : Comments.getAuthorName(comment),
+    postTitle: post.title,
+    htmlBody: comment.htmlBody,
+    commentUrl: Comments.getPageUrl(comment, true)
+  };
+  return properties;
 };
-
-if (typeof Telescope.settings.collection !== "undefined") {
-  Telescope.settings.collection.addField({
-    fieldName: 'emailNotifications',
-    fieldSchema: {
-      type: Boolean,
-      optional: true,
-      defaultValue: true,
-      autoform: {
-        group: 'notifications',
-        instructions: 'Enable email notifications for new posts and new comments (requires restart).'
-      }
-    }
-  });
-}
-
