4487464a3347a5e712c5807e19775c4592c81278
xavcz <xavier.cazalot@gmail.com>
2016-11-17 14:12:13 +0100

    remove duplicated withCurrentUser

--- packages/nova-core/lib/containers/withCurrentUser.js
+++ packages/nova-core/lib/containers/withCurrentUser.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 import { Meteor } from 'meteor/meteor';
-import { withApollo } from 'react-apollo';
 import Telescope from 'meteor/nova:lib';
 
 /**
  * withCurrentUser - HOC to give access to the currentUser as a prop of a WrappedComponent
- * note: it uses withApollo HOC to get the store
  **/
 export default function withCurrentUser(WrappedComponent) {
 
-  const WithCurrentUser = (props, context) => {
+  class WithCurrentUser extends Component {
+    constructor(...args) {
+      super(...args);
+    }
+    
+    render() {
+      const {client} = this.context; // grab the apollo client from the context
 
-    const {client, ...otherProps} = props; 
+      const currentUser = client ? client.store.getState().apollo.data[`User${Meteor.userId()}`] : null;
 
-    const currentUser = client ? client.store.getState().apollo.data[`User${Meteor.userId()}`] : null;
-
-    return currentUser ? <WrappedComponent currentUser={currentUser} {...otherProps} /> : <WrappedComponent {...otherProps} />;
+      return currentUser ? <WrappedComponent currentUser={currentUser} {...this.props} /> : <WrappedComponent {...this.props} />;
+    }
   }
 
+  WithCurrentUser.contextTypes = { client: PropTypes.object.isRequired };
   WithCurrentUser.displayName = `withCurrentUser(${Telescope.utils.getComponentDisplayName(WrappedComponent)}`;
   WithCurrentUser.WrappedComponent = WrappedComponent;
 
-  return hoistStatics(withApollo(WithCurrentUser), WrappedComponent);
+  return hoistStatics(WithCurrentUser, WrappedComponent);
 }
