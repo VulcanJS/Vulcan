f2b623e46bffc02e7c6b27737009ee5248add766
xavcz <xavier.cazalot@gmail.com>
2016-11-17 10:19:52 +0100

    withCurrentUser: component returned as a class so it can be extended

--- packages/nova-base-containers/lib/containers/withCurrentUser.js
+++ packages/nova-base-containers/lib/containers/withCurrentUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 import { Meteor } from 'meteor/meteor';
 import Telescope from 'meteor/nova:lib';
@@ -8,13 +8,18 @@ import Telescope from 'meteor/nova:lib';
  **/
 export default function withCurrentUser(WrappedComponent) {
 
-  const WithCurrentUser = (props, context) => {
+  class WithCurrentUser extends Component {
+    constructor(...args) {
+      super(...args);
+    }
+    
+    render() {
+      const {client} = context; // grab the apollo client from the context
 
-    const {client} = context; // grab the apollo client from the context
+      const currentUser = client ? client.store.getState().apollo.data[`User${Meteor.userId()}`] : null;
 
-    const currentUser = client ? client.store.getState().apollo.data[`User${Meteor.userId()}`] : null;
-
-    return currentUser ? <WrappedComponent currentUser={currentUser} {...props} /> : <WrappedComponent {...props} />;
+      return currentUser ? <WrappedComponent currentUser={currentUser} {...props} /> : <WrappedComponent {...props} />;
+    }
   }
 
   WithCurrentUser.contextTypes = { client: PropTypes.object.isRequired };
