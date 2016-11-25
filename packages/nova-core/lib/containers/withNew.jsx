698141e6fa8be1a4647f343aa475ad93c533e3dd
Sacha Greif <sacha357@gmail.com>
2016-11-25 10:58:32 +0900

    clean up and comment withNew

--- packages/nova-core/lib/containers/withNew.jsx
+++ packages/nova-core/lib/containers/withNew.jsx
@@ -1,3 +1,18 @@
+/*
+
+Generic mutation wrapper to insert a new document in a collection and update
+a related query on the client with the new item and a new total item count. 
+
+Sample mutation: 
+
+  mutation moviesNew($document: MoviesInput) {
+    moviesNew(document: $document) {
+      ...MoviesNewFormFragment
+    }
+  }
+
+*/
+
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -7,61 +22,79 @@ import update from 'immutability-helper';
 
 export default function withNew(WrappedComponent) {
 
-  class WithNew extends Component {
-  // const WithNew = props => {
-    render() {
-      const props = this.props;
-      console.log(props)
+  // create a new stateless component so we can access its props
+  const WithNew = props => {
 
-      const { collection, fragmentName, fragment } = props,
-            collectionName = collection._name,
-            mutationName = collection.options.mutations.new.name,
-            listResolverName = collection.options.resolvers.list.name,
-            totalResolverName = collection.options.resolvers.total.name;
+    console.log(props)
 
-      // if a queryName is passed as prop, build an "automated" updateQueries function
-      let updateQueries = {};
-      if (props.queryName) {
-        updateQueries = {
-          [props.queryName]: (prev, { mutationResult }) => {
-            console.log(prev)
-            console.log(mutationResult)
-            const newDocument = mutationResult.data[mutationName];
-            console.log(newDocument)
-            const newList = update(prev, {
-              [listResolverName]: { $unshift: [newDocument] },
-              [totalResolverName]: { $set: prev[totalResolverName] + 1 }
-            });
-            console.log(newList)
-            return newList;
-          }
+    const { collection, fragmentName, fragment, queryName } = props,
+          collectionName = collection._name,
+          mutationName = collection.options.mutations.new.name,
+          listResolverName = collection.options.resolvers.list.name,
+          totalResolverName = collection.options.resolvers.total.name;
+
+    // if a queryName is passed as prop, generate updateQueries function
+    let updateQueries = {};
+    if (queryName) {
+      updateQueries = {
+        [queryName]: (prev, { mutationResult }) => {
+
+          // get new document to insert
+          const newDocument = mutationResult.data[mutationName];
+
+          // generate new list with updated total count and document inserted at top
+          const newList = update(prev, {
+            [listResolverName]: { $unshift: [newDocument] },
+            [totalResolverName]: { $set: prev[totalResolverName] + 1 }
+          });
+
+          console.log(`// updated query ${queryName}:`)
+          console.log(prev)
+          console.log(newList)
+
+          return newList;
         }
       }
-
-      const ComponentWithNew = graphql(gql`
-        mutation ${mutationName}($document: ${collectionName}Input) {
-          ${mutationName}(document: $document) {
-            ...${fragmentName}
-          }
-        }
-        ${fragment}
-      `, {
-        props: ({ownProps, mutate}) => ({
-          newMutation: ({document}) => {
-            return mutate({ 
-              variables: { document },
-              updateQueries: props.updateQueries || updateQueries
-            })
-          }
-        }),
-      })(WrappedComponent);
-
-      return <ComponentWithNew {...props} />
     }
+
+    // wrap component with graphql HoC
+    const ComponentWithNew = graphql(gql`
+      mutation ${mutationName}($document: ${collectionName}Input) {
+        ${mutationName}(document: $document) {
+          ...${fragmentName}
+        }
+      }
+      ${fragment}
+    `, {
+      props: ({ownProps, mutate}) => ({
+        newMutation: ({document}) => {
+
+          console.log('// newMutation')
+          console.log(updateQueries)
+          
+          return mutate({ 
+            variables: { document },
+            updateQueries: props.updateQueries || updateQueries
+          })
+        }
+      }),
+    })(WrappedComponent);
+
+    // render wrapped component
+    return <ComponentWithNew {...props} />
   }
 
   WithNew.displayName = `withNew(${Telescope.utils.getComponentDisplayName(WrappedComponent)}`;
   WithNew.WrappedComponent = WrappedComponent;
-  return WithNew;
+
+  // hoist wrapped component's statics up to the new wrapper component and return it
   return hoistStatics(WithNew, WrappedComponent);
-};
+
+}
+
+withNew.propTypes = {
+  collection: React.PropTypes.object, // the collection to mutate
+  fragmentName: React.PropTypes.string, // the name of the fragment
+  fragment: React.PropTypes.object, // controls what data the mutation should return
+  queryName: React.PropTypes.string, // the query to update on the client
+}
