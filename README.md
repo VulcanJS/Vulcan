43de7d5dfdf6c237901b811f9019d2a1b405622b
manriquef <cisco.m@outlook.com>
2016-12-24 22:36:02 +0900

    update 2

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Telescope Nova
 
+<<<<<<< HEAD
 Version 0.27.5
 
 This is the Apollo/GraphQL version of Telescope Nova. [You can find the documentation here](http://nova-docs.telescopeapp.org/).
+=======
+### Looking for the Apollo/GraphQL version? Check out the [devel](https://github.com/TelescopeJS/Telescope/tree/devel) branch. 
+
+There are currently two distinct versions of Telescope: **Nova** and **Legacy**.
+
+**Nova** is the new, React-based version and all development will happen on this version going forward. It's used by the [master](https://github.com/TelescopeJS/Telescope/tree/master) and [devel](https://github.com/TelescopeJS/Telescope/tree/devel) branches. 
+
+Note that as of December 2016, the devel branch now uses [GraphQL](http://graphql.org) as its data layer while the master branch is still on the previous, non-GraphQL version. It is recommended you use the devel branch for any new projects if possible. 
+>>>>>>> TelescopeJS/master
 
 ### Other Versions
 
--- packages/nova-base-components/lib/posts/PostsCommentsThread.jsx
+++ packages/nova-base-components/lib/posts/PostsCommentsThread.jsx
@@ -1,3 +1,9 @@
+/*
+   Modified: 24DEC2016
+   Changed size of "New Comments" and "Comments"
+*/
+
+
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { ModalTrigger, withList, withCurrentUser, Components, registerComponent, Utils } from 'meteor/nova:core';
@@ -7,33 +13,33 @@ import gql from 'graphql-tag';
 const PostsCommentsThread = (props, context) => {
 
   const {loading, terms: { postId }, results, totalCount} = props;
-  
+
   if (loading) {
-  
+
     return <div className="posts-comments-thread"><Components.Loading/></div>
-  
+
   } else {
-    
+
     const resultsClone = _.map(results, _.clone); // we don't want to modify the objects we got from props
     const nestedComments = Utils.unflatten(resultsClone, '_id', 'parentCommentId');
 
     return (
       <div className="posts-comments-thread">
-        <h4 className="posts-comments-thread-title"><FormattedMessage id="comments.comments"/></h4>
+        <h5 className="posts-comments-thread-title"><FormattedMessage id="comments.comments"/></h5>
         <Components.CommentsList comments={nestedComments} commentCount={totalCount}/>
         {!!props.currentUser ?
           <div className="posts-comments-thread-new">
-            <h4><FormattedMessage id="comments.new"/></h4>
+            <h5><FormattedMessage id="comments.new"/></h5>
             <Components.CommentsNewForm
-              postId={postId} 
-              type="comment" 
+              postId={postId}
+              type="comment"
             />
           </div> :
           <div>
             <ModalTrigger size="small" component={<a><FormattedMessage id="comments.please_log_in"/></a>}>
               <Components.UsersAccountForm/>
             </ModalTrigger>
-          </div> 
+          </div>
         }
       </div>
     );
