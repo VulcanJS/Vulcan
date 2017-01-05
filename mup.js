2eb368daa42a045461b23a0cf7bf99a0423f5910
Discordius <habryka@berkeley.edu>
2017-01-05 01:29:18 -0800

    Created deployment folders with mup config files. Run "mup setup" and "mup deploy" in either to deploy to port 80 for prod and 82 for development.

---
+++ mup.js
@@ -0,0 +1,37 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '1.2.3.4',
+      username: 'root'
+      // pem:
+      // password:
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'app',
+    path: '../app',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'app.com',
+      MONGO_URL: 'mongodb://localhost/meteor'
+    },
+
+    //dockerImage: 'kadirahq/meteord'
+    deployCheckWaitTime: 60
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
