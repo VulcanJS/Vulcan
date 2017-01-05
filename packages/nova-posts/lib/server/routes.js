817a81b7fc93ef420287902e784e1988fc34dc86
xavcz <xavier.cazalot@gmail.com>
2017-01-05 07:52:34 +0100

    re-enable server-side method Posts.increaseClicks

--- packages/nova-posts/lib/server/routes.js
+++ packages/nova-posts/lib/server/routes.js
@@ -1,11 +1,33 @@
-import { Picker } from 'meteor/meteorhacks:picker';
 import escapeStringRegexp from 'escape-string-regexp';
+import { Picker } from 'meteor/meteorhacks:picker';
+import Events from 'meteor/nova:events';
 import Posts from '../collection.js';
 
-Picker.route('/out', function(params, req, res, next) {
-  var query = params.query;
-  if(query.url){ // for some reason, query.url doesn't need to be decoded
+// /**
+//  * @summary Increase the number of clicks on a post
+//  * @param {string} postId – the ID of the post being edited
+//  * @param {string} ip – the IP of the current user
+//  */
+Posts.increaseClicks = (postId, ip) => {
+  const clickEvent = {
+    name: 'click',
+    properties: {
+      postId: postId,
+      ip: ip
+    }
+  };
 
+  // make sure this IP hasn't previously clicked on this post
+  const existingClickEvent = Events.findOne({name: 'click', 'properties.postId': postId, 'properties.ip': ip});
+
+  if(!existingClickEvent){
+    Events.log(clickEvent);
+    return Posts.update(postId, { $inc: { clickCount: 1 }});
+  }
+};
+
+Picker.route('/out', ({ query}, req, res, next) => {
+  if(query.url){ // for some reason, query.url doesn't need to be decoded
     /*
     If the URL passed to ?url= is in plain text, any hash fragment
     will get stripped out.
@@ -15,8 +37,8 @@ Picker.route('/out', function(params, req, res, next) {
     const post = Posts.findOne({url: {$regex: escapeStringRegexp(query.url)}});
 
     if (post) {
-      var ip = req.headers && req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-      Posts.methods.increaseClicks(post._id, ip);
+      const ip = req.headers && req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+      Posts.increaseClicks(post._id, ip);
       res.writeHead(301, {'Location': query.url});
       res.end();
     } else {
