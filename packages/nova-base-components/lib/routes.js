5e3ed2b207ef6dc11e0527cc7385816ee29d91a4
SachaG <info@sachagreif.com>
2017-01-16 12:24:59 +0900

    Move voting-related resolvers to nova:voting package

--- packages/nova-base-components/lib/routes.js
+++ packages/nova-base-components/lib/routes.js
@@ -1,7 +1,7 @@
 import { Components, addRoute } from 'meteor/nova:core';
 
 addRoute([
-  {name:'posts.list',    path: '/',                     component: Components.PostsHome }, // index route
+  {name:'posts.list',     path: '/',                     component: Components.PostsHome }, // index route
   {name:'posts.daily',    path:'daily',                 component: Components.PostsDaily},
   {name:'posts.single',   path:'posts/:_id(/:slug)',    component: Components.PostsSingle},
   {name:'users.single',   path:'users/:slug',           component: Components.UsersSingle},
