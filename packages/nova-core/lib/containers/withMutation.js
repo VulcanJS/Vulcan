04182efe223fb18e3678615c024ed3ecd806afb9
SachaG <info@sachagreif.com>
2017-01-20 14:08:45 +0900

    Make mutation hoc accept mutations without arguments

--- packages/nova-core/lib/containers/withMutation.js
+++ packages/nova-core/lib/containers/withMutation.js
@@ -16,14 +16,25 @@ import gql from 'graphql-tag';
 
 export default function withMutation({name, args}) {
 
-  const args1 = _.map(args, (type, name) => `$${name}: ${type}`); // e.g. $url: String
-  const args2 = _.map(args, (type, name) => `${name}: $${name}`); // e.g. $url: url
+  let mutation;
 
-  return graphql(gql`
-    mutation ${name}(${args1}) {
-      ${name}(${args2})
-    }
-  `, {
+  if (args) {
+    const args1 = _.map(args, (type, name) => `$${name}: ${type}`); // e.g. $url: String
+    const args2 = _.map(args, (type, name) => `${name}: $${name}`); // e.g. $url: url
+    mutation = `
+      mutation ${name}(${args1}) {
+        ${name}(${args2})
+      }
+    `
+  } else {
+    mutation = `
+      mutation ${name} {
+        ${name}
+      }
+    `
+  }
+
+  return graphql(gql`${mutation}`, {
     props: ({ownProps, mutate}) => ({
       [name]: (vars) => {
         return mutate({ 
