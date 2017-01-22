c0cb4e2409d5eeed49acb4d45f587d8266af7d7b
Sonny Gauran <sonnygauran@gmail.com>
2017-01-23 03:19:16 +0800

    Added test for nova:posts
    When trying to import package, import fails because of missing dependency on nova:events
    
    Adding nova:events@1.0.0 to Package dependency resolved this issue.

--- packages/nova-posts/package.js
+++ packages/nova-posts/package.js
@@ -11,6 +11,7 @@ Package.onUse(function (api) {
 
   api.use([
     'nova:core@1.0.0',
+    'nova:events@1.0.0',
     'nova:users@1.0.0',
     'utilities:react-list-container@0.1.10'
   ]);
@@ -23,4 +24,15 @@ Package.onUse(function (api) {
   api.mainModule("lib/server.js", "server");
   api.mainModule("lib/client.js", "client");
 
+});
+
+
+Package.onTest(function(api) {
+  api.use([
+    'ecmascript',
+    'tinytest',
+    'nova:posts',
+  ]);
+  
+  api.mainModule('test.js');
 });
