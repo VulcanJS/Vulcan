dce38c1a284def508f84d9ff6f430053a03b2ac7
SachaG <info@sachagreif.com>
2017-02-01 10:39:18 +0900

    Simplify custom fragment code

--- packages/customization-demo/lib/fragments.js
+++ packages/customization-demo/lib/fragments.js
@@ -1,52 +1,12 @@
-import { registerFragment } from 'meteor/nova:lib';
+import { registerFragment, getFragment } from 'meteor/nova:lib';
 import gql from 'graphql-tag';
 
 const CustomPostsListFragment = gql`
   fragment CustomPostsList on Post {
-    _id
-    title
-    url
-    slug
-    thumbnailUrl
-    postedAt
-    sticky
-    status
-    categories {
-      # ...minimumCategoryInfo
-      _id
-      name
-      slug
-    }
-    commentCount
-    commenters {
-      # ...avatarUserInfo
-      _id
-      displayName
-      emailHash
-      slug
-    }
-    upvoters {
-      _id
-    }
-    downvoters {
-      _id
-    }
-    upvotes
-    downvotes
-    baseScore
-    score
-    viewCount
-    clickCount
-    user {
-      # ...avatarUserInfo
-      _id
-      displayName
-      emailHash
-      slug
-    }
-    userId
+    ...PostsList
     color # new custom property!
   }
+  ${getFragment('PostsList')}
 `;
 
 registerFragment(CustomPostsListFragment, 'PostsList');
