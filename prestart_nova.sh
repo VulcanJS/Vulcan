5bf7a6f2fcbe5d26fcaae7532bb42d2edc1f5a5e
xavcz <xavier.cazalot@gmail.com>
2017-03-05 13:21:31 +0000

    handle somehow windows in the prestart script

--- prestart_nova.sh
+++ prestart_nova.sh
@@ -1,7 +1,16 @@
-purple=$(tput setaf 141)
-blue=$(tput setaf 153)
-bold=$(tput bold)
-reset=$(tput sgr0)
+#!/usr/bin/env bash
+
+if tput setaf 1 &> /dev/null; then
+  purple=$(tput setaf 141)
+  blue=$(tput setaf 153)
+  bold=$(tput bold)
+  reset=$(tput sgr0)
+else 
+  purple=""
+  blue=""
+  bold=""
+  reset=""
+fi
 
 test -f settings.json || (echo "🛠  ${blue}Creating your own settings.json file...\n"; cp sample_settings.json settings.json;)
 
@@ -9,4 +18,6 @@ echo "🔭  ${bold}${purple}Happy hacking with Telescope Nova!${reset}";
 
 echo "📖  ${blue}The docs are available at: ${purple}http://nova-docs.telescopeapp.org";
 
-tput sgr0;
+if tput setaf 1 &> /dev/null; then
+  tput sgr0;
+fi
