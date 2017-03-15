349dd9df26e56f4761297297bc630979ebc0cb43
ESRogs <esrogs@gmail.com>
2017-03-14 17:38:55 -0700

    add collections.js file to the lw-notifications package

---
+++ packages/lw-notifications/lib/collection.js
@@ -0,0 +1,27 @@
+import schema from './schema.js';
+import mutations from './mutations.js';
+import resolvers from './resolvers.js';
+// TODO: is the following comment true in our case?
+import { createCollection } from 'meteor/nova:lib'; // import from nova:lib because nova:core isn't loaded yet
+
+/**
+ * @summary Telescope Notifications namespace
+ * @namespace Notifications
+ */
+const Notifications = createCollection({
+
+  collection: Meteor.notifications,
+
+  collectionName: 'notifications',
+
+  typeName: 'Notification',
+
+  schema,
+
+  resolvers,
+
+  mutations,
+
+});
+
+export default Notifications;
