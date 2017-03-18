e9aedce520f69b8ce2fc641859bf6b04b1d67de3
ESRogs <esrogs@gmail.com>
2017-03-17 17:45:08 -0700

    remove extraneous notificationData parameter from createNotifications

--- packages/lw-notifications/lib/callbacks.js
+++ packages/lw-notifications/lib/callbacks.js
@@ -2,14 +2,22 @@ import Notifications from './collection.js';
 import Users from 'meteor/nova:users';
 import { addCallback, newMutation } from 'meteor/nova:core';
 
-// ROGTODO: actually use notificationData
-const createNotifications = (userIds, documentType, notificationType, notificationData) => {
+const createNotifications = (userIds, notificationType, documentType, documentId) => {
   userIds.forEach(userId => {
+
     let user = Users.findOne(userId);
+
+    let notificationData = {
+      userId: userId,
+      documentId: documentId,
+      type: documentType,
+      notificationMessage: notificationType
+    }
+
     newMutation({
       action: 'notifications.new',
       collection: Notifications,
-      document: {type: documentType, notificationMessage: notificationType},
+      document: notificationData,
       currentUser: user,
       validate: false
     });
@@ -24,11 +32,7 @@ const Posts = Package['nova:posts'].default;
  * @summary Add notification callback when a post is approved
  */
 const PostsApprovedNotification = (post) => {
-  const notificationData = {
-    post: _.pick(post, '_id', 'userId', 'title', 'url')
-  };
-
-  createNotifications([post.userId], 'post', 'postApproved', notificationData);
+  createNotifications([post.userId], 'postApproved', 'post', post._id);
 }
 addCallback("posts.approve.async", PostsApprovedNotification);
 
@@ -40,20 +44,16 @@ const PostsNewNotifications = (post) => {
   let adminIds = _.pluck(Users.adminUsers({fields: {_id:1}}), '_id');
   let usersToNotify = _.pluck(Users.find({'notifications_posts': true}, {fields: {_id:1}}).fetch(), '_id');
 
-  const notificationData = {
-    post: _.pick(post, '_id', 'userId', 'title', 'url', 'slug')
-  };
-
   // remove post author ID from arrays
   adminIds = _.without(adminIds, post.userId);
   usersToNotify = _.without(usersToNotify, post.userId);
 
   if (post.status === Posts.config.STATUS_PENDING) {
     // if post is pending, only notify admins
-    createNotifications(adminIds, 'post', 'newPendingPost', notificationData);
+    createNotifications(adminIds, 'newPendingPost', 'post', post._id);
   } else {
     // if post is approved, notify everybody
-    createNotifications(usersToNotify, 'post', 'newPost', notificationData);
+    createNotifications(usersToNotify, 'newPost', 'post', post._id);
   }
 }
 addCallback("posts.new.async", PostsNewNotifications);
@@ -66,15 +66,11 @@ const CommentsNewNotifications = (comment) => {
 
     const post = Posts.findOne(comment.postId);
     const postAuthor = Users.findOne(post.userId);
-    const notificationData = {
-      comment: _.pick(comment, '_id', 'userId', 'author', 'htmlBody', 'postId'),
-      post: _.pick(post, '_id', 'userId', 'title', 'url')
-    };
 
     // 1. Notify author of post (if they have new comment notifications turned on)
     //    but do not notify author of post if they're the ones posting the comment
     if (Users.getSetting(postAuthor, "notifications_comments", true) && comment.userId !== postAuthor._id) {
-      createNotifications([postAuthor.userId], 'comment', 'newComment', notificationData);
+      createNotifications([postAuthor.userId], 'newComment', 'comment', comment._id);
     }
 
     // 2. Notify author of comment being replied to
@@ -87,9 +83,7 @@ const CommentsNewNotifications = (comment) => {
 
         // do not notify parent comment author if they have reply notifications turned off
         if (Users.getSetting(parentCommentAuthor, "notifications_replies", true)) {
-          // add parent comment to notification data
-          notificationData.parentComment = _.pick(parentComment, '_id', 'userId', 'author', 'htmlBody');
-          createNotifications([parentCommentAuthor.userId], 'comment', 'newReply', notificationData);
+          createNotifications([parentCommentAuthor.userId], 'newReply', 'comment', comment._id);
         }
       }
     }
