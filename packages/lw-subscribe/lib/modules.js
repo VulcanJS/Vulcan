003aa4403811c4dd32693befca384bdb9b447946
ESRogs <esrogs@gmail.com>
2017-03-21 11:33:55 -0700

    remove spurious import from lw-subscribe/lib/modules.js

--- packages/lw-subscribe/lib/modules.js
+++ packages/lw-subscribe/lib/modules.js
@@ -1,7 +1,6 @@
 import './custom_fields.js';
 import './helpers.js';
 import subscribeMutationsGenerator from './mutations.js';
-import './views.js';
 import './permissions.js';
 import './fragments.js';
 
