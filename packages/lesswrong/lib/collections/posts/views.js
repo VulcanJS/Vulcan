315cbee47f910b7f88f7124a350d122cc5068dc7
Harmanas Chopra <harmchop@gmail.com>
2017-04-11 16:50:00 -0700

    Updated defaultView to filter out drafts.

--- packages/lesswrong/lib/collections/posts/views.js
+++ packages/lesswrong/lib/collections/posts/views.js
@@ -1,6 +1,17 @@
 import Posts from 'meteor/vulcan:posts';
 
 /**
+ * @summary Base parameters that will be common to all other view unless specific properties are overwritten
+ */
+Posts.addDefaultView(terms => ({
+  selector: {
+    status: Posts.config.STATUS_APPROVED,
+    draft: {$ne: true},
+    isFuture: {$ne: true} // match both false and undefined
+  }
+}));
+
+/**
  * @summary Draft view
  */
 Posts.addView("draft", terms => {
