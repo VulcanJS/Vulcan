329c3adf2eb4705c95b89143abb9781f30f1eb10
dylan <dylan.kojicheslin@gmail.com>
2017-04-19 13:12:54 -0600

    Calendar

--- packages/example-a-reminder/lib/components/CalendarPage.jsx
+++ packages/example-a-reminder/lib/components/CalendarPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { registerComponent } from 'meteor/vulcan:core';
+import Calendar from 'react-widgets/lib/Calendar';
+import momentLocalizer from 'react-widgets/lib/localizers/moment';
+import Moment from 'moment';
+momentLocalizer(Moment);
 
 const CalendarPage = () => {
   return (
     <div>
       <h1>Calendar</h1>
-      <p>stuff</p>
+        <Calendar  defaultValue={new Date()} footer={true} />
     </div>
   )
 }
