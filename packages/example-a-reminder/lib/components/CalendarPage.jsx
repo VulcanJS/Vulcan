6deb594b45a5e8c790851d58cbd168074b689ed0
dylan <dylan.kojicheslin@gmail.com>
2017-04-27 14:30:46 -0600

    day reminder react class

--- packages/example-a-reminder/lib/components/CalendarPage.jsx
+++ packages/example-a-reminder/lib/components/CalendarPage.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { registerComponent } from 'meteor/vulcan:core';
+import { Components, registerComponent } from 'meteor/vulcan:core';
 import Calendar from 'react-widgets/lib/Calendar';
 import momentLocalizer from 'react-widgets/lib/localizers/moment';
 import Moment from 'moment';
+
 momentLocalizer(Moment);
+
+
+class reminderDayComponent extends React.Component{
+  render() {
+    const date = this.props.date;
+    //future dates styled, change to indicator for reminders on that day
+    const style =  { backgroundColor: date > new Date() && '#F57B7B'} ;
+    return (
+      <div style={style}>
+        {this.props.label}
+      </div>
+    );
+  }
+}
 
 const CalendarPage = () => {
   return (
     <div>
       <h1>Calendar</h1>
-        <Calendar  defaultValue={new Date()} footer={true} />
+        <Calendar  defaultValue={new Date()} footer={true} label={"stuff"} dayComponent={reminderDayComponent} />
     </div>
   )
 }
-
 registerComponent('CalendarPage', CalendarPage);
