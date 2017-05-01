8fc2984cb84795063b257e167942871909c78cc9
dylan <dylan.kojicheslin@gmail.com>
2017-05-01 17:51:21 -0600

    collection outline

--- packages/example-a-reminder/lib/modules/reminders/collection.js
+++ packages/example-a-reminder/lib/modules/reminders/collection.js
@@ -1,3 +1,23 @@
-//reminder collection
+//Reminders collection
 import { createCollection } from 'meteor/vulcan:core';
 import schema from './schema.js';
+// import resolvers from './resolvers.js';
+// import './fragments.js';
+// import mutations from './mutations.js';
+// import './permissions.js';
+// import './parameters.js';
+
+
+// const Reminders = createCollection({
+//
+//   collectionName: 'reminders',
+//
+//   typeName: 'Reminder',
+//
+//   schema,
+//
+//   resolvers,
+//
+//   mutations,
+//
+// });
