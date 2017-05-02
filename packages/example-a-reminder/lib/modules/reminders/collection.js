5ee9ad052ba341171aa3c6d1af64975286e94429
dylan <dylan.kojicheslin@gmail.com>
2017-05-01 23:30:34 -0600

    permissions pt2

--- packages/example-a-reminder/lib/modules/reminders/collection.js
+++ packages/example-a-reminder/lib/modules/reminders/collection.js
@@ -4,7 +4,7 @@ import schema from './schema.js';
 import resolvers from './resolvers.js';
 // import './fragments.js';
 import mutations from './mutations.js';
-// import './permissions.js';
+import './permissions.js';
 // import './parameters.js';
 
 
