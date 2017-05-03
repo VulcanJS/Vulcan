5cad83e18c4d14322c31394e6aa6fa65c1142b56
dylan <dylan.kojicheslin@gmail.com>
2017-05-02 21:54:12 -0600

    removed unused link

--- packages/example-a-reminder/lib/routes.jsx
+++ packages/example-a-reminder/lib/routes.jsx
@@ -1,4 +1,3 @@
 import { addRoute, getComponent } from 'meteor/vulcan:core';
 
-addRoute({name: "calendar", path: "/calendar", component: getComponent("CalendarPage")});
 addRoute({name:'Layout', path: '/', component: getComponent("Layout")});
