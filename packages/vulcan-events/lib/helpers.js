0ce8051130aa145d3dceee5865710ce4b884a0e2
SachaG <info@sachagreif.com>
2017-06-02 07:46:39 +0900

    Remove segment support (main events package should be service-agnostic apart from Google Analytics)

--- packages/vulcan-events/lib/helpers.js
+++ packages/vulcan-events/lib/helpers.js
@@ -1,4 +1,3 @@
-import Analytics from 'analytics-node';
 import { getSetting } from 'meteor/vulcan:core';
 import Events from './collection.js';
 /*
@@ -42,72 +41,6 @@ export const initGoogleAnalytics = () => {
   }
 };
 
-/*
-
-  We provide a special support for Segment, using analytics-node
-  See https://segment.com/docs/sources/server/node/
-  
-*/
-
-export const requestAnalyticsAsync = (hook, document, user) => {
-  
-  // get the segment write key from the settings
-  const useSegment = getSetting('useSegment');
-  const writeKey = getSetting('segmentWriteKey');
-
-  // the settings obviously tells to use segment
-  // and segment write key is defined & isn't the placeholder from sample_settings.json
-  if (useSegment && writeKey && writeKey !== '456bar') {
-    
-    const analytics = new Analytics(writeKey);
-    
-    if (hook.includes('users')) {
-      // if the mutation is related to users, use analytics.identify
-      // see https://segment.com/docs/sources/server/node/#identify
-      
-      // note: on users.new.async, user is undefined
-      const userId = user ? user._id : document._id;
-      
-      if (document.services) {
-        if(document.services.password) {
-          delete document.services.password;
-        }
-        
-        if (document.services.resume) {
-          delete document.services.resume;
-        }
-      }
-      
-      
-      const data = {
-        userId,
-        traits: document,
-      };
-      
-      // uncomment for debug
-      // console.log(`// dispatching identify on "${hook}" (user ${userId})`);
-      // console.log(data);
-      
-      analytics.identify(data);
-      
-    } else {
-      // else use analytics.track
-      // see https://segment.com/docs/sources/server/node/#track
-      
-      const data = {
-        userId: user._id,
-        event: hook,
-        properties: document,
-      };
-      
-      // uncomment for debug
-      // console.log(`// dispatching track on "${hook}"`);
-      // console.log(data);
-      
-      analytics.track(data);  
-    }
-  }
-}
 
 // collection based logging
 Events.log = function (event) {
