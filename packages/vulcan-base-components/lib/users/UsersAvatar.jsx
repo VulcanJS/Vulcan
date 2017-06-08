b45b63f48010859b3c64bd59fea44a5bfa072c7c
SachaG <info@sachagreif.com>
2017-06-07 18:08:22 -0700

    remove avatar inline styles, define styles in base-styles/main.scss instead

--- packages/vulcan-base-components/lib/users/UsersAvatar.jsx
+++ packages/vulcan-base-components/lib/users/UsersAvatar.jsx
@@ -3,37 +3,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Users from 'meteor/vulcan:users';
 import { Link } from 'react-router';
+import classNames from 'classnames';
 
-const UsersAvatar = ({user, size, link}) => {
-
-  const sizes = {
-    small: "20px",
-    medium: "30px",
-    large: "50px"
-  }
-
-  const aStyle = {
-    borderRadius: "100%",
-    display: "inline-block",
-    height: sizes[size],
-    width: sizes[size]
-  };
-
-  const imgStyle = {
-    borderRadius: "100%",
-    display: "block",
-    height: sizes[size],
-    width: sizes[size]
-  };
+const UsersAvatar = ({className, user, link}) => {
 
   const avatarUrl = user.avatarUrl || Users.avatar.getUrl(user);
 
-  const img = <img alt={Users.getDisplayName(user)} style={imgStyle} className="avatar" src={avatarUrl} title={user.username}/>;
+  const img = <img alt={Users.getDisplayName(user)} className="avatar-image" src={avatarUrl} title={user.username}/>;
   const initials = <span className="avatar-initials"><span>{Users.avatar.getInitials(user)}</span></span>;
 
   const avatar = avatarUrl ? img : initials;
 
-  return link ? <Link style={aStyle} className="users-avatar" to={Users.getProfileUrl(user)}>{avatar}</Link> : avatar;
+  return (
+    <div className={classNames('avatar', className)}>
+      {link ? 
+        <Link to={Users.getProfileUrl(user)}>
+          <span>{avatar}</span>
+        </Link> 
+        : <span>{avatar}</span>
+      }
+    </div>
+  );
 
 }
 
