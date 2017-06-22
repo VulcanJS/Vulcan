0bdd6e53974f9d7ae991b00c98b5407a5e9b9aae
Discordius <panisnecis@gmail.com>
2017-06-21 21:22:39 -0700

    Update postedAt when post is undrafted

---
+++ packages/lesswrong/lib/collections/posts/callbacks.js
@@ -0,0 +1,23 @@
+import Posts from 'meteor/vulcan:posts';
+import { addCallback, newMutation, editMutation, Utils } from 'meteor/vulcan:core';
+import { IntercomAPI } from 'react-intercom';
+
+function PostsEditRunPostUndraftedSyncCallbacks (modifier, post) {
+  if (modifier.$set && modifier.$set.draft === false && post.draft) {
+    modifier = runCallbacks("posts.undraft.sync", modifier, post);
+  }
+  return modifier;
+}
+addCallback("posts.edit.sync", PostsEditRunPostUndraftedSyncCallbacks);
+
+/**
+ * @summary set postedAt when a post is moved out of drafts
+ */
+function PostsSetPostedAt (modifier, post) {
+  modifier.$set.postedAt = new Date();
+  if (modifier.$unset) {
+    delete modifier.$unset.postedAt;
+  }
+  return modifier;
+}
+addCallback("posts.undraft.sync", PostsSetPostedAt);
