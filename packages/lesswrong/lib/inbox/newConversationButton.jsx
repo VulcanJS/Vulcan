1d5846ead607b2ccdd502da7831beca53786c604
Discordius <panisnecis@gmail.com>
2017-07-03 13:47:49 -0700

    Removed obsolote ParticipantInjector component

--- packages/lesswrong/lib/inbox/newConversationButton.jsx
+++ packages/lesswrong/lib/inbox/newConversationButton.jsx
@@ -6,17 +6,10 @@ Button used to start a new conversation for a given user
 
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
-import { Components, registerComponent, ModalTrigger, withList, withCurrentUser, getFragment } from 'meteor/vulcan:core';
-import { Redirect, withRouter } from 'react-router';
+import { Components, registerComponent, withCurrentUser, getFragment } from 'meteor/vulcan:core';
+import {  withRouter } from 'react-router';
 import Conversations from '../collections/conversations/collection.js';
 import Posts from 'meteor/vulcan:posts'
-
-const ParticipantInjector = props => {
-  addToAutofilledValues({participantIds: [props.currentUser._id, props.user._id]});
-  return <div></div>
-}
-
-registerComponent('ParticipantInjector', ParticipantInjector);
 
 class newConversationButton extends Component {
 
@@ -37,7 +30,6 @@ class newConversationButton extends Component {
               this.props.router.push({pathname: '/inbox', query: {select: conversation._id}});
             }}
           >
-          <Components.ParticipantInjector user={user} currentUser={currentUser} />
           </ Components.SmartForm>
           <Button onClick={() => this.props.closeModal()}>Close!</Button>
         </div>
