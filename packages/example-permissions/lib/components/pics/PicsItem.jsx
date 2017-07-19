66e527fab461fa484d9a5bbf6251c9e588207f76
SachaG <info@sachagreif.com>
2017-07-19 16:59:41 +0900

    create new example-permissions package to showcase groups & permissions API

---
+++ packages/example-permissions/lib/components/pics/PicsItem.jsx
@@ -0,0 +1,30 @@
+/* 
+
+An item in the pics list.
+
+*/
+
+import React from 'react';
+import { Components, registerComponent } from 'meteor/vulcan:core';
+
+import PicsDetail from './PicsDetails.jsx';
+
+const PicsItem = ({pic, currentUser}) =>
+
+  <div className="pics-item">
+
+    <Components.ModalTrigger className="pics-details-modal" component={<div className="pics-image"><img src={pic.imageUrl}/></div>}>
+      <PicsDetail documentId={pic._id} currentUser={currentUser} />
+    </Components.ModalTrigger>
+
+    <div className="pics-meta">
+
+      <div className="pics-comment-count">
+        <Components.Icon name="comment" /> {pic.commentsCount}
+      </div>
+      
+    </div>
+
+  </div>
+
+export default PicsItem;
