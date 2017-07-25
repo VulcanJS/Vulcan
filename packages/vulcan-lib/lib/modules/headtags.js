b75b7a3720697cec02146a2189578afab3160259
AYOUB HIDRI <ahidri01@lppar113063.local>
2017-07-25 11:28:49 +0200

    add function to remove tags from head

--- packages/vulcan-lib/lib/modules/headtags.js
+++ packages/vulcan-lib/lib/modules/headtags.js
@@ -3,3 +3,11 @@ export const Headtags = {
   link: [],
   script: [],
 }
+
+export const removeFromHeadTags = (type, name)=>{
+  HeadTags[type] = HeadTags[type].filter((tag)=>{
+    return (!tag.name || tag.name && tag.name !== name)
+  });
+
+  return HeadTags;
+}
