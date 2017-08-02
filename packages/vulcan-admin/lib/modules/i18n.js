dc702b0f3d3d3bc1300a2b8d337031a01997553d
SachaG <info@sachagreif.com>
2017-08-02 16:25:11 +0900

    fix admin i18n

--- packages/vulcan-admin/lib/modules/i18n.js
+++ packages/vulcan-admin/lib/modules/i18n.js
@@ -1,9 +1,9 @@
 import { addStrings } from 'meteor/vulcan:core';
 
 addStrings('en', {
-  'admin.users.name': 'Name',
-  'admin.users.created': 'Created',
-  'admin.users.groups': 'Groups',
-  'admin.users.actions': 'Actions',
-  'admin.users.email': 'Email',
+  'datatable.users.name': 'Name',
+  'datatable.users.created': 'Created',
+  'datatable.users.groups': 'Groups',
+  'datatable.users.actions': 'Actions',
+  'datatable.users.email': 'Email',
 });
