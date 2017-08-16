bf1466a253dc9e08989033100561a5fc3132057c
Discordius <panisnecis@gmail.com>
2017-08-15 21:44:28 -0700

    Fixed notifications and conversations

--- packages/lesswrong/lib/inbox/InboxWrapper.jsx
+++ packages/lesswrong/lib/inbox/InboxWrapper.jsx
@@ -8,18 +8,16 @@ import React, { PropTypes, Component } from 'react';
 import { Components, registerComponent, withCurrentUser } from 'meteor/vulcan:core';
 
 class InboxWrapper extends Component {
-
   render () {
 
     const terms = {view: 'userConversations', userId: (!!this.props.currentUser ? this.props.currentUser._id : "0")};
-
+    console.log("ConversationsList Terms", terms);
     return(
       <div className="wrapper">
           {!!this.props.currentUser ? <Components.InboxNavigation terms={terms}/> : <div></div>}
       </div>
     )
-
-  };
+  }
 }
 
 registerComponent('InboxWrapper', InboxWrapper, withCurrentUser);
