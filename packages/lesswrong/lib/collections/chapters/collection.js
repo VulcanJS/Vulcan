bdb4b35e188497562356b17fe8ec525cb7af45fc
Discordius <panisnecis@gmail.com>
2017-08-22 14:19:30 -0700

    Removed old comments in some places

--- packages/lesswrong/lib/collections/chapters/collection.js
+++ packages/lesswrong/lib/collections/chapters/collection.js
@@ -1,8 +1,6 @@
-// At present, every sequence starts with an empty chapter that does not show
-// on the sequence page.
-
 import { createCollection, getDefaultResolvers, getDefaultMutations } from 'meteor/vulcan:core';
 import schema from './schema.js';
+
 
 const Chapters = createCollection({
 
