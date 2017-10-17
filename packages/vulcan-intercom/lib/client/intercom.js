d81279385126937701a64142e3b5042c4f9baa3a
Sacha Greif <sacha357@gmail.com>
2017-10-17 10:27:40 +0900

    Simple Intercom package

---
+++ packages/vulcan-intercom/lib/client/intercom.js
@@ -0,0 +1,23 @@
+import { Components, replaceComponent, getSetting, withCurrentUser, Head } from 'meteor/vulcan:core';
+import React from 'react';
+import Intercom from 'react-intercom';
+
+const IntercomWrapper = ({ currentUser }) => {
+  const appId = getSetting('intercom.appId');
+
+  if (!appId) {
+    console.warn('Please add intercom.appId to your public settings or disable the vulcan:intercom package.');
+    return null;
+  }
+
+  return <Intercom 
+    appID={appId} 
+    email={currentUser.email}
+    name={currentUser.displayName}
+    _id={currentUser._id}
+    profileUrl={currentUser.pageUrl}
+  />;
+
+}
+
+Head.components.push([IntercomWrapper, withCurrentUser]);
---
+++ packages/vulcan-intercom/package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'vulcan:intercom',
+  summary: 'Vulcan Intercom integration package.',
+  version: '1.8.0',
+  git: "https://github.com/VulcanJS/Vulcan.git"
+});
+
+Package.onUse(function (api) {
+
+  api.versionsFrom('METEOR@1.5.2');
+
+  api.use([
+    'vulcan:core@1.8.0'
+  ]);
+
+  api.mainModule("lib/client/main.js", "client");
+  api.mainModule("lib/server/main.js", "server");
+
+});
