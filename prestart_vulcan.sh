4ceb93a8bea085551785afc16320e943dad915fe
MAGAN <Michael Magan>
2017-12-05 06:01:09 -0600

    Fixes shell script.

--- prestart_vulcan.sh
+++ prestart_vulcan.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 if tput setaf 1 &> /dev/null; then
   purple=$(tput setaf 141)
@@ -15,15 +15,15 @@ fi
 command -v meteor >/dev/null 2>&1 || { 
 echo "Vulcan requires Meteor but it's not installed. Trying to Install..." >&2; 
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
     # Mac OS platform
    echo "🌋  ${bold}${purple}Good news you have a Mac and we will install it now! ${reset}"; 
    curl https://install.meteor.com/ | bash;
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
     # GNU/Linux platform
     echo "🌋  ${bold}${purple}Good news you are on  GNU/Linux platform and we will install Meteor now! ${reset}"; 
     curl https://install.meteor.com/ | bash;
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
     # Windows NT platform
     echo "🌋  ${bold}${purple}Oh no! you are on a Windows platform and you will need to install Meteor Manually! ${reset}"; 
     echo "📖  ${blue}Meteor for Windows is available at: ${purple}https://install.meteor.com/windows";
