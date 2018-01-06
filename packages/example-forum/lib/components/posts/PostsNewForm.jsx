c77ac378c15969a65c869430dab11db3c9740cd6
Luca Hagel <hagel.luca@gmail.com>
2018-01-06 07:20:06 +0100

    Actually use imported PropTypes in PostsNewForm

--- packages/example-forum/lib/components/posts/PostsNewForm.jsx
+++ packages/example-forum/lib/components/posts/PostsNewForm.jsx
@@ -24,10 +24,10 @@ const PostsNewForm = (props, context) =>
     </Components.ShowIf>
 
 PostsNewForm.propTypes = {
-  closeModal: React.PropTypes.func,
-  router: React.PropTypes.object,
-  flash: React.PropTypes.func,
-  redirect: React.PropTypes.string,
+  closeModal: PropTypes.func,
+  router: PropTypes.object,
+  flash: PropTypes.func,
+  redirect: PropTypes.string,
 }
 
 PostsNewForm.contextTypes = {
