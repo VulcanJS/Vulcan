54149fb9e140d596c084744934f204ba5d14b9f4
Luca Hagel <hagel.luca@gmail.com>
2018-01-06 13:39:40 +0100

    Export debug group start & end functions

--- packages/vulcan-lib/lib/modules/debug.js
+++ packages/vulcan-lib/lib/modules/debug.js
@@ -1,7 +1,18 @@
 import { getSetting } from './settings.js';
 
-export const debug = function() {
+export const debug = function () {
   if (getSetting('debug', false)) {
     console.log.apply(null, arguments);
+  }
+}
+
+export const debugGroup = function () {
+  if (getSetting('debug', false)) {
+    console.groupCollapsed.apply(null, arguments);
+  }
+}
+export const debugGroupEnd = function () {
+  if (getSetting('debug', false)) {
+    console.groupEnd.apply(null, arguments);
   }
 }
