0e5feab3fa51ea36557f12103faa3d6870c93640
Justin Reynolds <justinr1234@gmail.com>
2018-01-25 12:56:40 -0600

    Add Enroll Account component and route

---
+++ packages/vulcan-accounts/imports/ui/components/EnrollAccount.jsx
@@ -0,0 +1,42 @@
+import { Components, registerComponent, withCurrentUser } from 'meteor/vulcan:core';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { intlShape } from 'meteor/vulcan:i18n';
+import { STATES } from '../../helpers.js';
+
+class AccountsEnrollAccount extends PureComponent {
+  componentDidMount() {
+    const token = this.props.params.token;
+    Accounts._loginButtonsSession.set('enrollAccountToken', token);
+  }
+
+  render() {
+    if (!this.props.currentUser) {
+      return (
+        <Components.AccountsLoginForm
+          formState={ STATES.ENROLL_ACCOUNT }
+        />
+      );
+    } else {
+      return (
+        <div className='password-reset-form'>
+          <div>{this.context.intl.formatMessage({id: 'accounts.info_password_changed'})}!</div>
+        </div>
+      );
+    }
+  }
+}
+
+AccountsEnrollAccount.contextTypes = {
+  intl: intlShape
+}
+
+AccountsEnrollAccount.propsTypes = {
+  currentUser: PropTypes.object,
+  params: PropTypes.object,
+};
+
+AccountsEnrollAccount.displayName = 'AccountsEnrollAccount';
+
+registerComponent('AccountsEnrollAccount', AccountsEnrollAccount, withCurrentUser);
