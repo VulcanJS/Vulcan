6facf15e17fec0eff0804d35360579513e7f586f
SachaG <358832+SachaG@users.noreply.github.com>
2018-01-27 13:11:41 +0900

    Super-hacky fix to the accounts setState issue

---
+++ packages/vulcan-accounts/imports/ui/components/StateSwitcher.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Components, registerComponent } from 'meteor/vulcan:core';
+import { Accounts } from 'meteor/accounts-base';
+
+import {
+  STATES
+} from '../../helpers.js';
+
+export class AccountsStateSwitcher extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const currentUser = props.currentUser;
+
+    this.state = {
+      formState: props.formState
+    }
+  }
+
+  switchToSignUp = (event) => {
+    event && event.preventDefault();
+    this.setState({
+      formState: STATES.SIGN_UP,
+    });
+    // this.clearMessages();
+  }
+
+  switchToSignIn = (event) => {
+    event && event.preventDefault();
+    this.setState({
+      formState: STATES.SIGN_IN,
+    });
+    // this.clearMessages();
+  }
+
+  switchToPasswordReset = (event) => {
+    event && event.preventDefault();
+    this.setState({
+      formState: STATES.PASSWORD_RESET,
+    });
+    // this.clearMessages();
+  }
+
+  switchToChangePassword = (event) => {
+    event && event.preventDefault();
+    this.setState({
+      formState: STATES.PASSWORD_CHANGE,
+    });
+    // this.clearMessages();
+  }
+
+  switchToSignOut = (event) => {
+    event && event.preventDefault();
+    this.setState({
+      formState: STATES.PROFILE,
+    });
+    // this.clearMessages();
+  }
+
+  cancelResetPassword = (event) => {
+    event && event.preventDefault();
+    Accounts._loginButtonsSession.set('resetPasswordToken', null);
+    this.setState({
+      formState: STATES.SIGN_IN,
+    });
+    // this.clearMessages();
+  }
+
+  switchToProfile = (event) => {
+    event && event.preventDefault();
+    this.setState({
+      formState: STATES.PROFILE,
+    });
+    // this.clearMessages();
+  }
+
+  render() {
+    const {
+      switchToSignUp,
+      switchToSignIn,
+      switchToPasswordReset,
+      switchToChangePassword,
+      switchToSignOut,
+      cancelResetPassword,
+      switchToProfile,
+    } = this;
+    
+    const handlers = {
+      switchToSignUp,
+      switchToSignIn,
+      switchToPasswordReset,
+      switchToChangePassword,
+      switchToSignOut,
+      cancelResetPassword,
+      switchToProfile,
+    }
+    return(
+      <Components.AccountsLoginFormInner formState={this.state.formState} handlers={handlers}/>
+    );
+  }
+}
+
+registerComponent('AccountsStateSwitcher', AccountsStateSwitcher);
