2eeb252bd53d7f087776546ebf021a0f7ee452ef
SachaG <358832+SachaG@users.noreply.github.com>
2018-02-12 18:49:22 +0900

    Let EditButton, NewButton pass through props to SmartForm

--- packages/vulcan-core/lib/modules/components/EditButton.jsx
+++ packages/vulcan-core/lib/modules/components/EditButton.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import { FormattedMessage, intlShape } from 'meteor/vulcan:i18n';
 
-const EditButton = ({ collection, document, bsStyle = 'primary' }, {intl}) =>
+const EditButton = ({ collection, document, bsStyle = 'primary', ...properties }, {intl}) =>
   <Components.ModalTrigger 
     label={intl.formatMessage({id: 'datatable.edit'})} 
     component={<Button bsStyle={bsStyle}><FormattedMessage id="datatable.edit" /></Button>}
   >
-    <Components.DatatableEditForm collection={collection} document={document} />
+    <Components.DatatableEditForm collection={collection} document={document} {...properties} />
   </Components.ModalTrigger>
 
 EditButton.contextTypes = {
