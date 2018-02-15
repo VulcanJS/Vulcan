466abb09e36e6b1c78d1c3a5bc29d914e41e123e
SachaG <358832+SachaG@users.noreply.github.com>
2018-02-15 12:05:47 +0900

    Add try…catch block to track

--- packages/vulcan-events/lib/modules/events.js
+++ packages/vulcan-events/lib/modules/events.js
@@ -16,7 +16,14 @@ export const addTrackFunction = f => {
 
 export const track = async (eventName, eventProperties, currentUser) => {
   for (let f of trackFunctions) {
-    await f(eventName, eventProperties, currentUser);
+    try {
+      await f(eventName, eventProperties, currentUser);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(`// ${f.name} track error`);
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
   }
 };
 
