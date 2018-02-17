c001d1e3372af585ec0d848e7ed48bde9cfd676e
SachaG <358832+SachaG@users.noreply.github.com>
2018-02-17 10:56:50 +0900

    Use proper user_id Intercom property

--- packages/vulcan-events-intercom/lib/client/intercom-client.js
+++ packages/vulcan-events-intercom/lib/client/intercom-client.js
@@ -18,40 +18,18 @@ function intercomIdentify(currentUser) {
     name: currentUser.displayName,
     email: currentUser.email,
     created_at: currentUser.createdAt,
-    _id: currentUser._id,
+    user_id: currentUser._id,
     pageUrl: currentUser.pageUrl,
   };
   (function() {
     var w = window;
     var ic = w.Intercom;
     if (typeof ic === 'function') {
-      ic('reattach_activator');
+      // ic('reattach_activator');
       // eslint-disable-next-line no-undef
       ic('update', intercomSettings);
     } else {
-      var d = document;
-      var i = function() {
-        i.c(arguments);
-      };
-      i.q = [];
-      i.c = function(args) {
-        i.q.push(args);
-      };
-      w.Intercom = i;
-      // eslint-disable-next-line no-inner-declarations
-      function l() {
-        var s = d.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = getSetting('intercom.widgetUrl', 'https://widget.intercom.io/widget/icygo7se');
-        var x = d.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-      }
-      if (w.attachEvent) {
-        w.attachEvent('onload', l);
-      } else {
-        w.addEventListener('load', l, false);
-      }
+      intercomInit();
     }
   })();
 }
