f74c15d9855f5e9d2d17cf6e6ec2053fd2f18b0f
Discordius <panisnecis@gmail.com>
2018-06-03 13:39:48 -0700

    recalculateBaseScore export

--- packages/vulcan-voting/lib/modules/index.js
+++ packages/vulcan-voting/lib/modules/index.js
@@ -3,6 +3,7 @@ export * from './make_voteable.js';
 export { withVote } from '../containers/withVote.js';
 export * from './helpers.js';
 export * from './vote.js';
+export * from './scoring.js';
 
 import './custom_fields.js';
 import './fragments.js';
