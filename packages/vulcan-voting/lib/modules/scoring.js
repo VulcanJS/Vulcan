f73ffe4d823bdd3440305a975c349b7f8da9535b
Discordius <panisnecis@gmail.com>
2018-06-03 13:39:23 -0700

    Added frontpage bonus for LW

--- packages/vulcan-voting/lib/modules/scoring.js
+++ packages/vulcan-voting/lib/modules/scoring.js
@@ -19,8 +19,14 @@ export const recalculateScore = item => {
     // time decay factor
     const TIME_DECAY_FACTOR = 1.15; //LW: Set this to 1.15 from 1.3 for LW purposes (want slower decay)
 
+    // Basescore bonuses for various categories
+    const FRONTPAGE_BONUS = 10;
+    const FEATURED_BONUS = 10;
+
     // use baseScore if defined, if not just use 0
-    const baseScore = item.baseScore || 0;
+    let baseScore = item.baseScore || 0;
+
+    baseScore = baseScore + ((item.frontpageDate ? FRONTPAGE_BONUS : 0) + (item.curatedDate ? FEATURED_BONUS : 0));
 
     // HN algorithm
     const newScore = Math.round((baseScore / Math.pow(ageInHours + 2, TIME_DECAY_FACTOR))*1000000)/1000000;
