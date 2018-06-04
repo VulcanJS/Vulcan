543e04697b33722687c8bab8f1039ce2c3c3e2da
Discordius <panisnecis@gmail.com>
2018-06-04 11:30:36 -0700

    Added missing imports (PR Worthy)

--- packages/vulcan-voting/lib/server/main.js
+++ packages/vulcan-voting/lib/server/main.js
@@ -1,6 +1,8 @@
 import './graphql.js';
 import './cron.js';
 import './scoring.js';
+import './callbacks.js';
 import './indexes.js';
 
 export * from '../modules/index.js';
+export * from './scoring.js';
