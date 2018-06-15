e783d51dca45dede899069ba46dbc1d95b61b5d5
SachaG <358832+SachaG@users.noreply.github.com>
2018-06-16 08:57:45 +0900

    v1.11.2

--- packages/vulcan-debug/package.js
+++ packages/vulcan-debug/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "vulcan:debug",
   summary: "Vulcan debug package",
-  version: '1.11.1',
+  version: '1.11.2',
   git: "https://github.com/VulcanJS/Vulcan.git",
   debugOnly: true
 });
@@ -17,8 +17,8 @@ Package.onUse(function (api) {
 
     // Vulcan packages
 
-    'vulcan:lib@1.11.1',
-    'vulcan:email@1.11.1',
+    'vulcan:lib@1.11.2',
+    'vulcan:email@1.11.2',
 
   ]);
 
--- packages/vulcan-routing/package.js
+++ packages/vulcan-routing/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "vulcan:routing",
   summary: "Vulcan router package",
-  version: '1.11.1',
+  version: '1.11.2',
   git: "https://github.com/VulcanJS/Vulcan.git"
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.versionsFrom('1.6.1');
 
   api.use([
-    'vulcan:lib@1.11.1',
+    'vulcan:lib@1.11.2',
   ]);
 
   api.mainModule('lib/server/main.js', 'server');
