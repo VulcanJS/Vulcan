193da8588729dba28c4ab3b2955adece59705769
SachaG <358832+SachaG@users.noreply.github.com>
2018-06-20 07:45:04 +0900

    Wrap checkboxgroup (fix #2004)

--- packages/vulcan-ui-bootstrap/lib/components/forms/Checkboxgroup.jsx
+++ packages/vulcan-ui-bootstrap/lib/components/forms/Checkboxgroup.jsx
@@ -3,24 +3,27 @@ import { Checkbox } from 'formsy-react-components';
 import { registerComponent } from 'meteor/vulcan:core';
 
 // note: treat checkbox group the same as a nested component, using `path`
-const CheckboxGroupComponent = ({ refFunction, path, value, updateCurrentValues, inputProperties }) =>
-  inputProperties.options.map((option, i) => (
-    <Checkbox
-      key={i}
-      {...inputProperties}
-      label={option.label}
-      value={value.includes(option.value)}
-      ref={refFunction}
-      onChange={(name, isChecked) => {
-        // give each individual checkbox its own path
-        const checkboxPath = `${path}.${i}`;
-        if (isChecked) {
-          updateCurrentValues({ [checkboxPath]: option.value });
-        } else {
-          updateCurrentValues({ [checkboxPath]: null });
-        }
-      }}
-    />
-  ));
+const CheckboxGroupComponent = ({ refFunction, path, value, updateCurrentValues, inputProperties }) => (
+  <div>
+    {inputProperties.options.map((option, i) => (
+      <Checkbox
+        key={i}
+        {...inputProperties}
+        label={option.label}
+        value={value.includes(option.value)}
+        ref={refFunction}
+        onChange={(name, isChecked) => {
+          // give each individual checkbox its own path
+          const checkboxPath = `${path}.${i}`;
+          if (isChecked) {
+            updateCurrentValues({ [checkboxPath]: option.value });
+          } else {
+            updateCurrentValues({ [checkboxPath]: null });
+          }
+        }}
+      />
+    ))}
+  </div>
+);
 
 registerComponent('FormComponentCheckboxGroup', CheckboxGroupComponent);
