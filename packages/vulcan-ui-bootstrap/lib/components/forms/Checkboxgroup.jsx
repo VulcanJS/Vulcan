d94a024973cbb174e95b20388c1a303ae1e942c3
SachaG <358832+SachaG@users.noreply.github.com>
2018-08-05 21:16:38 +0900

    Refactor Checkboxgroup to full function syntax

--- packages/vulcan-ui-bootstrap/lib/components/forms/Checkboxgroup.jsx
+++ packages/vulcan-ui-bootstrap/lib/components/forms/Checkboxgroup.jsx
@@ -4,26 +4,28 @@ import { registerComponent } from 'meteor/vulcan:core';
 import without from 'lodash/without';
 
 // note: treat checkbox group the same as a nested component, using `path`
-const CheckboxGroupComponent = ({ refFunction, label, path, value, updateCurrentValues, inputProperties }) => (
-  <div className="form-group row">
-    <label className="control-label col-sm-3">{label}</label>
-    <div className="col-sm-9">
-    {inputProperties.options.map((option, i) => (
-      <Checkbox
-        layout="elementOnly"
-        key={i}
-        {...inputProperties}
-        label={option.label}
-        value={value.includes(option.value)}
-        ref={refFunction}
-        onChange={(name, isChecked) => {
-          const newValue = isChecked ? [...value, option.value] : without(value, option.value);
-          updateCurrentValues({ [path]: newValue });
-        }}
-      />
-    ))}
+const CheckboxGroupComponent = ({ refFunction, label, path, value, updateCurrentValues, inputProperties }) => {
+  return (
+    <div className="form-group row">
+      <label className="control-label col-sm-3">{label}</label>
+      <div className="col-sm-9">
+        {inputProperties.options.map((option, i) => (
+          <Checkbox
+            layout="elementOnly"
+            key={i}
+            {...inputProperties}
+            label={option.label}
+            value={value.includes(option.value)}
+            ref={refFunction}
+            onChange={(name, isChecked) => {
+              const newValue = isChecked ? [...value, option.value] : without(value, option.value);
+              updateCurrentValues({ [path]: newValue });
+            }}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 registerComponent('FormComponentCheckboxGroup', CheckboxGroupComponent);
