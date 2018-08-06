e27f5af9e1d5c42b0450bc044338fa8dcb2ce7b6
SachaG <358832+SachaG@users.noreply.github.com>
2018-08-06 10:30:15 +0900

    Make Checkboxgroup and Select handle their own value formatting

--- packages/vulcan-ui-bootstrap/lib/components/forms/Select.jsx
+++ packages/vulcan-ui-bootstrap/lib/components/forms/Select.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { intlShape } from 'meteor/vulcan:i18n';
 import { Select } from 'formsy-react-components';
 import { registerComponent } from 'meteor/vulcan:core';
+import { getFieldType } from 'meteor/vulcan:forms';
 
-const SelectComponent = ({refFunction, inputProperties, ...properties}, { intl }) => {
+const SelectComponent = ({refFunction, inputProperties, datatype, ...properties}, { intl }) => {
   const noneOption = {
     label: intl.formatMessage({ id: 'forms.select_option' }),
-    value: '',
+    value: getFieldType(datatype) === String ? '' : null, // depending on field type, empty value can be '' or null
     disabled: true,
   };
   let otherOptions = Array.isArray(inputProperties.options) && inputProperties.options.length ? inputProperties.options : [];
-  // uncomment following to convert options values to strings
-  // otherOptions = otherOptions.map(({ label, value }) => ({ label, value: value.toString()}));
   const options = [noneOption, ...otherOptions];
   return <Select {...inputProperties} options={options} ref={refFunction}/>
 };
