d9784ad0aa6602e3378daf241607dcc636152769
SachaG <358832+SachaG@users.noreply.github.com>
2018-08-07 15:37:51 +0900

    Improve StaticText form component with basic URL parsing

--- packages/vulcan-ui-bootstrap/lib/components/forms/StaticText.jsx
+++ packages/vulcan-ui-bootstrap/lib/components/forms/StaticText.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { registerComponent } from 'meteor/vulcan:core';
 
+const parseUrl = value => {
+  return value.slice(0,4) === 'http' ? <a href={value} target="_blank">{value}</a> : value;
+}
+
 const StaticComponent = ({ value, label }) => (
   <div className="form-group row">
     <label className="control-label col-sm-3">{label}</label>
-    <div className="col-sm-9">{value}</div>
+    <div className="col-sm-9">{parseUrl(value)}</div>
   </div>
 );
 
