4c4f11b1a023627ad0f8c41dc10d92422156ea87
Apollinaire <apollinaire.lecocq@live.fr>
2018-08-17 16:28:41 +0200

    re-establish vulcan's custom enroll and reset urls
    since https://github.com/VulcanJS/Vulcan/commit/f3c1f7bf3191742525c5d8408662ab68ea8a18f4 the urls sent in the resetPassword and enrollment emails were the Meteor default one, `/#/reset-password`

--- packages/vulcan-users/lib/server/urls.js
+++ packages/vulcan-users/lib/server/urls.js
@@ -1,4 +1,7 @@
-if (typeof Accounts !== 'undefined') {
-  Accounts.urls.resetPassword = (token) => Meteor.absoluteUrl(`reset-password/${token}`);
-  Accounts.urls.enrollAccount = (token) => Meteor.absoluteUrl(`enroll-account/${token}`);
-}
+Meteor.startup(() => {
+  console.log(typeof Accounts);
+  if (typeof Accounts !== 'undefined') {
+    Accounts.urls.resetPassword = token => Meteor.absoluteUrl(`reset-password/${token}`);
+    Accounts.urls.enrollAccount = token => Meteor.absoluteUrl(`enroll-account/${token}`);
+  }
+});
