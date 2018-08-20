944b57df76fdf97cf97781cba05ecca03fc4f8b9
Apollinaire <apollinaire.lecocq@live.fr>
2018-08-20 12:39:26 +0200

    remove console.log

--- packages/vulcan-users/lib/server/urls.js
+++ packages/vulcan-users/lib/server/urls.js
@@ -1,5 +1,4 @@
 Meteor.startup(() => {
-  console.log(typeof Accounts);
   if (typeof Accounts !== 'undefined') {
     Accounts.urls.resetPassword = token => Meteor.absoluteUrl(`reset-password/${token}`);
     Accounts.urls.enrollAccount = token => Meteor.absoluteUrl(`enroll-account/${token}`);
