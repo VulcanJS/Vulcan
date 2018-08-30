7b12cf6e66c9756dabd73c990e2bacfe207c9d21
Raymond Arnold <rarnold@spotify.com>
2018-08-30 14:30:14 -0700

    Fix console errors

--- packages/vulcan-forms/lib/components/FormGroup.jsx
+++ packages/vulcan-forms/lib/components/FormGroup.jsx
@@ -72,7 +72,7 @@ class FormGroup extends PureComponent {
     const { classes, name, defaultStyle, flexStyle} = this.props
     const groupStyling = !(name == 'default' || defaultStyle)
     return (
-      <div className={groupStyling && classes.formSection}>
+      <div className={classNames({[classes.formSection]: groupStyling})}>
         { groupStyling && this.renderHeading()}
         { (!this.state.collapsed || this.hasErrors()) &&
           <div className={classNames(classes.formSectionFields, {[classes.formSectionBody]: groupStyling, [classes.flex]: flexStyle})}
