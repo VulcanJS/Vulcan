17f96712ff44ce9c86ebda0e14407a1f2f0d90f4
Eric Burel <e.burel.contact@gmail.com>
2018-09-05 16:00:07 +0200

    cleaned up the options management in hocs
    Will fix issues when not specifying a fragmentName in withDelete

--- packages/vulcan-core/lib/modules/containers/withMulti.js
+++ packages/vulcan-core/lib/modules/containers/withMulti.js
@@ -34,43 +34,34 @@ Terms object can have the following properties:
          
 */
 
-import React, { Component } from 'react';
-import { withApollo, graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import update from 'immutability-helper';
-import { getSetting, getFragment, getFragmentName, getCollection, Utils, multiClientTemplate } from 'meteor/vulcan:lib';
-import Mingo from 'mingo';
-import compose from 'recompose/compose';
-import withState from 'recompose/withState';
-import find from 'lodash/find';
+import React, { Component } from "react";
+import { withApollo, graphql } from "react-apollo";
+import gql from "graphql-tag";
+import update from "immutability-helper";
+import { getSetting, getFragment, getFragmentName, getCollection, Utils, multiClientTemplate } from "meteor/vulcan:lib";
+import Mingo from "mingo";
+import compose from "recompose/compose";
+import withState from "recompose/withState";
+import find from "lodash/find";
+
+import { extractCollectionInfo, extractFragmentInfo } from "./handleOptions";
 
 export default function withMulti(options) {
   // console.log(options)
 
   const {
-    collectionName,
     limit = 10,
-    pollInterval = getSetting('pollInterval', 20000),
+    pollInterval = getSetting("pollInterval", 20000),
     enableTotal = true,
     enableCache = false,
-    extraQueries,
+    extraQueries
   } = options;
 
-  const collection = options.collection || getCollection(collectionName);
+  const { collectionName, collection } = extractCollectionInfo(options);
+  const { fragmentName, fragment } = extractFragmentInfo(options, collectionName);
+
   const typeName = collection.options.typeName;
   const resolverName = Utils.camelCaseify(Utils.pluralize(typeName));
-
-  let fragment;
-
-  if (options.fragment) {
-    fragment = options.fragment;
-  } else if (options.fragmentName) {
-    fragment = getFragment(options.fragmentName);
-  } else {
-    fragment = getFragment(`${collection.options.collectionName}DefaultFragment`);
-  }
-
-  const fragmentName = getFragmentName(fragment);
 
   // build graphql query from options
   const query = gql`
@@ -83,12 +74,12 @@ export default function withMulti(options) {
     withApollo,
 
     // wrap component with HoC that manages the terms object via its state
-    withState('paginationTerms', 'setPaginationTerms', props => {
+    withState("paginationTerms", "setPaginationTerms", props => {
       // get initial limit from props, or else options
       const paginationLimit = (props.terms && props.terms.limit) || limit;
       const paginationTerms = {
         limit: paginationLimit,
-        itemsPerPage: paginationLimit,
+        itemsPerPage: paginationLimit
       };
 
       return paginationTerms;
@@ -111,8 +102,8 @@ export default function withMulti(options) {
               input: {
                 terms: mergedTerms,
                 enableCache,
-                enableTotal,
-              },
+                enableTotal
+              }
             },
             // note: pollInterval can be set to 0 to disable polling (20s by default)
             pollInterval,
@@ -127,7 +118,7 @@ export default function withMulti(options) {
                 resolverName,
                 apolloClient
               );
-            },
+            }
           };
 
           if (options.fetchPolicy) {
@@ -154,7 +145,7 @@ export default function withMulti(options) {
             loading = props.data.networkStatus === 1,
             loadingMore = props.data.networkStatus === 2,
             error = props.data.error,
-            propertyName = options.propertyName || 'results';
+            propertyName = options.propertyName || "results";
 
           if (error) {
             // eslint-disable-next-line no-console
@@ -178,10 +169,10 @@ export default function withMulti(options) {
             loadMore(providedTerms) {
               // if new terms are provided by presentational component use them, else default to incrementing current limit once
               const newTerms =
-                typeof providedTerms === 'undefined'
+                typeof providedTerms === "undefined"
                   ? {
                       /*...props.ownProps.terms,*/ ...props.ownProps.paginationTerms,
-                      limit: results.length + props.ownProps.paginationTerms.itemsPerPage,
+                      limit: results.length + props.ownProps.paginationTerms.itemsPerPage
                     }
                   : providedTerms;
 
@@ -193,7 +184,7 @@ export default function withMulti(options) {
             loadMoreInc(providedTerms) {
               // get terms passed as argument or else just default to incrementing the offset
               const newTerms =
-                typeof providedTerms === 'undefined'
+                typeof providedTerms === "undefined"
                   ? { ...props.ownProps.terms, ...props.ownProps.paginationTerms, offset: results.length }
                   : providedTerms;
 
@@ -208,16 +199,16 @@ export default function withMulti(options) {
                   newResults[resolverName] = [...previousResults[resolverName], ...fetchMoreResult.data[resolverName]];
                   // return the previous results "augmented" with more
                   return { ...previousResults, ...newResults };
-                },
+                }
               });
             },
 
             fragmentName,
             fragment,
             ...props.ownProps, // pass on the props down to the wrapped component
-            data: props.data,
+            data: props.data
           };
-        },
+        }
       }
     )
   );
@@ -243,7 +234,7 @@ const queryReducer = (typeName, previousResults, action, collection, mergedTerms
     const listWithoutDocument = data[resolverName].results.filter(doc => doc._id !== document._id);
     const currentTotalCount = data[resolverName].totalCount;
     const newResults = update(data, {
-      [resolverName]: { $set: { results: listWithoutDocument, totalCount: currentTotalCount - 1 } },
+      [resolverName]: { $set: { results: listWithoutDocument, totalCount: currentTotalCount - 1 } }
     });
     return newResults;
   };
@@ -253,7 +244,7 @@ const queryReducer = (typeName, previousResults, action, collection, mergedTerms
     const listWithDocument = [...data[resolverName].results, document];
     const currentTotalCount = data[resolverName].totalCount;
     const newResults = update(data, {
-      [resolverName]: { $set: { results: listWithDocument, totalCount: currentTotalCount + 1 } },
+      [resolverName]: { $set: { results: listWithDocument, totalCount: currentTotalCount + 1 } }
     });
     return newResults;
   };
@@ -332,7 +323,7 @@ const queryReducer = (typeName, previousResults, action, collection, mergedTerms
     [resolverName]: {
       results: [...newResults[resolverName].results],
       totalCount: newResults[resolverName].totalCount,
-      __typename: `Multi${typeName}Output`,
-    },
+      __typename: `Multi${typeName}Output`
+    }
   };
 };
--- packages/vulcan-core/lib/modules/containers/withUpsert.js
+++ packages/vulcan-core/lib/modules/containers/withUpsert.js
@@ -27,50 +27,50 @@ Child Props:
   
 */
 
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import { getFragment, getFragmentName, getCollection, upsertClientTemplate } from 'meteor/vulcan:core';
-import clone from 'lodash/clone';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { upsertClientTemplate } from "meteor/vulcan:core";
+import clone from "lodash/clone";
 
-const withUpsert = (options) => {
+import { extractCollectionInfo, extractFragmentInfo } from "./handleOptions";
 
-  const { collectionName } = options;
-  // get options
-  const collection = options.collection || getCollection(collectionName);
-  const fragment = options.fragment || getFragment(options.fragmentName || `${collectionName}DefaultFragment`);
-  const fragmentName = getFragmentName(fragment);
+const withUpsert = options => {
+  const { collectionName, collection } = extractCollectionInfo(options);
+  const { fragmentName, fragment } = extractFragmentInfo(options, collectionName);
+
   const typeName = collection.options.typeName;
-  const query = gql`${upsertClientTemplate({ typeName, fragmentName })}${fragment}`;
+  const query = gql`
+    ${upsertClientTemplate({ typeName, fragmentName })}
+    ${fragment}
+  `;
 
   return graphql(query, {
     alias: `withUpsert${typeName}`,
     props: ({ ownProps, mutate }) => ({
-
-      [`upsert${typeName}`]: (args) => {
+      [`upsert${typeName}`]: args => {
         const { selector, data } = args;
-        return mutate({ 
+        return mutate({
           variables: { selector, data }
           // note: updateQueries is not needed for editing documents
         });
       },
 
       // OpenCRUD backwards compatibility
-      upsertMutation: (args) => {
+      upsertMutation: args => {
         const { selector, set, unset } = args;
         const data = clone(set);
-        unset && Object.keys(unset).forEach(fieldName => {
-          data[fieldName] = null;
-        });
-        return mutate({ 
+        unset &&
+          Object.keys(unset).forEach(fieldName => {
+            data[fieldName] = null;
+          });
+        return mutate({
           variables: { selector, data }
           // note: updateQueries is not needed for editing documents
         });
       }
-
-    }),
+    })
   });
-
-}
+};
 
 export default withUpsert;
