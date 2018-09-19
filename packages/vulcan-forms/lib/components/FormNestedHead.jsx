7e66908d2194546ae99d60397c25e425491392f4
Erik Dakoda <erik@qeebi.com>
2018-09-19 13:16:48 -0400

    Refactoring to remove react-bootstrap and to customize FormNestedArray for material-ui

---
+++ packages/vulcan-forms/lib/components/FormNestedHead.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { registerComponent } from 'meteor/vulcan:core';
+import { FormattedMessage } from 'meteor/vulcan:i18n';
+
+const FormNestedHead = ({ label, addItem }) => (
+  <label className="control-label col-sm-3">{label}</label>
+);
+
+FormNestedHead.propTypes = {
+  label: PropTypes.string,
+  addItem: PropTypes.func,
+};
+
+registerComponent('FormNestedHead', FormNestedHead);
