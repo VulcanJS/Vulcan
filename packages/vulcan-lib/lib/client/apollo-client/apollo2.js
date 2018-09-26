bbb9263c27a90d420ff1cb18b67d2c912412ccd3
Eric Burel <e.burel.contact@gmail.com>
2018-09-26 15:36:24 +0200

    experiment with reloading the client when adding a mutation

--- packages/vulcan-lib/lib/client/apollo-client/apollo2.js
+++ packages/vulcan-lib/lib/client/apollo-client/apollo2.js
@@ -1,20 +1,37 @@
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
-import watchedMutationLink from './links/watchedMutation'
-import errorLink from './links/error'
-import httpLink from './links/http'
-import meteorAccountsLink from './links/meteor'
-import stateLink from './links/state'
-import cache from './cache'
+import watchedMutationLink from './links/watchedMutation';
+import errorLink from './links/error';
+import httpLink from './links/http';
+import meteorAccountsLink from './links/meteor';
+import { getStateLink } from './links/state';
+import cache from './cache';
+import { createApolloClient } from '../../modules/apollo';
 
+// these links do not change once created
+const staticLinks = [watchedMutationLink, errorLink, meteorAccountsLink, httpLink];
 
-export const apolloClient = new ApolloClient({
-  link: ApolloLink.from([
-    stateLink,
-    watchedMutationLink,
-    errorLink,
-    meteorAccountsLink,
-    httpLink
-  ]),
-  cache,
+const initialClient = new ApolloClient({
+  link: ApolloLink.from([getStateLink(), ...staticLinks]),
+  cache
 });
+
+let apolloClient = initialClient;
+
+// client is only available through this helper so we can update it
+export const getApolloClient = () => apolloClient;
+
+// TODO: try this
+//@see https://github.com/apollographql/apollo-link-state/issues/306
+export const reloadApolloClient = () => {
+  // get the current cache
+  const currentCache = apolloClient.cache;
+  // get the stateLink
+  const newApolloClient = createApolloClient({
+    link: ApolloLink.from([getStateLink(), ...staticLinks]),
+    cache: currentCache
+  });
+  // update the client
+  apolloClient = newApolloClient;
+  return newApolloClient;
+};
--- packages/vulcan-lib/lib/client/apollo-client/links/state.js
+++ packages/vulcan-lib/lib/client/apollo-client/links/state.js
@@ -1,26 +1,81 @@
 /**
  * Setup apollo-link-state
- * Apollo-link-state helps to manage a local store for caching and client-side 
+ * Apollo-link-state helps to manage a local store for caching and client-side
  * data storing
  * It replaces previous implementation using redux
  * Link state doc:
+ * @see https://www.apollographql.com/docs/react/essentials/local-state.html
  * @see https://www.apollographql.com/docs/link/links/state.html
  * General presentation on Links
  * @see https://www.apollographql.com/docs/link/
  * Example
  * @see https://hackernoon.com/storing-local-state-in-react-with-apollo-link-state-738f6ca45569
  */
-import { withClientState } from 'apollo-link-state'
-import cache from '../cache'
+import { withClientState } from 'apollo-link-state';
+import cache from '../cache';
+import { getDefaultResolvers } from '../../../../../vulcan-core/lib/client/main';
 
-const createStateLink = ({ resolvers, defaults }) => {
-    const stateLink = withClientState({
-        cache,
-        defaults,
-        resolvers,
-    })
-    return stateLink
-}
+/**
+ * Create a state link
+ * We allow to load an existing cache
+ */
+const createStateLink = ({ cache: providedCache = cache, resolvers, defaults }) => {
+  const stateLink = withClientState({
+    cache: providedCache,
+    defaults,
+    resolvers
+  });
+  return stateLink;
+};
+const initialStateLink = createStateLink();
 
-export default createStateLink({})
+let stateLink = initialStateLink;
+export const getStateLink = () => stateLink;
 
+export const reloadStateLink = ({ cache: providedCache }) => {
+  const newStateLink = createStateLink({
+    cache: providedCache,
+    defaults: getDefaults(),
+    resolvers: getResolvers()
+  });
+  stateLink = newStateLink;
+  return newStateLink;
+};
+// TODO: need to find the equivalent of "replaceReducer" for the apolloClient or
+// the stateLink
+//@see https://github.com/apollographql/apollo-link-state/issues/306
+
+// enhancement workflow
+const registeredDefaults = {};
+/**
+ * Defaults are default response to queries
+ */
+export const addDefault = ({ name, defaultValue, options = {} }) => {
+  registeredDefaults[name] = defaultValue;
+  return registeredDefaults;
+};
+export const getDefaults = () => registeredDefaults;
+
+// Mutation are equivalent to a Redux Action + Reducer
+// except it uses GraphQL
+const registeredMutations = {};
+export const addMutation = ({ name, mutation, options = {} }) => {
+  registeredMutations[name] = mutation;
+  return registeredMutations;
+};
+export const getMutations = () => registeredMutations;
+// Queries are equivalent to Redux concept of selector
+// except it uses GraphQL
+const registeredQueries = {};
+export const addQuery = ({ name, query, options = {} }) => {
+  registeredQueries[name] = query;
+  return registeredQueries;
+};
+export const getQueries = () => registeredQueries;
+
+export const getResolvers = () => ({
+  Mutation: getMutations(),
+  Query: getQueries()
+});
+
+export default createStateLink({});
