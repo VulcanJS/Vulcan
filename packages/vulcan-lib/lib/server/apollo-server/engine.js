b9b1b96a6e4857bd614fe691df7ce3dbb5b377d5
Eric Burel <e.burel.contact@gmail.com>
2018-10-05 16:03:54 +0200

    fix when engine api key is not defined

--- packages/vulcan-lib/lib/server/apollo-server/engine.js
+++ packages/vulcan-lib/lib/server/apollo-server/engine.js
@@ -3,6 +3,8 @@ import { getSetting } from '../../modules/settings.js';
 export const engineApiKey = process.env.ENGINE_API_KEY || getSetting('apolloEngine.apiKey');
 // options now available:
 // @see https://www.apollographql.com/docs/apollo-server/api/apollo-server.html#EngineReportingOptions
-export const engineConfig = {
-  apiKey: engineApiKey
-};
+export const engineConfig = engineApiKey
+  ? {
+      apiKey: engineApiKey
+    }
+  : undefined;
