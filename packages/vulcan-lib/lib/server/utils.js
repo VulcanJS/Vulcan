de1c13b3d14cb50cc13c3b1019bc638f16b4ffb1
Apollinaire <apollinaire.lecocq@live.fr>
2018-10-09 15:09:30 +0200

    Revert changes made to getUnusedSlug in #2075

--- packages/vulcan-lib/lib/server/utils.js
+++ packages/vulcan-lib/lib/server/utils.js
@@ -13,20 +13,4 @@ Utils.sanitize = function(s) {
   });
 };
 
-Utils.getUnusedSlug = async function (collection, slug) {
-  let suffix = '';
-  let index = 0;
-
-  const slugRegex = new RegExp('^' + slug + '-[0-9]+$');
-  // get all the slugs matching slug or slug-123 in that collection
-  const results = await Connectors.find( collection, { slug: { $in: [slug, slugRegex] } }, { fields: { slug: 1, _id: 0 } });
-  const usedSlugs = results.map(item => item.slug);
-  // increment the index at the end of the slug until we find an unused one
-  while (usedSlugs.indexOf(slug + suffix) !== -1) {
-    index++;
-    suffix = '-' + index;
-  }
-  return slug + suffix;
-};
-
 export { Utils };
