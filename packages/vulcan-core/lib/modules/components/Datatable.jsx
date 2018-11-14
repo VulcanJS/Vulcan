71628705646f3539a9489bad52d69f91fd8d5965
eric-burel <e.burel.contact@gmail.com>
2018-11-14 12:20:49 +0100

    split Datatable into purely visual components

--- packages/vulcan-core/lib/modules/components/Datatable.jsx
+++ packages/vulcan-core/lib/modules/components/Datatable.jsx
@@ -30,7 +30,7 @@ class Datatable extends PureComponent {
       value: '',
       query: '',
       currentSort: {}
-    }
+    };
   }
 
   toggleSort = column => {
@@ -46,7 +46,7 @@ class Datatable extends PureComponent {
   }
 
   updateQuery(e) {
-    e.persist()
+    e.persist();
     e.preventDefault();
     this.setState({
       value: e.target.value
@@ -70,7 +70,7 @@ class Datatable extends PureComponent {
       const options = {
         collection,
         ...this.props.options
-      }
+      };
 
       const DatatableWithMulti = withMulti(options)(Components.DatatableContents);
 
@@ -82,11 +82,11 @@ class Datatable extends PureComponent {
       const orderBy = Object.keys(this.state.currentSort).length == 0 ? {} : { ...this.state.currentSort, _id: -1 };
 
       return (
-        <div className={`datatable datatable-${collection.options.collectionName}`}>
+        <Components.DatatableLayout collectionName={collection.options.collectionName}>
           <Components.DatatableAbove {...this.props} collection={collection} canInsert={canInsert} value={this.state.value} updateQuery={this.updateQuery} />
           <DatatableWithMulti {...this.props} collection={collection} terms={{query: this.state.query, orderBy: orderBy }} currentUser={this.props.currentUser} toggleSort={this.toggleSort} currentSort={this.state.currentSort}/>
-        </div>
-      )
+        </Components.DatatableLayout>
+      );
     }
   }
 }
@@ -103,16 +103,22 @@ Datatable.propTypes = {
   newFormOptions: PropTypes.object,
   editFormOptions: PropTypes.object,
   emptyState: PropTypes.object,
-}
+};
 
 Datatable.defaultProps = {
   showNew: true,
   showEdit: true,
   showSearch: true,
-}
+};
 registerComponent('Datatable', Datatable, withCurrentUser);
-export default Datatable
+export default Datatable;
 
+const DatatableLayout = ({ collectionName, children }) => {
+  <div className={`datatable datatable-${collectionName}`}>
+    {children}
+  </div>;
+};
+registerComponent({ name: 'DatatableLayout', component: DatatableLayout });
 
 /*
 
@@ -123,9 +129,9 @@ const DatatableAbove = (props, { intl }) => {
   const { collection, currentUser, showSearch, showNew, canInsert, value, updateQuery, options, newFormOptions } = props;
 
   return (
-    <div className="datatable-above">
+    <Components.DatatableAboveLayout>
       {showSearch && (
-        <input
+        <Components.DatatableAboveSearchInput
           className="datatable-search form-control"
           placeholder={`${intl.formatMessage({ id: 'datatable.search', defaultMessage: 'Search' })}…`}
           type="text"
@@ -135,13 +141,28 @@ const DatatableAbove = (props, { intl }) => {
         />
       )}
       {showNew && canInsert && <Components.NewButton collection={collection} currentUser={currentUser} mutationFragmentName={options && options.fragmentName} {...newFormOptions}/>}
-    </div>
-  )
-}
+    </Components.DatatableAboveLayout>
+  );
+};
 DatatableAbove.contextTypes = {
   intl: intlShape,
 };
 registerComponent('DatatableAbove', DatatableAbove);
+
+const DatatableAboveSearchInput = (props) => (
+  <input
+    {...props}
+  />
+);
+registerComponent({ name: 'DatatableAboveSearchInput', component: DatatableAboveSearchInput });
+
+const DatatableAboveLayout = ({ children }) => (
+  <div className="datatable-above">
+    {children}
+  </div>
+);
+registerComponent({ name: 'DatatablAboveLayout', component: DatatableAboveLayout });
+
   
 /*
 
@@ -169,43 +190,50 @@ const DatatableHeader = ({ collection, column, toggleSort, currentSort }, { intl
 
     // if sortable is a string, use it as the name of the property to sort by. If it's just `true`, use column.name
     const sortPropertyName = typeof column.sortable === 'string' ? column.sortable : column.name;
-    return column.sortable ? <Components.DatatableSorter name={sortPropertyName} label={formattedLabel} toggleSort={toggleSort} currentSort={currentSort} sortable={column.sortable}/> : <th>{formattedLabel}</th>;
+    return column.sortable 
+    ? <Components.DatatableSorter name={sortPropertyName} label={formattedLabel} toggleSort={toggleSort} currentSort={currentSort} sortable={column.sortable}/> 
+    : <Components.DatatableHeaderCellLayout>{formattedLabel}</Components.DatatableHeaderCellLayout>;
 
   } else {
 
     const formattedLabel = intl.formatMessage({ id: columnName, defaultMessage: columnName });
-    return <th className={`datatable-th-${columnName.toLowerCase().replace(/\s/g,'-')}`}>{formattedLabel}</th>;
-
+    return (
+      <Components.DatatableHeaderCellLayout className={`datatable-th-${columnName.toLowerCase().replace(/\s/g, '-')}`}>
+        {formattedLabel}
+      </Components.DatatableHeaderCellLayout>
+    );
   }
-}
-
+};
 DatatableHeader.contextTypes = {
   intl: intlShape
 };
-
 registerComponent('DatatableHeader', DatatableHeader);
+
+const DatatableHeaderCellLayout = ({ children, ...otherProps }) => (
+  <th {...otherProps}>{children}</th>
+);
+registerComponent({ name: 'DatatableHeaderCellLayout', component: DatatableHeaderCellLayout });
 
 const SortNone = () =>
   <svg width='16' height='16' viewBox='0 0 438 438' fill='none' xmlns='http://www.w3.org/2000/svg'>
     <path d='M25.7368 247.243H280.263C303.149 247.243 314.592 274.958 298.444 291.116L171.18 418.456C161.128 428.515 144.872 428.515 134.926 418.456L7.55631 291.116C-8.59221 274.958 2.85078 247.243 25.7368 247.243ZM298.444 134.884L171.18 7.54408C161.128 -2.51469 144.872 -2.51469 134.926 7.54408L7.55631 134.884C-8.59221 151.042 2.85078 178.757 25.7368 178.757H280.263C303.149 178.757 314.592 151.042 298.444 134.884Z' transform='translate(66 6)' fill='#000' fillOpacity='0.2' />
-  </svg>
+  </svg>;
 
 const SortDesc = () =>
   <svg width="16" height="16" viewBox="0 0 438 438" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M25.7368 0H280.263C303.149 0 314.592 27.7151 298.444 43.8734L171.18 171.213C161.128 181.272 144.872 181.272 134.926 171.213L7.55631 43.8734C-8.59221 27.7151 2.85078 0 25.7368 0Z" transform="translate(66 253.243)" fill="black" fillOpacity="0.7"/>
     <path d="M171.18 7.54408L298.444 134.884C314.592 151.042 303.149 178.757 280.263 178.757H25.7368C2.85078 178.757 -8.59221 151.042 7.55631 134.884L134.926 7.54408C144.872 -2.51469 161.128 -2.51469 171.18 7.54408Z" transform="translate(66 6)" fill="black" fillOpacity="0.2"/>
-  </svg>
+  </svg>;
 
 const SortAsc = () =>
   <svg width="16" height="16" viewBox="0 0 438 438" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M298.444 134.884L171.18 7.54408C161.128 -2.51469 144.872 -2.51469 134.926 7.54408L7.55631 134.884C-8.59221 151.042 2.85078 178.757 25.7368 178.757H280.263C303.149 178.757 314.592 151.042 298.444 134.884Z" transform="translate(66 6)" fill="black" fillOpacity="0.7"/>
     <path d="M280.263 0H25.7368C2.85078 0 -8.59221 27.7151 7.55631 43.8734L134.926 171.213C144.872 181.272 161.128 181.272 171.18 171.213L298.444 43.8734C314.592 27.7151 303.149 0 280.263 0Z" transform="translate(66 253.243)" fill="black" fillOpacity="0.2"/>
-  </svg>
+  </svg>;
 
 const DatatableSorter = ({ name, label, toggleSort, currentSort }) => 
-
   <th>
-    <div className="datatable-sorter" onClick={() => {toggleSort(name)}}>
+    <div className="datatable-sorter" onClick={() => {toggleSort(name);}}>
       <span className="datatable-sorter-label">{label}</span>
       <span className="sort-icon">
         {!currentSort[name] ? (
@@ -218,7 +246,7 @@ const DatatableSorter = ({ name, label, toggleSort, currentSort }) =>
       }
       </span>
     </div>
-  </th>
+  </th>;
 
 registerComponent('DatatableSorter', DatatableSorter);
 
@@ -243,31 +271,59 @@ const DatatableContents = (props) => {
   const hasMore = totalCount > results.length;
 
   return (
-    <div className="datatable-list">
+    <Components.DatatableContentLayout>
       {title && <Components.DatatableTitle title={title}/>}
-      <table className="table">
-        <thead>
-          <tr>
+      <Components.DatatableContentInnerLayout>
+        <Components.DatatableContentHeadLayout>
             {_.sortBy(columns, column => column.order).map((column, index) => <Components.DatatableHeader key={index} collection={collection} column={column} toggleSort={toggleSort} currentSort={currentSort} />)}
             {showEdit ? <th><FormattedMessage id="datatable.edit"/></th> : null}
-          </tr>
-        </thead>
-        <tbody>
+        </Components.DatatableContentHeadLayout>
+        <Components.DatatableContentBodyLayout>
           {results.map((document, index) => <Components.DatatableRow {...props} collection={collection} columns={columns} document={document} key={index} showEdit={showEdit} currentUser={currentUser}/>)}
-        </tbody>
-      </table>
+        </Components.DatatableContentBodyLayout>
+      </Components.DatatableContentInnerLayout>
       {hasMore &&
-        <div className="datatable-list-load-more">
+      <Components.DatatableContentMoreLayout>
           {isLoadingMore ?
             <Components.Loading/> :
             <Components.Button variant="primary" onClick={e => {e.preventDefault(); loadMore();}}>Load More ({count}/{totalCount})</Components.Button>
           }
-        </div>
+      </Components.DatatableContentMoreLayout>
       }
-    </div>
-  )
-}
+    </Components.DatatableContentLayout>
+  );
+};
 registerComponent('DatatableContents', DatatableContents);
+
+const DatatableContentLayout = ({ children }) => (
+  <div className="datatable-list">
+    {children}
+  </div>
+);
+registerComponent({ name: 'DatatableContentLayout', component: DatatableContentLayout });
+const DatatableContentInnerLayout = ({ children }) => (
+  <table className="table">
+    {children}
+  </table>
+);
+registerComponent({ name: 'DatatableContentInnerLayout', component: DatatableContentInnerLayout });
+const DatatableContentHeadLayout = ({ children }) => (
+  <thead>
+    <tr>
+      {children}
+    </tr></thead>
+);
+registerComponent({ name: 'DatatableContentHeadLayout', component: DatatableContentHeadLayout });
+const DatatableContentBodyLayout = ({ children }) => (
+  <tbody>{children}</tbody>
+);
+registerComponent({ name: 'DatatableContentBodyLayout', component: DatatableContentBodyLayout });
+const DatatableContentMoreLayout = ({ children }) => (
+  <div className="datatable-list-load-more">
+    {children}
+  </div>
+);
+registerComponent({ name: 'DatatableContentMoreLayout', component: DatatableContentMoreLayout });
 
 /*
 
@@ -275,7 +331,7 @@ DatatableTitle Component
 
 */
 const DatatableTitle = ({ title }) => 
-  <div className="datatable-title">{title}</div>
+  <div className="datatable-title">{title}</div>;
 
 registerComponent('DatatableTitle', DatatableTitle);
 
@@ -293,24 +349,29 @@ const DatatableRow = (props, { intl }) => {
   const modalProps = { title: <code>{document._id}</code> };
 
   return (
-  <tr className={`datatable-item ${row}`}>
-
-    {_.sortBy(columns, column => column.order).map((column, index) => <Components.DatatableCell key={index} column={column} document={document} currentUser={currentUser} />)}
-
+  <Components.DatatableRowLayout className={`datatable-item ${row}`}>
+      {_.sortBy(columns, column => column.order).map((column, index) => (
+        <Components.DatatableCell key={index} column={column} document={document} currentUser={currentUser} />
+      ))}
     {showEdit && canEdit ?
-      <td>
+      <Components.DatatableCellLayout>
         <Components.EditButton collection={collection} documentId={document._id} currentUser={currentUser} mutationFragmentName={options && options.fragmentName} modalProps={modalProps} {...editFormOptions}/>
-      </td>
+      </Components.DatatableCellLayout>
     : null}
-
-  </tr>
-  )
-}
+  </Components.DatatableRowLayout>
+  );
+};
 registerComponent('DatatableRow', DatatableRow);
 
 DatatableRow.contextTypes = {
   intl: intlShape
 };
+const DatatableRowLayout = ({ children, ...otherProps }) => (
+  <tr {...otherProps}>
+    {children}
+  </tr>
+);
+registerComponent({ name: 'DatatableRowLayout', component: DatatableRowLayout });
 
 /*
 
@@ -318,13 +379,22 @@ DatatableCell Component
 
 */
 const DatatableCell = ({ column, document, currentUser }) => {
-  const Component = column.component || column.componentName && Components[column.componentName] || Components.DatatableDefaultCell;
+  const Component = column.component 
+  || column.componentName && Components[column.componentName] 
+  || Components.DatatableDefaultCell;
   const columnName = column.name || column;
   return (
-    <td className={`datatable-item-${columnName.toLowerCase().replace(/\s/g,'-')}`}><Component column={column} document={document} currentUser={currentUser} /></td>
-  )
-}
+    <Components.DatatableCellLayout className={`datatable-item-${columnName.toLowerCase().replace(/\s/g, '-')}`}>
+      <Component column={column} document={document} currentUser={currentUser} />
+    </Components.DatatableCellLayout>
+  );
+};
 registerComponent('DatatableCell', DatatableCell);
+
+const DatatableCellLayout = ({ children, ...otherProps }) => (
+  <td {...otherProps}>{children}</td>
+);
+registerComponent({ name: 'DatatableCellLayout', component: DatatableCellLayout });
 
 /*
 
@@ -332,6 +402,7 @@ DatatableDefaultCell Component
 
 */
 const DatatableDefaultCell = ({ column, document }) =>
-  <div>{typeof column === 'string' ? getFieldValue(document[column]) : getFieldValue(document[column.name])}</div>
+  <div>{typeof column === 'string' ? getFieldValue(document[column]) : getFieldValue(document[column.name])}</div>;
 
 registerComponent('DatatableDefaultCell', DatatableDefaultCell);
+
