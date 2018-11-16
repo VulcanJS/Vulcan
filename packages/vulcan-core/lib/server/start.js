8e3d6b8d0db6025c8c553df2a28c2c9f0f253489
eric-burel <e.burel.contact@gmail.com>
2018-11-16 17:57:38 +0100

    revert start change

--- packages/vulcan-core/lib/server/start.js
+++ packages/vulcan-core/lib/server/start.js
@@ -2,12 +2,6 @@ import {Inject} from 'meteor/meteorhacks:inject-initial';
 import { SyncedCron } from 'meteor/percolatestudio:synced-cron';
 import { getSetting, registerSetting } from 'meteor/vulcan:lib';
 
-import {
-  populateComponentsApp,
-  populateRoutesApp,
-  initializeFragments
-} from 'meteor/vulcan:lib';
-
 registerSetting('mailUrl', null, 'The SMTP URL used to send out email');
 
 if (getSetting('mailUrl')) {
@@ -18,11 +12,6 @@ Meteor.startup(function() {
   if (typeof SyncedCron !== 'undefined') {
     SyncedCron.start();
   }
-
-  // init the application components and routes, including components & routes from 3rd-party packages
-  initializeFragments();
-  populateComponentsApp();
-  populateRoutesApp();
 });
 
 Inject.obj('serverTimezoneOffset', {offset: new Date().getTimezoneOffset()});
