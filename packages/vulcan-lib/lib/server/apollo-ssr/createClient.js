e95098dcedd278deb1fa00aafacc319511793177
eric-burel <e.burel.contact@gmail.com>
2018-11-16 17:48:17 +0100

    try to load the schema

--- packages/vulcan-lib/lib/server/apollo-ssr/createClient.js
+++ packages/vulcan-lib/lib/server/apollo-ssr/createClient.js
@@ -7,36 +7,38 @@
 
 
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import fetch from 'node-fetch'
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// Alternative that does not send real HTTP queries
-// @see https://www.apollographql.com/docs/react/features/server-side-rendering.html#local-queries
-// import { SchemaLink } from 'apollo-link-schema';
-// import schema from "???"
+import { SchemaLink } from 'apollo-link-schema';
+// TODO: how to import the schema?
+import { GraphQLSchema } from '../../modules/graphql'
+const schema = {} // TODO ???
 
+// @see https://www.apollographql.com/docs/react/features/server-side-rendering.html#local-queries
+// import { createHttpLink } from 'apollo-link-http';
+// import fetch from 'node-fetch'
 
 const createClient = (req) => {
     const client = new ApolloClient({
         ssrMode: true,
+        link: new SchemaLink({ schema }),
+
+        // @see https://www.apollographql.com/docs/react/features/server-side-rendering.html#local-queries
         // Remember that this is the interface the SSR server will use to connect to the
         // API server, so we need to ensure it isn't firewalled, etc
-        link: createHttpLink({
-            uri: 'http://localhost:3000',
-            credentials: 'same-origin',
-            headers: {
-                // NOTE: this is a Connect req, not an Express req,
-                // so req.header is not defined
-                // cookie: req.header('Cookie'),
-                cookie: req.headers['cookie'],
-            },
-            // need to explicitely pass fetch server side
-            fetch
-        }),
+        //link: createHttpLink({
+        //    uri: 'http://localhost:3000',
+        //    credentials: 'same-origin',
+        //    headers: {
+        //        // NOTE: this is a Connect req, not an Express req,
+        //        // so req.header is not defined
+        //        // cookie: req.header('Cookie'),
+        //        cookie: req.headers['cookie'],
+        //    },
+        //    // need to explicitely pass fetch server side
+        //    fetch
+        //}),
 
-        // Alternative that does not send real HTTP queries
-        // link: new SchemaLink({ schema }),
         
         cache: new InMemoryCache(),
     });
