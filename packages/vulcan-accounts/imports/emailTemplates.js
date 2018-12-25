ba561bb7fa5292ca0d1f2da48d94ba495dbd964e
Apollinaire <apollinaire.lecocq@live.fr>
2018-12-25 09:34:25 +0100

    fix accounts-password dependency

--- packages/vulcan-accounts/imports/emailTemplates.js
+++ packages/vulcan-accounts/imports/emailTemplates.js
@@ -1,5 +1,12 @@
-import { Accounts } from 'meteor/accounts-base';
-import { getSetting } from 'meteor/vulcan:core';
+import {Accounts} from 'meteor/accounts-base';
+import {getSetting} from 'meteor/vulcan:core';
 
-Accounts.emailTemplates.siteName = getSetting('public.title', '');
-Accounts.emailTemplates.from = getSetting('public.title', '')+ ' <' + getSetting('defaultEmail', 'no-reply@example.com') +'>';
+// the emailTemplates are made available by accounts-password, which we don't want to depend on
+if (Package['accounts-password']) {
+  Accounts.emailTemplates.siteName = getSetting('public.title', '');
+  Accounts.emailTemplates.from =
+    getSetting('public.title', '') +
+    ' <' +
+    getSetting('defaultEmail', 'no-reply@example.com') +
+    '>';
+}
