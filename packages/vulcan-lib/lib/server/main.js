9f18d4f95f5ad000a2eea8990906c5fc1c7f236a
SachaG <358832+SachaG@users.noreply.github.com>
2019-01-03 21:45:50 +0900

    Re-enable webAppConnectHandlersUse

--- packages/vulcan-lib/lib/server/main.js
+++ packages/vulcan-lib/lib/server/main.js
@@ -10,7 +10,7 @@ export * from './query.js';
 export * from '../modules/index.js';
 export * from './mutators.js';
 // TODO: what to do with this?
-//export * from './meteor_patch.js';
+export * from './meteor_patch.js';
 //export * from './render_context.js';
 //export * from './inject_data.js';
 export * from './utils.js';
