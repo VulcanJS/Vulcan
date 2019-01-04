b118943f2406019b70eb776bb645fcf10627794c
eric-burel <e.burel.contact@gmail.com>
2019-01-04 11:43:54 +0100

    removed error link server side where it does not make sense

--- packages/vulcan-lib/lib/server/apollo-server/settings.js
+++ packages/vulcan-lib/lib/server/apollo-server/settings.js
@@ -12,19 +12,20 @@ import _merge from 'lodash/merge';
 //);
 // registerSetting('apolloServer.jsonParserOptions.limit', undefined, 'DEPRECATED bodyParser jsonParser limit');
 
-// NOTE: some option can be functions, so they cannot be 
+// NOTE: some option can be functions, so they cannot be
 // defined as Meteor settings, which are pure JSON (no function)
 
 // @see https://www.apollographql.com/docs/apollo-server/api/apollo-server.html#constructor-options-lt-ApolloServer-gt
-let apolloServerOptions = { };
-export const registerApolloServerOptions = (options) => {
-    apolloServerOptions = _merge(apolloServerOptions, options);
+let apolloServerOptions = {};
+export const registerApolloServerOptions = options => {
+  apolloServerOptions = _merge(apolloServerOptions, options);
 };
 export const getApolloServerOptions = () => apolloServerOptions;
 
 // @see https://www.apollographql.com/docs/apollo-server/api/apollo-server.html#Parameters-2
-let apolloApplyMiddlewareOptions = { };
-export const registerApolloApplyMiddlewareOptions = (options) => {
-    apolloApplyMiddlewareOptions = _merge(apolloApplyMiddlewareOptions, options);
+let apolloApplyMiddlewareOptions = {};
+export const registerApolloApplyMiddlewareOptions = options => {
+  apolloApplyMiddlewareOptions = _merge(apolloApplyMiddlewareOptions, options);
 };
-export const getApolloApplyMiddlewareOptions = () => apolloApplyMiddlewareOptions;
+export const getApolloApplyMiddlewareOptions = () =>
+  apolloApplyMiddlewareOptions;
