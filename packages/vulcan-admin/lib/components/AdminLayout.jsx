74e41dd97dd250ea3aa2d70d8c1a799e65c5e025
Apollinaire <apollinaire.lecocq@live.fr>
2019-01-09 11:29:25 +0100

    use withAccess in AdminLayout

--- packages/vulcan-admin/lib/components/AdminLayout.jsx
+++ packages/vulcan-admin/lib/components/AdminLayout.jsx
@@ -2,43 +2,32 @@
  * @Author: Apollinaire Lecocq <apollinaire>
  * @Date:   08-01-19
  * @Last modified by:   apollinaire
- * @Last modified time: 08-01-19
+ * @Last modified time: 09-01-19
  */
 import React from 'react';
-import {
-  registerComponent,
-  Components,
-  withCurrentUser,
-} from 'meteor/vulcan:core';
+import {registerComponent, Components, withAccess} from 'meteor/vulcan:core';
+
+// we need a component to wrap the `withAccess` hoc around something
+function EmptyComponent({children}) {
+  return children;
+}
+EmptyComponent.displayName = 'EmptyComponent';
+
+const RestrictToAdmins = withAccess({groups: ['admins']})(EmptyComponent);
 
 /**
  * A simple component that renders the existing layout and checks wether the currentUser is an admin or not.
  */
 
-class AdminLayout extends React.PureComponent {
-  renderWithRestrictedAccess(children, currentUser) {
-    // while the currentUser is loading, don't render anything.
-    if (currentUserLoading) {
-      return null;
-    } //if the currentUser is an admin, then render the children
-    else if (currentUser && currentUser.isAdmin) {
-      return children;
-    } // for every other case (just a member or not logged), render the 404 message
-    return <Components.Error404 />;
-  }
-
-  render() {
-    const {currentUser, children} = this.props;
-    return (
-      <Components.Layout>
-        {this.renderWithRestrictedAccess(children, currentUser)}
-      </Components.Layout>
-    );
-  }
+function AdminLayout({children}) {
+  return (
+    <Components.Layout>
+      <RestrictToAdmins>{children}</RestrictToAdmins>
+    </Components.Layout>
+  );
 }
 
 registerComponent({
   name: 'AdminLayout',
   component: AdminLayout,
-  hocs: [withCurrentUser],
 });
