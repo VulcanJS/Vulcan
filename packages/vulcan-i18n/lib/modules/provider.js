c41111fdb6d9d8c0cbef8444f7592306e4d815bd
Apollinaire <apollinaire.lecocq@live.fr>
2019-01-18 18:43:32 +0100

    Add `formatLabel` to IntlProvider

--- packages/vulcan-i18n/lib/modules/provider.js
+++ packages/vulcan-i18n/lib/modules/provider.js
@@ -1,16 +1,58 @@
-import React, { Component } from 'react';
-import { getString } from 'meteor/vulcan:lib';
-import { intlShape } from './shape.js';
+import React, {Component} from 'react';
+import {getString} from 'meteor/vulcan:lib';
+import {intlShape} from './shape.js';
 
-export default class IntlProvider extends Component{
+export default class IntlProvider extends Component {
+  formatMessage = ({id, defaultMessage}, values) => {
+    return getString({id, defaultMessage, values, locale: this.props.locale});
+  };
 
-  formatMessage = ({ id, defaultMessage }, values) => {
-    return getString({ id, defaultMessage, values, locale: this.props.locale });
-  }
+  /**
+   * formatLabel - Get a label for a field, for a given collection, in the current language. The evaluation is as follows : i18n(collectioName.fieldName) > i18n(global.fieldName) > i18n(fieldName) > schema.fieldName.label > fieldName
+   *
+   * @param  {object} options
+   * @param  {string} options.fieldName          The name of the field to evaluate (required)
+   * @param  {string} options.collectionName     The name of the collection the field belongs to
+   * @param  {object} options.schema             The schema of the collection
+   * @param  {object} values                     The values to pass to format the i18n string
+   * @return {string}                            The translated label
+   */
 
-  formatStuff = (something) => {
+  formatLabel = ({fieldName, collectionName, schema}, values) => {
+    if (!fieldName) {
+      throw new Error(
+        'fieldName option passed to formatLabel cannot be empty or undefined'
+      );
+    }
+    const defaultMessage = '|*|*|';
+    // Get the intl label
+    let intlLabel = defaultMessage;
+    // try collectionName.fieldName as intl id
+    if (collectionName) {
+      intlLabel = this.formatMessage(
+        {id: `${collectionName.toLowerCase()}.${fieldName}`, defaultMessage},
+        values
+      );
+    }
+    // try global.fieldName then just fieldName as intl id
+    if (intlLabel === defaultMessage) {
+      intlLabel = this.formatMessage(
+        {id: `global.${fieldName}`, defaultMessage},
+        values
+      );
+      if (intlLabel === defaultMessage) {
+        intlLabel = this.formatMessage({id: fieldName}, values);
+      }
+    }
+    // define the schemaLabel. If the schema has been initialized with SimpleSchema, the label should be here even if it has not been declared https://github.com/aldeed/simple-schema-js#label
+    let schemaLabel =
+      schema && schema[fieldName] ? schema[fieldName].label : null;
+    return intlLabel || schemaLabel || fieldName;
+  };
+
+  formatStuff = something => {
     return something;
-  }
+  };
 
   getChildContext() {
     return {
@@ -19,20 +61,20 @@ export default class IntlProvider extends Component{
         formatTime: this.formatStuff,
         formatRelative: this.formatStuff,
         formatNumber: this.formatStuff,
-        formatPlural: this.formatStuff, 
+        formatPlural: this.formatStuff,
         formatMessage: this.formatMessage,
+        formatLabel: this.formatLabel,
         formatHTMLMessage: this.formatStuff,
         now: this.formatStuff,
-      }
+      },
     };
   }
-  
-  render(){
+
+  render() {
     return this.props.children;
   }
-
 }
 
 IntlProvider.childContextTypes = {
-  intl: intlShape
+  intl: intlShape,
 };
--- packages/vulcan-i18n/lib/modules/shape.js
+++ packages/vulcan-i18n/lib/modules/shape.js
@@ -1,8 +1,8 @@
 /*
-* Copyright 2015, Yahoo Inc.
-* Copyrights licensed under the New BSD License.
-* See the accompanying LICENSE file for terms.
-*/
+ * Copyright 2015, Yahoo Inc.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
 
 import PropTypes from 'prop-types';
 
@@ -13,22 +13,23 @@ const numeric2digit = oneOf(['numeric', '2-digit']);
 const funcReq = func.isRequired;
 
 export const intlConfigPropTypes = {
-  locale       : string,
-  formats      : object,
-  messages     : object,
+  locale: string,
+  formats: object,
+  messages: object,
   textComponent: any,
 
-  defaultLocale : string,
+  defaultLocale: string,
   defaultFormats: object,
 };
 
 export const intlFormatPropTypes = {
-  formatDate       : funcReq,
-  formatTime       : funcReq,
-  formatRelative   : funcReq,
-  formatNumber     : funcReq,
-  formatPlural     : funcReq,
-  formatMessage    : funcReq,
+  formatDate: funcReq,
+  formatTime: funcReq,
+  formatRelative: funcReq,
+  formatNumber: funcReq,
+  formatPlural: funcReq,
+  formatMessage: funcReq,
+  formatLabel: funcReq,
   formatHTMLMessage: funcReq,
 };
 
@@ -40,8 +41,8 @@ export const intlShape = shape({
 });
 
 export const messageDescriptorPropTypes = {
-  id            : string.isRequired,
-  description   : string,
+  id: string.isRequired,
+  description: string,
   defaultMessage: string,
 };
 
@@ -50,30 +51,30 @@ export const dateTimeFormatPropTypes = {
   formatMatcher: oneOf(['basic', 'best fit']),
 
   timeZone: string,
-  hour12  : bool,
+  hour12: bool,
 
-  weekday     : narrowShortLong,
-  era         : narrowShortLong,
-  year        : numeric2digit,
-  month       : oneOf(['numeric', '2-digit', 'narrow', 'short', 'long']),
-  day         : numeric2digit,
-  hour        : numeric2digit,
-  minute      : numeric2digit,
-  second      : numeric2digit,
+  weekday: narrowShortLong,
+  era: narrowShortLong,
+  year: numeric2digit,
+  month: oneOf(['numeric', '2-digit', 'narrow', 'short', 'long']),
+  day: numeric2digit,
+  hour: numeric2digit,
+  minute: numeric2digit,
+  second: numeric2digit,
   timeZoneName: oneOf(['short', 'long']),
 };
 
 export const numberFormatPropTypes = {
   localeMatcher,
 
-  style          : oneOf(['decimal', 'currency', 'percent']),
-  currency       : string,
+  style: oneOf(['decimal', 'currency', 'percent']),
+  currency: string,
   currencyDisplay: oneOf(['symbol', 'code', 'name']),
-  useGrouping    : bool,
+  useGrouping: bool,
 
-  minimumIntegerDigits    : number,
-  minimumFractionDigits   : number,
-  maximumFractionDigits   : number,
+  minimumIntegerDigits: number,
+  minimumFractionDigits: number,
+  maximumFractionDigits: number,
   minimumSignificantDigits: number,
   maximumSignificantDigits: number,
 };
