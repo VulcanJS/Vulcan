4f2044ea419ff125d6dc99ccdf75e6f3c8ef59cc
eric-burel <eb@lebrun-burel.com>
2019-02-05 18:46:22 +0100

    fixed regression in auth

--- packages/vulcan-lib/lib/client/auth.js
+++ packages/vulcan-lib/lib/client/auth.js
@@ -11,6 +11,7 @@ import { Meteor } from 'meteor/meteor';
 
 const cookie = new Cookies();
 
+
 function setToken(loginToken, expires) {
   if (loginToken && expires !== -1) {
     cookie.set('meteor_login_token', loginToken, {
@@ -36,22 +37,25 @@ function initToken() {
 }
 
 Meteor.startup(() => {
+  console.log('init token');
   initToken();
 });
 
-// TODO: legacy code, not sure if necessary?
-// const originalSetItem = Meteor._localStorage.setItem;
-// Meteor._localStorage.setItem = function setItem(key, value) {
-//   if (key === 'Meteor.loginToken') {
-//     Meteor.defer(resetToken);
-//   }
-//   originalSetItem.call(Meteor._localStorage, key, value);
-// };
-
-// const originalRemoveItem = Meteor._localStorage.removeItem;
-// Meteor._localStorage.removeItem = function removeItem(key) {
-//   if (key === 'Meteor.loginToken') {
-//     Meteor.defer(resetToken);
-//   }
-//   originalRemoveItem.call(Meteor._localStorage, key);
-// };
+// TODO: cleanup
+// This part of the code overrides the default localStorage function,
+// so that when Meteor.loginToken is set, it is also automatically 
+// stored as a cookie (necessary for SSR to work as expected for all HTTP requests)
+const originalSetItem = Meteor._localStorage.setItem;
+Meteor._localStorage.setItem = function setItem(key, value) {
+  if (key === 'Meteor.loginToken') {
+    Meteor.defer(initToken);
+  }
+  originalSetItem.call(Meteor._localStorage, key, value);
+};
+const originalRemoveItem = Meteor._localStorage.removeItem;
+Meteor._localStorage.removeItem = function removeItem(key) {
+  if (key === 'Meteor.loginToken') {
+    Meteor.defer(initToken);
+  }
+  originalRemoveItem.call(Meteor._localStorage, key);
+};
