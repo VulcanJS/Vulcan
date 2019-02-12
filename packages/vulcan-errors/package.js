815a65d853ae9ef0e4c9f8848723db4163ca44d1
Apollinaire <apollinaire.lecocq@live.fr>
2019-02-12 13:00:46 +0100

    prettier & lint code

--- packages/vulcan-errors/package.js
+++ packages/vulcan-errors/package.js
@@ -2,19 +2,14 @@ Package.describe({
   name: 'vulcan:errors',
   summary: 'Vulcan error tracking package',
   version: '1.12.16',
-  git: 'https://github.com/VulcanJS/Vulcan.git'
+  git: 'https://github.com/VulcanJS/Vulcan.git',
 });
 
 Package.onUse(function(api) {
-
   api.versionsFrom('1.6.1');
-  
-  api.use([
-    'ecmascript',
-    'vulcan:core@1.12.16',
-  ]);
+
+  api.use(['ecmascript', 'vulcan:core@1.12.16']);
 
   api.mainModule('lib/server/main.js', 'server');
   api.mainModule('lib/client/main.js', 'client');
-
 });
--- packages/vulcan-events-intercom/package.js
+++ packages/vulcan-events-intercom/package.js
@@ -2,19 +2,14 @@ Package.describe({
   name: 'vulcan:events-intercom',
   summary: 'Vulcan Intercom integration package.',
   version: '1.12.16',
-  git: 'https://github.com/VulcanJS/Vulcan.git'
+  git: 'https://github.com/VulcanJS/Vulcan.git',
 });
 
-Package.onUse(function (api) {
-
+Package.onUse(function(api) {
   api.versionsFrom('1.6.1');
 
-  api.use([
-    'vulcan:core@1.12.16',
-    'vulcan:events@1.12.16'
-  ]);
+  api.use(['vulcan:core@1.12.16', 'vulcan:events@1.12.16']);
 
   api.mainModule('lib/client/main.js', 'client');
   api.mainModule('lib/server/main.js', 'server');
-
 });
