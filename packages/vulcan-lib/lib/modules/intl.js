5125e61d21794fd3c80d079b0b9a2bf608aaf958
Valentin Boettcher <hiro@protagon.space>
2019-03-09 10:55:45 +0100

    Avoid double logging.

--- packages/vulcan-lib/lib/modules/intl.js
+++ packages/vulcan-lib/lib/modules/intl.js
@@ -1,6 +1,7 @@
 import SimpleSchema from 'simpl-schema';
 import { getSetting } from '../modules/settings';
 import { debug } from 'meteor/vulcan:lib';
+import { O_CREAT } from 'constants';
 
 export const Strings = {};
 
@@ -22,11 +23,11 @@ export const getString = ({id, values, defaultMessage, locale}) => {
 
   // use default locale
   if(!message) {
-    debug(`\x1b[32m>> INTL: No string found for id "${id}" in locale "${locale}."\x1b[0m`);
+    debug(`\x1b[32m>> INTL: No string found for id "${id}" in locale "${locale}".\x1b[0m`);
     message = Strings[defaultLocale] && Strings[defaultLocale][id];
 
     // if default locale hasn't got the message too
-    if(!message)
+    if(!message && locale !== defaultLocale)
       debug(`\x1b[32m>> INTL: No string found for id "${id}" in the default locale ("${defaultLocale}").\x1b[0m`);
   }
 
