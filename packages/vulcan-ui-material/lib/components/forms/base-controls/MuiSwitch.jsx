c23c6e0d4cef2e449c8420c8e1cf4a549955559e
Harold <hjuarez20@gmail.com>
2019-03-18 18:16:45 -0600

    Fix onChange function on MuiSwitch component

--- packages/vulcan-ui-material/lib/components/forms/base-controls/MuiSwitch.jsx
+++ packages/vulcan-ui-material/lib/components/forms/base-controls/MuiSwitch.jsx
@@ -24,7 +24,7 @@ const MuiSwitch = createReactClass({
     const value = target.checked;
     
     //this.setValue(value);
-    this.props.onChange(this.props.name, value);
+    this.props.onChange(value);
     
     setTimeout(() => {document.activeElement.blur();});
   },
