602c9291b40413573fe240caeb99be3eef6bc033
eric-burel <eb@lebrun-burel.com>
2019-04-04 11:40:47 +0200

    remove old test

--- packages/vulcan-core/test/containers.test.js
+++ packages/vulcan-core/test/containers.test.js
@@ -26,48 +26,6 @@ describe('vulcan-core/containers', function() {
       expect(wrapper.html()).toEqual('FOO');
     });
   });
-  describe('handleOptions', function() {
-    const expectedCollectionName = 'COLLECTION_NAME';
-    const collectionNameOptions = { collectionName: expectedCollectionName };
-    const expectedCollection = { options: collectionNameOptions };
-    it('get collectionName from collection', function() {
-      const options = { collection: expectedCollection };
-      const { collection, collectionName } = extractCollectionInfo(options);
-      expect(collection).toEqual(expectedCollection);
-      expect(collectionName).toEqual(expectedCollectionName);
-    });
-    it('get collection from collectioName', function() {
-      // MOCK getCollection
-      const { collection, collectionName } = extractCollectionInfo(collectionNameOptions);
-      expect(collection).toEqual(expectedCollection);
-      expect(collectionName).toEqual(expectedCollectionName);
-    });
-    const expectedFragmentName = 'FRAGMENT_NAME';
-    const expectedFragment = {
-      definitions: [{ name: { value: expectedFragmentName } }],
-    };
-    it('get fragment from fragmentName', function() {
-      // MOCK getCollection
-      const options = { fragmentName: expectedFragmentName };
-      const { fragment, fragmentName } = extractFragmentInfo(options);
-      expect(fragment).toEqual(expectedFragment);
-      expect(fragmentName).toEqual(expectedFragmentName);
-    });
-    it('get fragmentName from fragment', function() {
-      const options = { fragment: expectedFragment };
-      const { fragment, fragmentName } = extractFragmentInfo(options);
-      expect(fragment).toEqual(expectedFragment);
-      expect(fragmentName).toEqual(expectedFragmentName);
-    });
-    it('get fragmentName and fragment from collectionName', function() {
-      // if options does not contain fragment, we get the collection default fragment based on its name
-      const options = {};
-      const { fragment, fragmentName } = extractFragmentInfo(options, expectedCollectionName);
-      expect(fragment).toEqual(expectedFragment);
-      expect(fragmentName).toEqual(expectedFragmentName);
-    });
-  });
-
   describe('withMessages', function() {
     const WrappedComponent = props => <div />;
     const apolloClient = null; // TODO: init an apolloClient, that must be available in the context
