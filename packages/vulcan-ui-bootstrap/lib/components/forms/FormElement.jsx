ee5907535392e75f8df3b13827b8e2ccb36456d9
SachaG <358832+SachaG@users.noreply.github.com>
2019-04-04 09:51:55 +0900

    use registerComponent instead of replaceComponent for FormElement

--- packages/vulcan-ui-bootstrap/lib/components/forms/FormElement.jsx
+++ packages/vulcan-ui-bootstrap/lib/components/forms/FormElement.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
 import Form from 'react-bootstrap/Form';
-import { replaceComponent } from 'meteor/vulcan:core';
+import { registerComponent } from 'meteor/vulcan:core';
 
-replaceComponent('FormElement', Form);
+registerComponent('FormElement', Form);
