7d60155cb6c64df5555e20ae24d0619652cb8bd4
eric-burel <eb@lebrun-burel.com>
2019-05-23 14:20:29 +0200

    update JSDOM initialization so it should work with react-testing

--- packages/vulcan-test/lib/modules/initComponentTest.js
+++ packages/vulcan-test/lib/modules/initComponentTest.js
@@ -3,13 +3,20 @@
  * Must be called AFTER components registration
  */
 // setup JSDOM server side for testing (necessary for Enzyme to mount)
-import 'jsdom-global/register';
+import jsdom from 'jsdom-global';
 import Enzyme from 'enzyme';
+// TODO: must be updated depending on the React version
+// @see https://www.npmjs.com/package/enzyme-adapter-react-16
 import Adapter from 'enzyme-adapter-react-16.3';
 
 const initComponentTest = () => {
+  // init a JSDOM to allow rendering server side
+  jsdom("", {
+		runScripts: "outside-only"
+	});
   // setup enzyme
   Enzyme.configure({ adapter: new Adapter() });
+  //
   // and then load them in the app so that <Component.Whatever /> is defined
   import { populateComponentsApp, initializeFragments } from 'meteor/vulcan:lib';
   // we need registered fragments to be initialized because populateComponentsApp will run
