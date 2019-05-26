7534d13bf100300864da830b36b05bc266d0dae4
Erik Dakoda <erik@qeebi.com>
2019-05-26 18:32:40 -0400

    Moved the functionality of FormNestedHead and FormNestedFoot into FormNestedArrayLayout to improve simplicity and flexibility

--- packages/vulcan-ui-material/lib/components/forms/FormNestedArrayLayout.jsx
+++ packages/vulcan-ui-material/lib/components/forms/FormNestedArrayLayout.jsx
@@ -1,31 +1,93 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { replaceComponent } from 'meteor/vulcan:core';
-
+import { instantiateComponent, replaceComponent } from 'meteor/vulcan:core';
 import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import Grid from '@material-ui/core/Grid';
+import RemoveIcon from 'mdi-material-ui/Delete';
+import AddIcon from 'mdi-material-ui/Plus';
 
-const FormNestedArrayLayout = ({ hideLabel = false, label, content, children }) => (
-	<div>
-		{hideLabel === false ?
-			<Typography
-				component="label"
-				variant="caption"
-				style={{ fontSize: 16 }}
-			>
-				{label}
-			</Typography>
-			: null}
-		<div>{content || children}</div>
-	</div>
-);
+
+const IconRemove = () => <RemoveIcon/>;
+replaceComponent('IconRemove', IconRemove);
+
+
+const IconAdd = () => <AddIcon/>;
+replaceComponent('IconAdd', IconAdd);
+
+
+const FormNestedArrayLayout = (props) => {
+  const {
+    hasErrors,
+    nestedArrayErrors,
+    label,
+    hideLabel,
+    addItem,
+    beforeComponent,
+    afterComponent,
+    formComponents,
+    children,
+  } = props;
+  const FormComponents = formComponents;
+  
+  return (
+    <div className="form-nested-array-layout">
+      
+      {instantiateComponent(beforeComponent, props)}
+      
+      {
+        !hideLabel &&
+        
+        <Typography
+          component="label"
+          variant="subtitle1"
+          gutterBottom
+        >
+          {label}
+        </Typography>
+      }
+      
+      {children}
+      
+      {
+        addItem &&
+        
+        <Grid container direction="column" alignItems="flex-end">
+          <Fab color="primary" onClick={addItem} className="form-nested-button">
+            <AddIcon/>
+          </Fab>
+        </Grid>
+      }
+      
+      {
+        hasErrors
+          ?
+          <FormComponents.FieldErrors errors={nestedArrayErrors}/>
+          :
+          null
+      }
+      
+      {instantiateComponent(afterComponent, props)}
+    
+    </div>
+  );
+};
+
 
 FormNestedArrayLayout.propTypes = {
-  hasErrors: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+  hasErrors: PropTypes.bool.isRequired,
+  nestedArrayErrors: PropTypes.array,
   label: PropTypes.node,
-	hideLabel: PropTypes.bool,
-	content: PropTypes.node,
+  hideLabel: PropTypes.bool,
+  addItem: PropTypes.funct,
+  beforeComponent: PropTypes.node,
+  afterComponent: PropTypes.node,
+  formComponents: PropTypes.object,
+  children: PropTypes.node,
 };
+
+
 replaceComponent({
-	name: 'FormNestedArrayLayout',
-	component: FormNestedArrayLayout,
+  name: 'FormNestedArrayLayout',
+  component: FormNestedArrayLayout,
 });
--- packages/vulcan-ui-material/lib/components/forms/FormNestedDivider.jsx
+++ packages/vulcan-ui-material/lib/components/forms/FormNestedDivider.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { replaceComponent } from 'meteor/vulcan:core';
-// import { FormattedMessage } from 'meteor/vulcan:i18n';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Divider from '@material-ui/core/Divider';
+import classNames from 'classnames';
+
 
 const styles = theme => ({
   divider: {
-    // marginLeft: -24,
-    // marginRight: -24,
-    marginTop: 16,
-    marginBottom: 23,
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 3,
   },
 });
 
-const FormNestedDivider = ({ classes, label, addItem }) => <Divider className={classes.divider} />;
+
+const FormNestedDivider = ({ classes, label, addItem }) => 
+  <Divider className={classNames('form-nested-divider', classes.divider)} />;
+
 
 FormNestedDivider.propTypes = {
   classes: PropTypes.object.isRequired,
   label: PropTypes.string,
   addItem: PropTypes.func,
 };
+
 
 replaceComponent('FormNestedDivider', FormNestedDivider, [withStyles, styles]);
