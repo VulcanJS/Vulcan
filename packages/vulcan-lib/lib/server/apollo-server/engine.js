eb4c63318589931b762fe7a75f34615cfe127214
Erik Dakoda <erik@qeebi.com>
2019-06-19 18:50:43 -0400

    Add support for Apollo Engine schemaTag in addition to apiKey

--- packages/vulcan-lib/lib/server/apollo-server/engine.js
+++ packages/vulcan-lib/lib/server/apollo-server/engine.js
@@ -1,10 +1,13 @@
 import { getSetting } from '../../modules/settings.js';
-// see https://github.com/apollographql/apollo-cache-control
-export const engineApiKey = process.env.ENGINE_API_KEY || getSetting('apolloEngine.apiKey');
-// options now available:
 // @see https://www.apollographql.com/docs/apollo-server/api/apollo-server.html#EngineReportingOptions
-export const engineConfig = engineApiKey
-  ? {
-      apiKey: engineApiKey,
-    }
-  : undefined;
+
+let engineConfigObject = getSetting('apolloEngine');
+
+if (!engineConfigObject || !engineConfigObject.apiKey) {
+  engineConfigObject = {
+    apiKey: process.env.ENGINE_API_KEY,
+    schemaTag: process.env.ENGINE_SCHEMA_TAG
+  };
+}
+
+export const engineConfig = engineConfigObject && engineConfigObject.apiKey ? engineConfigObject : undefined;
