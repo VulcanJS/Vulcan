e8053546ca5d93b1d950c744111d414b38e7bbf8
eric-burel <eb@lebrun-burel.com>
2019-08-29 12:04:29 +0200

    add test for user deletion

--- packages/vulcan-users/lib/modules/mutations.js
+++ packages/vulcan-users/lib/modules/mutations.js
@@ -49,7 +49,7 @@ const updateMutation = {
     if (!document) {
       throw new Error(`Could not find document to update for selector: ${JSON.stringify(selector)}`);
     }
-    
+
     performCheck(this, currentUser, document);
 
     return updateMutator({
@@ -110,8 +110,8 @@ const mutations = {
 export default mutations;
 
 registerCallback({
-  name:'user.create.validate',
-  iterator: {document:'the document being inserted'},
+  name: 'user.create.validate',
+  iterator: { document: 'the document being inserted' },
   properties: [
     { document: 'The document being inserted' },
     { currentUser: 'The current user' },
@@ -122,8 +122,8 @@ registerCallback({
   description: 'Validate a document before insertion (can be skipped when inserting directly on server).',
 });
 registerCallback({
-  name:'user.create.before',
-  iterator: {document:'the document being inserted'},
+  name: 'user.create.before',
+  iterator: { document: 'the document being inserted' },
   properties: [
     { document: 'The document being inserted' },
     { currentUser: 'The current user' },
@@ -134,8 +134,8 @@ registerCallback({
   description: 'Perform operations on a new document before it\'s inserted in the database.',
 });
 registerCallback({
-  name:'user.create.after',
-  iterator: {document:'the document after being inserted in the database'},
+  name: 'user.create.after',
+  iterator: { document: 'the document after being inserted in the database' },
   properties: [
     { currentUser: 'The current user' },
   ],
@@ -144,7 +144,7 @@ registerCallback({
   description: 'Perform operations on a new document after it\'s inserted in the database but *before* the mutation returns it.',
 });
 registerCallback({
-  name:'user.create.async',
+  name: 'user.create.async',
   iterator: { document: 'The document being inserted' },
   properties: [
     { data: 'The document being inserted' }, //for backward compatibility
@@ -156,32 +156,32 @@ registerCallback({
 });
 registerCallback({
   name: 'user.update.validate',
-  iterator: {data: 'The client data'},
+  iterator: { data: 'The client data' },
   properties: [
-    {document: 'The document being updated'},
-    {currentUser: 'The current user.'},
-    {validationErrors: 'an object that can be used to accumulate validation errors.'},
+    { document: 'The document being updated' },
+    { currentUser: 'The current user.' },
+    { validationErrors: 'an object that can be used to accumulate validation errors.' },
   ],
   runs: 'sync',
   description: 'Validate a document before update (can be skipped when updating directly on server).'
 });
 registerCallback({
   name: 'user.update.before',
-  iterator: {data:'The client data'},
+  iterator: { data: 'The client data' },
   properties: [
-    {document: 'The document being edited'},
-    {currentUser: 'The current user'},
-    {newDocument: 'A preview of the future document'},
+    { document: 'The document being edited' },
+    { currentUser: 'The current user' },
+    { newDocument: 'A preview of the future document' },
   ],
   runs: 'sync',
   description: 'Perform operations on a document before it\'s updated on the database.',
 });
 registerCallback({
   name: 'user.update.after',
-  iterator: {newdocument: 'The document after the update'},
+  iterator: { newdocument: 'The document after the update' },
   properties: [
-    {document: 'The document before the update'},
-    {currentUser: 'The current user'},
+    { document: 'The document before the update' },
+    { currentUser: 'The current user' },
   ],
   runs: 'sync',
   description: 'Perform operations on a document after it\'s updated in the database but *before* the mutation returns it.'
@@ -189,38 +189,38 @@ registerCallback({
 registerCallback({
   name: 'user.update.async',
   properties: [
-    {newDocument: 'The document after the update'},
-    {document: 'The document before the update'},
-    {currentUser: 'The current user'},
-    {collection: 'The Users collection'},
+    { newDocument: 'The document after the update' },
+    { document: 'The document before the update' },
+    { currentUser: 'The current user' },
+    { collection: 'The Users collection' },
   ],
   runs: 'async',
   description: 'Perform operations on a document after it\'s updated in the database asynchronously.'
 });
 registerCallback({
   name: 'user.delete.validate',
-  iterator: {document: 'The document being deleted'},
+  iterator: { document: 'The document being deleted' },
   properties: [
-    {currentUser: 'The current user'},
+    { currentUser: 'The current user' },
   ],
   runs: 'sync',
   description: 'Validate a document before deletion (can be skipped when deleting directly on the server)'
 });
 registerCallback({
   name: 'user.delete.before',
-  iterator: {document: 'The document being deleted'},
+  iterator: { document: 'The document being deleted' },
   properties: [
-    {currentUser: 'The current user'},
+    { currentUser: 'The current user' },
   ],
   runs: 'sync',
   description: 'Perform operations on a document before it\'s deleted from the database',
 });
 registerCallback({
   name: 'user.delete.async',
-  properties:[
-    {document: 'The document being deleted'},
-    {currentUser: 'The current user'},
-    {collection: 'The Users collection'},
+  properties: [
+    { document: 'The document being deleted' },
+    { currentUser: 'The current user' },
+    { collection: 'The Users collection' },
   ],
   runs: 'async',
   description: 'Perform operations on a document after it\'s deleted from the database asynchronously.'
