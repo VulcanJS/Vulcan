fdd40ae746af1f4694a907cf75ae7938fefff0c4
eric-burel <eb@lebrun-burel.com>
2019-09-04 16:20:21 +0200

    init server tests correctly

--- packages/vulcan-lib/test/server/mutators.test.js
+++ packages/vulcan-lib/test/server/mutators.test.js
@@ -15,8 +15,8 @@ import {
   removeAllCallbacks, createCollection,
 } from 'meteor/vulcan:core';
 import {
-    isoCreateCollection,
-    initServerTest
+  isoCreateCollection,
+  initServerTest
 } from 'meteor/vulcan:test';
 
 const createDummyCollection = (typeName, schema) =>
@@ -42,93 +42,89 @@ const foo2Schema = {
 };
 let Foo2s = createDummyCollection('Foo2', foo2Schema);
 
-describe('vulcan:lib/mutators', function(){
+describe('vulcan:lib/mutators', function () {
 
-    let defaultParams;
-    before(async function () {
-        initServerTest();
-  
-      /*Foo2s = await isoCreateCollection({
-        collectionName: 'Foo2s',
-        typeName: 'Foo2',
-        schema: {
-          _id: {
-            type: String,
-            canRead: ['guests'],
-            optional: true,
-          },
-          foo: {
-            type: String,
-            canCreate: ['guests'],
-            canRead: ['guests'],
-            canUpdate: ['guests'],
-          },
-        },
-        resolvers: getDefaultResolvers('Foo2s'),
-        mutations: getDefaultMutations('Foo2s'),
-      });*/
-    });
-    beforeEach(function () {
-        removeAllCallbacks('foo2.create.after');
-        removeAllCallbacks('foo2.create.before');
-        removeAllCallbacks('foo2.create.async');
-        defaultParams = {
-            collection: Foo2s,
-            document: { foo2: 'bar' },
-            currentUser: null,
-            validate: () => true,
-            context: {
-                Users
-            }
-        };
-    });
+  let defaultParams;
+  /*Foo2s = await isoCreateCollection({
+    collectionName: 'Foo2s',
+    typeName: 'Foo2',
+    schema: {
+      _id: {
+        type: String,
+        canRead: ['guests'],
+        optional: true,
+      },
+      foo: {
+        type: String,
+        canCreate: ['guests'],
+        canRead: ['guests'],
+        canUpdate: ['guests'],
+      },
+    },
+    resolvers: getDefaultResolvers('Foo2s'),
+    mutations: getDefaultMutations('Foo2s'),
+  });*/
+  beforeEach(function () {
+    removeAllCallbacks('foo2.create.after');
+    removeAllCallbacks('foo2.create.before');
+    removeAllCallbacks('foo2.create.async');
+    defaultParams = {
+      collection: Foo2s,
+      document: { foo2: 'bar' },
+      currentUser: null,
+      validate: () => true,
+      context: {
+        Users
+      }
+    };
+  });
 
-    test('should run createMutator', async function () {
-        const { data : resultDocument} = await createMutator(defaultParams);
-        expect(resultDocument).toBeDefined();
+  test('should run createMutator', async function () {
+    const { data: resultDocument } = await createMutator(defaultParams);
+    expect(resultDocument).toBeDefined();
+  });
+  // before
+  test.skip('run before callback before document is saved', function () {
+    // TODO get the document in the database
+  });
+  //after
+  test('run after callback  before document is returned', async function () {
+    const afterSpy = sinon.spy();
+    addCallback('foo2.create.after', (document) => {
+      afterSpy();
+      document.after = true;
+      return document;
     });
-    // before
-    test.skip('run before callback before document is saved', function(){
-        // TODO get the document in the database
+    const { data: resultDocument } = await createMutator(defaultParams);
+    expect(afterSpy.calledOnce).toBe(true);
+    expect(resultDocument.after).toBe(true);
+  });
+  // async
+  test('run async callback', async function () {
+    // TODO need a sinon stub
+    const asyncSpy = sinon.spy();
+    addCallback('foo2.create.async', (properties) => {
+      asyncSpy(properties);
+      // TODO need a sinon stub
+      //expect(originalData.after).toBeUndefined()
     });
-    //after
-    test('run after callback  before document is returned', async function(){
-        const afterSpy = sinon.spy();
-        addCallback('foo2.create.after', (document) => {
-            afterSpy();
-            document.after = true;
-            return document;
-        });
-        const { data: resultDocument } = await createMutator(defaultParams);
-        expect(afterSpy.calledOnce).toBe(true);
-        expect(resultDocument.after).toBe(true);
+    const { data: resultDocument } = await createMutator(defaultParams);
+    expect(asyncSpy.calledOnce).toBe(true);
+  });
+  test.skip('provide initial data to async callbacks', async function () {
+    const asyncSpy = sinon.spy();
+    addCallback('foo2.create.after', (document) => {
+      document.after = true;
+      return document;
     });
-    // async
-    test('run async callback', async function(){
-        // TODO need a sinon stub
-        const asyncSpy = sinon.spy();
-        addCallback('foo2.create.async', (properties) => {
-            asyncSpy(properties);
-            // TODO need a sinon stub
-            //expect(originalData.after).toBeUndefined()
-        });
-        const { data: resultDocument } = await createMutator(defaultParams);
-        expect(asyncSpy.calledOnce).toBe(true);
+    addCallback('foo2.create.async', (properties) => {
+      asyncSpy(properties);
+      // TODO need a sinon stub
+      //expect(originalData.after).toBeUndefined()
     });
-    test.skip('provide initial data to async callbacks', async function(){
-        const asyncSpy = sinon.spy();
-        addCallback('foo2.create.after', (document) => {
-            document.after = true;
-            return document;
-        });
-        addCallback('foo2.create.async', (properties) => {
-            asyncSpy(properties);
-            // TODO need a sinon stub
-            //expect(originalData.after).toBeUndefined()
-        });
-        const { data: resultDocument } = await createMutator(defaultParams);
-        expect(asyncSpy.calledOnce).toBe(true);
-        // TODO: check result
-    });
+    const { data: resultDocument } = await createMutator(defaultParams);
+    expect(asyncSpy.calledOnce).toBe(true);
+    // TODO: check result
+  });
 
 });
--- packages/vulcan-test/lib/server/main.js
+++ packages/vulcan-test/lib/server/main.js
@@ -2,3 +2,7 @@ export * from '../modules';
 export { default as isoCreateCollection } from './isoCreateCollection';
 export { default as initServerTest } from './initServerTest';
 export { default as initComponentTest } from './initComponentTest';
+
+// init test in any case
+import { default as initServerTest } from './initServerTest';
+initServerTest();
