e00c6cd4d7af0f4510f3fa6b1972072687220877
eric-burel <eb@lebrun-burel.com>
2019-09-13 14:59:17 +0200

    locking vulcan:core dependencies versions so it does not load ulterior versions
    Currently if you install vulcan:core@1.13.1, it will still use vulcan:lib@1.13.2 as a peer dependency. This causes issues for older app because those versions are usually not compatible.

--- packages/vulcan-core/package.js
+++ packages/vulcan-core/package.js
@@ -5,23 +5,23 @@ Package.describe({
   git: 'https://github.com/VulcanJS/Vulcan.git',
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.6.1');
 
   api.use([
-    'vulcan:lib@1.13.2',
-    'vulcan:i18n@1.13.2',
-    'vulcan:users@1.13.2',
-    'vulcan:debug@1.13.2',
+    'vulcan:lib@=1.13.2',
+    'vulcan:i18n@=1.13.2',
+    'vulcan:users@=1.13.2',
+    'vulcan:debug@=1.13.2',
   ]);
 
-  api.imply(['vulcan:lib@1.13.2']);
+  api.imply(['vulcan:lib@=1.13.2']);
 
   api.mainModule('lib/server/main.js', 'server');
   api.mainModule('lib/client/main.js', 'client');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use(['ecmascript', 'meteortesting:mocha', 'vulcan:core', 'vulcan:test', 'vulcan:users']);
   api.mainModule('./test/index.js');
   api.mainModule('./test/client/index.js', ['client']);
