f2dd4bd2d67717ef6ed84392b050fbeaab331210
juliensl <juliensl@hotmail.fr>
2019-10-14 14:57:50 +0200

    story for MuiSuggest

--- stories/form.stories.js
+++ stories/form.stories.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
@@ -6,36 +5,92 @@ import { action } from '@storybook/addon-actions';
 import { Components } from 'meteor/vulcan:core';
 import 'meteor/vulcan:forms';
 
+const options = [{
+    value: 'Afghan',
+    label: 'Afghan',
+  },
+  {
+    value: 'Albanais',
+    label: 'Albanais',
+  },
+  {
+    value: 'Algérien',
+    label: 'Algérien',
+  },
+  {
+    value: 'Allemand',
+    label: 'Allemand',
+  },
+  {
+    value: 'Belge',
+    label: 'Belge',
+  },
+  {
+    value: 'Beninois',
+    label: 'Beninois',
+  },
+  {
+    value: 'Bosniaque',
+    label: 'Bosniaque',
+  },
+  {
+    value: 'Botswanais',
+    label: 'Botswanais',
+  }]
+
 storiesOf('vulcan-forms')
-.add('FormError - message', () => (
-    <Components.FormError error={{
-        message:'An error message'
-    }}/>
-))
-.add('FormError intl - no properties', () => (
-    <Components.FormError error={{
-        id:'intl-id'
-    }}/>
-))
-.add('FormError intl', () => (
-    <Components.FormError error={{
-        id: 'intl-id',
-        properties:{
-            name:'address.street'
-        }
-    }}/>
-))
-.add('FormNestedArrayLayout - nested item before/after components', () => (
-    <Components.FormNestedArray 
-    value={[{name: 'Jane'}, {name:'DELETED'}, { name:'John'}]}
-    deletedValues={['peoples.1']}
-    path="peoples"
-    formComponents={{...Components, FormNestedItem:() => <div><input /></div>}}
-    errors={[]}
-    updateCurrentValues={action('updateCurrentValues')}
-    arrayField={{
-        beforeComponent: (props) => <div>BEFORE {props.itemIndex} {props.visibleItemIndex}</div>,
-        afterComponent: (props) => <div>AFTER</div>
-    }}
+  .add('FormError - message', () => (
+    <Components.FormError
+      error={{
+        message: 'An error message',
+      }}
     />
-));
+  ))
+  .add('FormError intl - no properties', () => (
+    <Components.FormError
+      error={{
+        id: 'intl-id',
+      }}
+    />
+  ))
+  .add('FormError intl', () => (
+    <Components.FormError
+      error={{
+        id: 'intl-id',
+        properties: {
+          name: 'address.street',
+        },
+      }}
+    />
+  ))
+  .add('FormNestedArrayLayout - nested item before/after components', () => (
+    <Components.FormNestedArray
+      value={[{ name: 'Jane' }, { name: 'DELETED' }, { name: 'John' }]}
+      deletedValues={['peoples.1']}
+      path="peoples"
+      formComponents={{
+        ...Components,
+        FormNestedItem: () => (
+          <div>
+            <input />
+          </div>
+        ),
+      }}
+      errors={[]}
+      updateCurrentValues={action('updateCurrentValues')}
+      arrayField={{
+        beforeComponent: props => (
+          <div>
+            BEFORE {props.itemIndex} {props.visibleItemIndex}
+          </div>
+        ),
+        afterComponent: props => <div>AFTER</div>,
+      }}
+    />
+  ))
+  .add('Form base-controls MuiSuggest', () => (
+    <Components.MuiSuggest
+    // inputProperties = { {options: options} }
+      options={options}
+    />
+  ));
