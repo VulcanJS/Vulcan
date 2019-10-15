b886bcd4d8052fd0e964b2d328a1fdb3248a157c
eric-burel <eb@lebrun-burel.com>
2019-10-15 17:19:04 +0200

    first tests for filtering in multi, not yet passing

---
+++ packages/vulcan-test/lib/modules/createDummyCollection.js
@@ -0,0 +1,34 @@
+import SimpleSchema from 'simpl-schema';
+// return a collection object for unit testing
+const createDummyCollection = ({
+    collectionName = 'Dummies',
+    options = {},
+    schema = {
+        _id: {
+            type: String, canRead: ['admins']
+        }
+    },
+    // results to various calls
+    results = {
+        find: [],
+        findOne: null,
+        load: null
+    },
+    ...otherFields
+}) => {
+    const Dummies = {
+        options: { collectionName, ...options },
+        simpleSchema: () => new SimpleSchema(schema),
+        find: () => ({
+            fetch: () => results.find,
+            count: () => results.length
+        }),
+        loader: {
+            load: () => results.load,
+            prime: () => { }
+        },
+        ...otherFields
+    };
+    return Dummies;
+};
+export default createDummyCollection;
