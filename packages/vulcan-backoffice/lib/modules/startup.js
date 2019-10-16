057b36a9fdfe1567e04cd3a6ce9c1c9c76c2f09b
eric-burel <eb@lebrun-burel.com>
2019-10-16 09:45:00 +0200

    working draft with populate function

--- packages/vulcan-backoffice/lib/modules/startup.js
+++ packages/vulcan-backoffice/lib/modules/startup.js
@@ -4,12 +4,15 @@
 import { getSetting, Collections } from 'meteor/vulcan:core'
 import setupBackoffice from './setupBackoffice'
 import { devOptions } from './options'
+import { addCallback } from 'meteor/vulcan:lib'
 
 const enabled = getSetting('backoffice.enabled', Meteor.isDevelopment)
 
 if (enabled) {
     const options = Meteor.isDevelopment ? devOptions : undefined // loose permissions during development
-    // const providedOptions = runCallbacks('backoffice.options')
-    // const collectionOptions = runCallbacks('backoffice.collectionOptions')
-    setupBackoffice(Collections, options)
+    // setupBackoffice must be run before routes and components are populated
+    // but after startup so that Collections are available
+    addCallback('populate.before', function _setupBackoffice() {
+        setupBackoffice(Collections, options)
+    })
 }
