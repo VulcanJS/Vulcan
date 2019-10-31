b3c7d1db40e733e515ab6b2a226d37c53d305f84
Eric Burel <eb@lebrun-burel.com>
2019-10-31 11:02:59 +0100

    Update DatatableFilter.jsx

--- packages/vulcan-core/lib/modules/components/Datatable/DatatableFilter.jsx
+++ packages/vulcan-core/lib/modules/components/Datatable/DatatableFilter.jsx
@@ -291,7 +291,7 @@ const DatatableFilterNumbers = ({ filters, setFilters }) => (
       inputProperties={{
         onChange: event => {
           const value = event.target.value;
-          if (!value) { //Bug fix - Condition 'value === ''' is always false
+          if (!value) {
             const newFilters = Object.assign({}, filters);
             delete newFilters._lte;
             setFilters(newFilters);
