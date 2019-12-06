b90ff01f128d4a9d728bb87d081af6810c81d8c2
Lucas Sardois <sardois.lucas@gmail.com>
2019-12-06 15:38:11 +0100

    Add Password control form component

---
+++ packages/vulcan-ui-bootstrap/lib/components/forms/Password.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import { Components, registerComponent } from 'meteor/vulcan:core';
+
+const Password = ({ refFunction, inputProperties, itemProperties }) => (
+  <Components.FormItem path={inputProperties.path} label={inputProperties.label} {...itemProperties}>
+    <Form.Control {...inputProperties} ref={refFunction} type="text" />
+  </Components.FormItem>
+);
+
+registerComponent('FormComponentPassword', Password);
