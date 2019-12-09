68dbe3db0e9f7d296a5278b5b3b7f09507b21b05
eric-burel <eb@lebrun-burel.com>
2019-12-09 16:02:07 +0100

    update react-dropzone, test vulcan form upload

--- stories/form/form.stories.js
+++ stories/form/form.stories.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
