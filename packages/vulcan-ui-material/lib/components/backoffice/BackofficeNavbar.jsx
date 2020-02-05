44946ed63d0e56dd5f4311b46f1d143cf23bfed5
eric-burel <eb@lebrun-burel.com>
2020-02-05 09:57:44 +0100

    add link to home in navbar

--- packages/vulcan-ui-material/lib/components/backoffice/BackofficeNavbar.jsx
+++ packages/vulcan-ui-material/lib/components/backoffice/BackofficeNavbar.jsx
@@ -3,28 +3,22 @@ import { registerComponent } from 'meteor/vulcan:lib';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
 
-const BackofficeNavbar = ({onClick}) => (
-	<AppBar position="static">
-		<Toolbar>
+const BackofficeNavbar = ({ onClick, basePath }) => (
+  <AppBar position="static">
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu" onClick={onClick}>
+        <MenuIcon />
+      </IconButton>
 
-			<IconButton
-				edge="start"
-				color="inherit"
-				aria-label="menu"
-				onClick={onClick}
-			>
-				<MenuIcon />
-			</IconButton>
+      <Link to={basePath}>
+        <Typography variant="h6">Backoffice</Typography>
+      </Link>
+    </Toolbar>
+  </AppBar>
+);
 
-			<Typography variant="h6">
-				Backoffice
-			</Typography>
-		</Toolbar>
-	</AppBar>
-)
-
-registerComponent('BackofficeNavbar', BackofficeNavbar)
+registerComponent('BackofficeNavbar', BackofficeNavbar);
