ed4e921f50e8e47e3c4395e1cad6bc06c137733c
SachaG <358832+SachaG@users.noreply.github.com>
2020-02-10 14:53:53 +0900

    Fix async callbacks when array of functions is passed directly

--- packages/vulcan-lib/lib/modules/callbacks.js
+++ packages/vulcan-lib/lib/modules/callbacks.js
@@ -178,22 +178,23 @@ export const runCallbacks = function () {
  * @param {String} hook - First argument: the name of the hook
  * @param {Any} args - Other arguments will be passed to each successive iteration
  */
-export const runCallbacksAsync = function () {
-
-  let hook, args;
+export const runCallbacksAsync = function() {
+  let hook, args, callbacks, formattedHook;
   if (typeof arguments[0] === 'object' && arguments.length === 1) {
     const singleArgument = arguments[0];
     hook = singleArgument.name;
+    formattedHook = formatHookName(hook);
     args = [singleArgument.properties]; // wrap in array for apply
+    callbacks = singleArgument.callbacks ? singleArgument.callbacks : Callbacks[formattedHook];
   } else {
     // OpenCRUD backwards compatibility
     // the first argument is the name of the hook or an array of functions
     hook = arguments[0];
+    formattedHook = formatHookName(hook);
+    callbacks = Array.isArray(hook) ? hook : Callbacks[formattedHook];
     // successive arguments are passed to each iteration
     args = Array.prototype.slice.call(arguments).slice(1);
   }
-
-  const callbacks = Array.isArray(hook) ? hook : Callbacks[hook];
 
   if (typeof callbacks !== 'undefined' && !!callbacks.length) {
     const _runCallbacksAsync = () =>
