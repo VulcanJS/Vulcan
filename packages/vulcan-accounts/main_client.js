31bd078b249659d468c7cd1e09bb8738668efc1b
Apollinaire <no-reply@apollinai.re>
2020-02-10 22:06:08 +0100

    Add useSignout hook to the accounts package

--- packages/vulcan-accounts/main_client.js
+++ packages/vulcan-accounts/main_client.js
@@ -4,8 +4,9 @@ import './imports/components.js';
 import './imports/login_session.js';
 import './imports/routes.js';
 import { STATES } from './imports/helpers.js';
+import useSignout from './imports/useSignout.js';
 
 import './imports/ui/components/LoginForm.jsx';
 
-export { Accounts, STATES };
+export { Accounts, STATES, useSignout };
 export default Accounts;
