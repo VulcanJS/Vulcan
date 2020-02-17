c7f55c4ee6366ab8e7a9f30580a0cc8ce8d37c81
eric-burel <eb@lebrun-burel.com>
2020-02-17 10:49:45 +0100

    import navbar menu icon from mdi-material-ui

--- packages/vulcan-ui-material/lib/components/backoffice/BackofficeNavbar.jsx
+++ packages/vulcan-ui-material/lib/components/backoffice/BackofficeNavbar.jsx
@@ -4,7 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from 'mdi-material-ui/Menu';
 import { Link } from 'react-router-dom';
 
 const BackofficeNavbar = ({ onClick, basePath }) => (
