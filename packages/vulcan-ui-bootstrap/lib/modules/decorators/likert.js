aecca62bd6e2d4e4c3e556f8d31e17e7fd105cd8
SachaG <358832+SachaG@users.noreply.github.com>
2020-02-28 14:07:34 +0900

    Fix Likert decorator on client by removing GraphQL types declarations

--- packages/vulcan-ui-bootstrap/lib/modules/decorators/likert.js
+++ packages/vulcan-ui-bootstrap/lib/modules/decorators/likert.js
@@ -1,11 +1,10 @@
-import { addGraphQLSchema } from 'meteor/vulcan:core';
 import SimpleSchema from 'simpl-schema';
 
-export const makeLikert = (field = {}, options = {}) => {
+export const makeLikert = (field = {}) => {
   // get typeName from fieldName unless it's already specified in field object
-  const { fieldName } = options;
-  const { typeName = `${fieldName}Type` } = field;
+  const { canRead, canCreate, canUpdate } = field;
   const fieldOptions = field.options;
+
   if (!fieldOptions) {
     throw new Error(`Likert fields need an 'options' property`);
   }
@@ -15,29 +14,17 @@ export const makeLikert = (field = {}, options = {}) => {
   fieldOptions.forEach(({ value }) => {
     typeObject[value] = {
       type: SimpleSchema.Integer,
+      canRead,
+      canCreate,
+      canUpdate,
     };
   });
-
-  // create GraphQL types for main type, create input type, and update input type
-  addGraphQLSchema(`type ${typeName} {
-  ${fieldOptions.map(({ value }) => `${value}: Int`).join('\n  ')}
-}
-
-input Create${typeName}DataInput {
-  ${fieldOptions.map(({ value }) => `${value}: Int`).join('\n  ')}
-}
-
-input Update${typeName}DataInput {
-  ${fieldOptions.map(({ value }) => `${value}: Int`).join('\n  ')}
-}
-  `);
 
   // add additional field object properties
   const likertField = {
     ...field,
     type: new SimpleSchema(typeObject),
     input: 'likert',
-    typeName,
   };
 
   return likertField;
