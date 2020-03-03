fc2197d0a2f984c633aec9923c11a38e3cc192dd
juliensl <juliensl@hotmail.fr>
2020-03-03 17:11:08 +0100

    bugfix : give guest permissions to everybody

--- packages/vulcan-users/lib/modules/permissions.js
+++ packages/vulcan-users/lib/modules/permissions.js
@@ -78,10 +78,10 @@ Users.getGroups = (user, document) => {
  */
 Users.getActions = user => {
   let userGroups = Users.getGroups(user);
-  if (!userGroups.includes('guests')) {
-    // always give everybody permission for guests actions, too
-    userGroups.push('guests');
-  }
+
+  // always give everybody permission for guests actions, too
+  userGroups.push('guests');
+  
   let groupActions = userGroups.map(groupName => {
     // note: make sure groupName corresponds to an actual group
     const group = Users.groups[groupName];
