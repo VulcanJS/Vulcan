f333a8050b0630736e4f2fc7702a2c59ca6d311d
Eric Burel <eb@lebrun-burel.com>
2020-04-16 21:58:27 +0200

    Update main_client.js

--- packages/vulcan-accounts/main_client.js
+++ packages/vulcan-accounts/main_client.js
@@ -4,9 +4,9 @@ import './imports/components.js';
 import './imports/login_session.js';
 import './imports/routes.js';
 import { STATES } from './imports/helpers.js';
-import useSignout from './imports/useSignout.js';
+import useMeteorLogout from './imports/useMeteorLogout.js';
 
 import './imports/ui/components/LoginForm.jsx';
 
-export { Accounts, STATES, useSignout };
+export { Accounts, STATES, useMeteorLogout };
 export default Accounts;
