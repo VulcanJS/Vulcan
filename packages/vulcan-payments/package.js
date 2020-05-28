186df75971a07b95e867b4770c80d445dfcc0936
eric-burel <eb@lebrun-burel.com>
2020-05-28 17:42:40 +0200

    bump version

--- packages/vulcan-payments/package.js
+++ packages/vulcan-payments/package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'vulcan:payments',
   summary: 'Vulcan payments package',
-  version: '1.15.0',
+  version: '1.15.1',
   git: 'https://github.com/VulcanJS/Vulcan.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.6.1');
 
-  api.use(['promise', 'vulcan:core@=1.15.0', 'fourseven:scss@4.12.0']);
+  api.use(['promise', 'vulcan:core@=1.15.1', 'fourseven:scss@4.12.0']);
 
   api.mainModule('lib/server/main.js', 'server');
   api.mainModule('lib/client/main.js', 'client');
