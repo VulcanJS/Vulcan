f65995c45a9664f1e4b4ef4e090f930f1a62d9b2
François Miglianico <francois.miglianico@gmail.com>
2021-02-25 09:24:55 +0100

    Issue #2664 - Removed unused parameter in canDelete permission check

--- packages/vulcan-forms/lib/components/FormSubmit.jsx
+++ packages/vulcan-forms/lib/components/FormSubmit.jsx
@@ -51,7 +51,6 @@ const FormSubmit = ({
     ) : null}
   
     { deleteDocument && Users.canDelete({
-        context: { Users },
         user: currentUser,
         document,
         collectionName
