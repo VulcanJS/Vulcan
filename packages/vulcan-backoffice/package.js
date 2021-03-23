0e5338be2059b837947877e19d646d19d3f79f0a
neobii <iwilldevelopcodeforyou@gmail.com>
2021-03-22 23:15:35 -0500

    updated to vulcan accounts 1.6.2

--- packages/vulcan-backoffice/package.js
+++ packages/vulcan-backoffice/package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'vulcan:backoffice',
   summary: 'Vulcan automated backoffice generator',
-  version: '1.16.1',
+  version: '1.16.2',
   git: 'https://github.com/VulcanJS/Vulcan.git',
 });
 
 Package.onUse(api => {
-  api.use(['vulcan:core@=1.16.1', 'vulcan:i18n@=1.16.1', 'vulcan:accounts@1.16.1']);
+  api.use(['vulcan:core@=1.16.1', 'vulcan:i18n@=1.16.1', 'vulcan:accounts@1.16.2']);
 
   api.mainModule('lib/server/main.js', 'server');
   api.mainModule('lib/client/main.js', 'client');
