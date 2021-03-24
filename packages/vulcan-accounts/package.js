6afe620a379b147b5836269537c12a3b9eae4bab
SachaG <358832+SachaG@users.noreply.github.com>
2021-03-24 10:36:26 +0900

    Re-add missing dependencies

--- packages/vulcan-accounts/package.js
+++ packages/vulcan-accounts/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'vulcan:accounts',
-  version: '1.16.2',
+  version: '1.16.2_1',
   summary: 'Accounts UI for React in Meteor 1.3+',
   git: 'https://github.com/studiointeract/accounts-ui',
   documentation: 'README.md',
@@ -9,21 +9,13 @@ Package.describe({
 Package.onUse(function(api) {
   api.use('vulcan:core@=1.16.2');
 
-  // api.use('ecmascript');
-  // api.use('tracker');
-  // api.use('underscore');
-  // api.use('accounts-base@1.8.0');
-  // api.use('check');
-  // api.use('random');
-  // api.use('email@2.0.0');
-  // api.use('session');
-  // api.use('service-configuration');
-
-  api.imply('accounts-base@1.8.0');
-
+  api.use('tracker@1.2.0');
+  api.use('session@1.2.0');
   api.use('accounts-oauth@1.2.0', { weak: true });
   api.use('accounts-password@1.7.0', { weak: true });
   api.use('service-configuration@1.0.11', { weak: true});
+
+  api.imply('accounts-base@1.8.0');
 
   api.mainModule('main_client.js', 'client');
   api.mainModule('main_server.js', 'server');
