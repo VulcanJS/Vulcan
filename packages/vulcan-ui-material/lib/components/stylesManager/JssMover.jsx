08ee05e9e06556adae95bcf5a3217954510dc891
eric-burel <eb@lebrun-burel.com>
2021-09-15 17:51:27 +0200

    improve JSSMover when NoSsr is set

--- packages/vulcan-ui-material/lib/components/stylesManager/JssMover.jsx
+++ packages/vulcan-ui-material/lib/components/stylesManager/JssMover.jsx
@@ -1,14 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import debug from 'debug';
+const debugJss = debug('vulcan:jss');
 
 /**
  * A quickfix to move JSS styles upper in the tree
  */
 export const JssMover = ({ children }) => {
+  const [elems, setElems] = useState([]);
+  // We need this check when NoSsr is set, because the elments might not be there
   useEffect(() => {
-    console.warn('JssMover component moves data-jss styles to the top');
-    console.warn('See https://github.com/mui-org/material-ui/issues/24109');
-    const elements = document.querySelectorAll('[data-jss][data-meta^=Mui],[data-jss][data-meta=ForwardRef]');
-    document.head.prepend(...elements);
+    const checkJss = setInterval(function() {
+      const elements = document.querySelectorAll('[data-jss][data-meta^=Mui],[data-jss][data-meta=ForwardRef]');
+      debugJss('Waiting for jss to be applied...');
+      if (elements.length) {
+        setElems(elements);
+        clearInterval(checkJss);
+      }
+    }, 100);
+    return () => {
+      clearInterval(checkJss);
+    };
   }, []);
+  useEffect(
+    () => {
+      console.warn('JssMover component moves data-jss styles to the top');
+      console.warn('See https://github.com/mui-org/material-ui/issues/24109');
+      document.head.prepend(...elems);
+    },
+    [elems.length]
+  );
   return children;
 };
