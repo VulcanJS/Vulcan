a37d6dc4b4f6bfb7a303ae5f1af1a2bc1989c2f9
eric-burel <eb@lebrun-burel.com>
2021-09-15 09:27:34 +0200

    make jss functionnal$ '

--- packages/vulcan-ui-material/lib/components/stylesManager/JssCleanup.jsx
+++ packages/vulcan-ui-material/lib/components/stylesManager/JssCleanup.jsx
@@ -4,21 +4,17 @@
  * @see https://cssinjs.org/server-side-rendering?v=v10.7.1
  */
 import React, { PureComponent } from 'react';
+import { useEffect } from 'react';
 
-export class JssCleanup extends PureComponent {
-  // Remove the server-side injected CSS.
-  componentDidMount() {
+export const JssCleanup = ({ children }) => {
+  useEffect(() => {
     if (!document || !document.getElementById) return;
-
     const jssStyles = document.getElementById('jss-server-side');
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
+  }, []);
+  return children;
+};
 
 export default JssCleanup;
