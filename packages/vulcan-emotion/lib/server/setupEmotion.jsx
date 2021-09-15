0461a14f03b0569330fd7eb0b943145d92843f0c
eric-burel <eb@lebrun-burel.com>
2021-09-15 14:45:47 +0200

    draft vulcan-emotion package

---
+++ packages/vulcan-emotion/lib/server/setupEmotion.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// Setup SSR
+import { addCallback } from 'meteor/vulcan:core';
+import { CacheProvider } from '@emotion/react';
+import createEmotionServer from '@emotion/server/create-instance';
+import createEmotionCache from './createEmotionCache';
+
+// @see https://next.material-ui.com/guides/server-rendering/#main-content
+const setupEmotion = () => {
+  addCallback('router.server.renderWrapper', function collectStyles(app, { context }) {
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
+    context.extractCriticalToChunks = extractCriticalToChunks;
+    context.constructStyleTagsFromChunks = constructStyleTagsFromChunks;
+    const wrappedApp = <CacheProvider value={cache}>{app}</CacheProvider>;
+    return wrappedApp;
+  });
+
+  addCallback('router.server.postRender', function appendStyleTags(sink, { context }) {
+    const emotionChunks = context.extractCriticalToChunks(sink.body);
+    const emotionCss = context.constructStyleTagsFromChunks(emotionChunks);
+    sink.appendToHead(emotionCss);
+    return sink;
+  });
+};
+
+export default setupEmotion;
---
+++ packages/vulcan-emotion/package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'vulcan:styled-components',
+  summary: 'Add Styled Components to Vulcan.',
+  version: '1.16.1',
+  git: 'https://github.com/VulcanJS/Vulcan.git',
+});
+
+Package.onUse(function(api) {
+  api.use(['vulcan:core@=1.16.1']);
+
+  api.mainModule('lib/server/main.js', 'server');
+  api.mainModule('lib/client/main.js', 'client');
+});
