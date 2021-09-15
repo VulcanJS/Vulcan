169af5d3ac60743dc6480d342594bd7f3d145957
eric-burel <eb@lebrun-burel.com>
2021-09-15 10:06:08 +0200

    remove faulty comma

--- packages/vulcan-ui-material/lib/client/wrapWithMuiTheme.jsx
+++ packages/vulcan-ui-material/lib/client/wrapWithMuiTheme.jsx
@@ -4,7 +4,7 @@ import { addCallback, Components } from 'meteor/vulcan:core';
 function wrapWithMuiTheme(app, { apolloClient }) {
   return (
     <Components.StylesManager>
-      <Components.ThemeProvider apolloClient={apolloClient}>{app}</Components.ThemeProvider>;
+      <Components.ThemeProvider apolloClient={apolloClient}>{app}</Components.ThemeProvider>
     </Components.StylesManager>
   );
 }
