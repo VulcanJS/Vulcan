0ebd3fed73e7df2e5d617478eb10186bfc7eda57
eric-burel <eb@lebrun-burel.com>
2021-09-15 18:57:50 +0200

    handle multiple scenarios in JssMover

--- packages/vulcan-ui-material/lib/components/stylesManager/JssMover.jsx
+++ packages/vulcan-ui-material/lib/components/stylesManager/JssMover.jsx
@@ -1,14 +1,33 @@
+import { replaceComponent, Components } from 'meteor/vulcan:lib';
 import { useEffect } from 'react';
 
 /**
  * A quickfix to move JSS styles upper in the tree
+ *
+ * @see packages/vulcan-core/lib/client/components/AppGenerator.jsx
+ * (we don't have access to router yet, so we also add smth similar in AppGenerator)
  */
 export const JssMover = ({ children }) => {
   useEffect(() => {
     console.warn('JssMover component moves data-jss styles to the top');
     console.warn('See https://github.com/mui-org/material-ui/issues/24109');
     const elements = document.querySelectorAll('[data-jss][data-meta^=Mui],[data-jss][data-meta=ForwardRef]');
-    document.head.prepend(...elements);
+    if (elements.length) {
+      document.head.prepend(...elements);
+    }
   }, []);
+  // also apply every second
+  useEffect(() => {
+    const checkJss = setInterval(() => {
+      const elements = document.querySelectorAll('[data-jss][data-meta^=Mui],[data-jss][data-meta=ForwardRef]');
+      if (elements.length) {
+        console.warn('Moving JSS styles every x seconds');
+        document.head.prepend(...elements);
+      }
+    }, 3000);
+    return () => {
+      clearInterval(checkJss);
+    };
+  });
   return children;
 };
