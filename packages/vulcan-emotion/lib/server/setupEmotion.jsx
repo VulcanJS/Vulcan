1d4d73a3c18c0c338873e5de1b6f0cc68ea7bae7
eric-burel <eb@lebrun-burel.com>
2021-09-15 17:28:19 +0200

    emotion can fill cache but not yet working

--- packages/vulcan-emotion/lib/server/setupEmotion.jsx
+++ packages/vulcan-emotion/lib/server/setupEmotion.jsx
@@ -7,18 +7,21 @@ import createEmotionCache from '../modules/createEmotionCache';
 
 // @see https://next.material-ui.com/guides/server-rendering/#main-content
 const setupEmotion = () => {
-  addCallback('router.server.renderWrapper', function collectStyles(app, { context }) {
+  // NOTE: this doesn't work with server.renderWrapper
+  addCallback('router.server.wrapper', function collectStyles(app, { context }) {
     const cache = createEmotionCache();
-    const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
-    context.extractCriticalToChunks = extractCriticalToChunks;
-    context.constructStyleTagsFromChunks = constructStyleTagsFromChunks;
+    context.cache = cache;
     const wrappedApp = <CacheProvider value={cache}>{app}</CacheProvider>;
     return wrappedApp;
   });
 
   addCallback('router.server.postRender', function appendStyleTags(sink, { context }) {
-    const emotionChunks = context.extractCriticalToChunks(sink.body);
-    const emotionCss = context.constructStyleTagsFromChunks(emotionChunks);
+    const html = sink.body;
+    const { cache } = context;
+    const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
+    // FIXME: chunks are empty, yet the cache is correctl filled
+    const emotionChunks = extractCriticalToChunks(html);
+    const emotionCss = constructStyleTagsFromChunks(emotionChunks);
     sink.appendToHead(emotionCss);
     return sink;
   });
